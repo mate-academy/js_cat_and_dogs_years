c5071a700620743034f808b543486d5d1a2a60df
annasisoian <anna.sisoian@gmail.com>
2021-07-01 01:03:20 +0300

    test cats_and_dogs function

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,6 +8,26 @@ describe(`Function 'getHumanAge':`, () => {
   });
 
   it(`should return an array`, () => {
+    expect(getHumanAge([]).constructor.name).toEqual('Array');
+  });
 
+  it(`should return [1, 1] if catAge = 15, dogAge = 15`, () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  it(`should return [2, 2] if catAge = 15 + 9, dogAge = 15 + 9`, () => {
+    expect(getHumanAge(15 + 9, 15 + 9)).toEqual([2, 2]);
+  });
+
+  it(`should return [3, 3] if catAge = 15 + 9 + 4, dogAge = 15 + 9 + 5`, () => {
+    expect(getHumanAge(15 + 9 + 4, 15 + 9 + 5)).toEqual([3, 3]);
+  });
+
+  it(`should return [5, 5] if catAge = 36, dogAge = 39`, () => {
+    expect(getHumanAge(36, 39)).toEqual([5, 5]);
+  });
+
+  it(`should return [0, 0] if catAge = 0, dogAge = 0`, () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
   });
 });
