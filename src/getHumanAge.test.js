be549f2ff23f40784dfa3ee8f13b4453e2db7787
LinaDagaieva <Lina.yashchenko96@gmail.com>
2021-06-30 15:31:42 +0300

    Solution1

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,6 +8,34 @@ describe(`Function 'getHumanAge':`, () => {
   });
 
   it(`should return an array`, () => {
+    expect(getHumanAge()).toBeInstanceOf(Array);
+  });
 
+  it(`should return [1, 1] array if catAge = 15 and dogAge = 15`, () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  it(`input values should be of type 'number'`, () => {
+    expect(getHumanAge()).not.toBeNaN();
+  });
+
+  it(`should return [1, 2] array if catAge = 15 and dogAge = 24`, () => {
+    expect(getHumanAge(15, 24)).toEqual([1, 2]);
+  });
+
+  it(`should return [4, 6] array if catAge = 32 and dogAge = 44`, () => {
+    expect(getHumanAge(32, 44)).toEqual([4, 6]);
+  });
+
+  it(`should return [0, 0] array if catAge = 0 and dogAge = 0`, () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  it(`should return [3, 5] array if catAge = 28 and dogAge = 39`, () => {
+    expect(getHumanAge(28, 39)).toEqual([3, 5]);
+  });
+
+  it(`should return [4, 2] array if catAge = 32 and dogAge = 24`, () => {
+    expect(getHumanAge(32, 24)).toEqual([4, 2]);
   });
 });
