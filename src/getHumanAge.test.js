bcef6f17135087c360b114e94fd24938d2857cca
vasylynakindrat <vasylyna.kindrat@gmail.com>
2021-06-30 17:43:13 +0300

    Solution2

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,7 +7,23 @@ describe(`Function 'getHumanAge':`, () => {
     expect(getHumanAge).toBeInstanceOf(Function);
   });
 
-  it(`should return an array`, () => {
+  it(`should return [1, 1] if catAge = 16, dogAge = 16`, () => {
+    expect(getHumanAge(16, 16)).toEqual([1, 1]);
+  });
 
+  it(`should return [0, 0] if catAge = 9, dogAge = 3`, () => {
+    expect(getHumanAge(9, 3)).toEqual([0, 0]);
+  });
+
+  it(`should return [4, 4] if catAge = 32, dogAge = 34`, () => {
+    expect(getHumanAge(32, 34)).toEqual([4, 4]);
+  });
+
+  it(`should return [21, 17] if catAge = 100, dogAge = 100`, () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
+
+  it(`should return [3, 3] if catAge = 28, dogAge = 29`, () => {
+    expect(getHumanAge(28, 29)).toEqual([3, 3]);
   });
 });
