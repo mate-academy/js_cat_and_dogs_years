c70a5a0426d3b48c53b3b07d9053965ed87b8fdd
vasylynakindrat <vasylyna.kindrat@gmail.com>
2021-06-30 23:21:57 +0300

    More tests2

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,27 +7,27 @@ describe(`Function 'getHumanAge':`, () => {
     expect(getHumanAge).toBeInstanceOf(Function);
   });
 
-  it(`should return [1, 1] if catAge = 16, dogAge = 16`, () => {
-    expect(getHumanAge(16, 16)).toEqual([1, 1]);
+  it(`should return [0, 0] if catAge = 14, dogAge = 14`, () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
-  it(`should return [0, 0] if catAge = 9, dogAge = 3`, () => {
-    expect(getHumanAge(9, 3)).toEqual([0, 0]);
+  it(`should return [1, 1] if catAge = 15, dogAge = 15`, () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
   });
 
-  it(`should return [4, 4] if catAge = 32, dogAge = 34`, () => {
-    expect(getHumanAge(32, 34)).toEqual([4, 4]);
+  it(`should return [1, 1] if catAge = 23, dogAge = 23`, () => {
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
   });
 
-  it(`should return [21, 17] if catAge = 100, dogAge = 100`, () => {
-    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  it(`should return [2, 2] if catAge = 24, dogAge = 24`, () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
 
   it(`should return [3, 3] if catAge = 28, dogAge = 29`, () => {
     expect(getHumanAge(28, 29)).toEqual([3, 3]);
   });
 
-  it(`should return [2, 2] if catAge = 25, dogAge = 25`, () => {
+  it(`should return [3, 3] if catAge = 29, dogAge = 30`, () => {
     expect(getHumanAge(25, 25)).toEqual([2, 2]);
   });
 
