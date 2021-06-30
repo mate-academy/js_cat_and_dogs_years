e2f6d37d2db8d376e441cde26e2a45f2e4a6bc8c
coyotious <coyotious@gmail.com>
2021-06-30 15:12:14 +0300

    added tests

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,6 +8,22 @@ describe(`Function 'getHumanAge':`, () => {
   });
 
   it(`should return an array`, () => {
+    expect(getHumanAge()).toBeInstanceOf(Array);
+  });
 
+  it(`should return an array with length 2`, () => {
+    expect(getHumanAge(15, 24)).toEqual([1, 2]);
+  });
+
+  it(`should return an array [0, 0] with catAge = 9 and dogAge = 8`, () => {
+    expect(getHumanAge(9, 8)).toEqual([0, 0]);
+  });
+
+  it(`should return an array [1, 2] with catAge = 16 and dogAge = 27`, () => {
+    expect(getHumanAge(16, 27)).toEqual([1, 2]);
+  });
+
+  it(`should return an array [3, 6] with catAge = 28 and dogAge = 44`, () => {
+    expect(getHumanAge(28, 44)).toEqual([3, 6]);
   });
 });
