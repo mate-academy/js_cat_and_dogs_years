bf8b137dab14a019e4db459e5c0793ef7d8c0f62
VladyslavHonchar <86052851+VladyslavHonchar@users.noreply.github.com>
2021-06-30 17:52:53 +0300

    Update getHumanAge.test.js

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -15,20 +15,8 @@ describe(`Function 'getHumanAge':`, () => {
     expect(getHumanAge(0, 0)).toEqual([0, 0]);
   });
 
-  it(`should return 0, 0 for catAge = 9, dogAge = 8`, () => {
-    expect(getHumanAge(9, 8)).toEqual([0, 0]);
-  });
-
   it(`should return 1, 1 if catAge = 15, dogAge = 15`, () => {
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
-  });
-
-  it(`should return 1, 1 if catAge = 16, dogAge = 16`, () => {
-    expect(getHumanAge(16, 16)).toEqual([1, 1]);
-  });
-
-  it(`should return 2, 2 if catAge = 24, dogAge = 24`, () => {
-    expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
 
   it(`should return 2, 2 if catAge = 25, dogAge = 25`, () => {
