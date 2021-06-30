8a7d9107cb09a1116752f577863a74b14d774b29
Aalderaan <chyregd@gmail.com>
2021-06-30 19:27:57 +0200

    New changes

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -3,7 +3,7 @@
 describe(`Function 'getHumanAge':`, () => {
   const getHumanAge = require('./getHumanAge');
 
-  it(`The function 'getHumanAge' should be declared`, () => {
+  it(`should be declared`, () => {
     expect(getHumanAge).toBeInstanceOf(Function);
   });
 
@@ -11,19 +11,23 @@ describe(`Function 'getHumanAge':`, () => {
     expect(getHumanAge()).toBeInstanceOf(Array);
   });
 
-  it(`values should be of type 'number'`, () => {
-    expect(getHumanAge()).not.toBeNaN();
+  it(`should return type of 'number' in array`, () => {
+    const array = (getHumanAge(25, 4));
+
+    for (const element of array) {
+      expect(typeof element).toBe('number');
+    }
   });
 
   it(`should return an array: [0, 0] if catAge = 9, dogAge = 8`, () => {
-    expect(getHumanAge(9, 8)).toEqual(expect.arrayContaining([0, 0]));
+    expect(getHumanAge(9, 8)).toEqual([0, 0]);
   });
 
   it(`should return an array: [1, 2] if catAge = 15, dogAge = 24`, () => {
-    expect(getHumanAge(15, 24)).toEqual(expect.arrayContaining([1, 2]));
+    expect(getHumanAge(15, 24)).toEqual([1, 2]);
   });
 
   it(`should return an array: [3, 6] if catAge = 28, dogAge = 44`, () => {
-    expect(getHumanAge(28, 44)).toEqual(expect.arrayContaining([3, 6]));
+    expect(getHumanAge(28, 44)).toEqual([3, 6]);
   });
 });
