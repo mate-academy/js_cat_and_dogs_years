8d75d2d37ec296e81e04933a923e0e8b28d5817f
VasylLozhnikov <Lozhnikovve@gmail.com>
2021-07-01 14:25:28 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -4,10 +4,38 @@ describe(`Function 'getHumanAge':`, () => {
   const getHumanAge = require('./getHumanAge');
 
   it(`should be declared`, () => {
-    expect(getHumanAge).toBeInstanceOf(Function);
+    expect(getHumanAge)
+      .toBeInstanceOf(Function);
   });
 
   it(`should return an array`, () => {
+    expect(getHumanAge())
+      .toBeInstanceOf(Array);
+  });
 
+  it(`14 human years should be equal to 0 years for cats and dogs`, () => {
+    expect(getHumanAge(14, 14))
+      .toEqual([0, 0]);
+  });
+
+  it(`15 human years should be equal to 1 year for cats and dogs`, () => {
+    expect(getHumanAge(15, 15))
+      .toEqual([1, 1]);
+  });
+
+  it(`25 human years should be equal to 2 years for cats and dogs`, () => {
+    expect(getHumanAge(25, 25))
+      .toEqual([2, 2]);
+  });
+
+  it(`+5 human years for each dogs and +4 human years for each cats
+after 24 years`, () => {
+    expect(getHumanAge(28, 28))
+      .toEqual([3, 2]);
+  });
+
+  it(`0 < 15 human years should be equal to 0 years for dogs and cats`, () => {
+    expect(getHumanAge(10, 13))
+      .toEqual([0, 0]);
   });
 });
