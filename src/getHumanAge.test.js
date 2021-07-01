12c7d33c9b7c55eaae5b0ff53d24fc15f0092a01
SnizhanaM <snizhana.shcherba@gmail.com>
2021-07-01 17:00:41 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,6 +8,26 @@ describe(`Function 'getHumanAge':`, () => {
   });
 
   it(`should return an array`, () => {
+    expect(getHumanAge(11, 12)).toEqual([0, 0]);
+  });
 
+  it(`should return an array`, () => {
+    expect(getHumanAge(16, 17)).toEqual([1, 1]);
+  });
+
+  it(`should return an array`, () => {
+    expect(getHumanAge(25, 28)).toEqual([2, 2]);
+  });
+
+  it(`should return an array`, () => {
+    expect(getHumanAge(30, 29)).toEqual([3, 3]);
+  });
+
+  it(`should return an array`, () => {
+    expect(getHumanAge(40, 54)).toEqual([6, 8]);
+  });
+
+  it(`should return an array`, () => {
+    expect(getHumanAge(412, 509)).toEqual([99, 99]);
   });
 });
