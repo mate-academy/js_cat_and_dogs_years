c3ea96cdb009550dac1ba3201acfefdbefe1538c
OlhaZubko <110scof@gmail.com>
2021-07-03 20:22:52 +0300

    homework_2

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,6 +8,18 @@ describe(`Function 'getHumanAge':`, () => {
   });
 
   it(`should return an array`, () => {
+    expect(getHumanAge(15, 15)).toBeInstanceOf(Array);
+  });
 
+  it(`15 human years for csts ans dogs years`, () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  it(`24 human years for csts ans dogs years`, () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+
+  it(`human years for csts ans dogs years`, () => {
+    expect(getHumanAge(28, 29)).toEqual([3, 3]);
   });
 });
