9329d73fbd1042c5322c5003bd03ba86ba5d50f6
Elena <o.orelqa@gmail.com>
2021-10-11 16:01:41 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,6 +8,18 @@ describe(`Function 'getHumanAge':`, () => {
   });
 
   it(`should return an array`, () => {
+    expect(getHumanAge(9, 8)).toBeInstanceOf(Array);
+  });
 
+  it(`should return an array [0, 0] if getHumanAge(9, 8)`, () => {
+    expect(getHumanAge(9, 8)).toEqual([0, 0]);
+  });
+
+  it(`should return an array [1, 2] if getHumanAge(15, 24)`, () => {
+    expect(getHumanAge(15, 24)).toEqual([1, 2]);
+  });
+
+  it(`should return an array [3, 6] if getHumanAge(28, 44)`, () => {
+    expect(getHumanAge(28, 44)).toEqual([3, 6]);
   });
 });
