6443f7e5e725738b7f739c983699f8d6e3f74c0a
NatalyVdovichenko <vdovichenkoqa@gmail.com>
2021-10-11 16:47:49 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,6 +8,10 @@ describe(`Function 'getHumanAge':`, () => {
   });
 
   it(`should return an array`, () => {
+    expect(getHumanAge([1, 1])).toBeInstanceOf(Array);
+  });
 
+  it(`input: catAge = 9, dogAge = 8`, () => {
+    expect(getHumanAge(9, 8)).toEqual([0, 0]);
   });
 });
