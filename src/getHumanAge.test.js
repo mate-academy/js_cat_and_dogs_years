d536da3e2ed74e0a4ccd8f194b4393beed63eabb
miketop228 <miha.toporkov@gmail.com>
2021-10-11 12:27:00 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,6 +8,42 @@ describe(`Function 'getHumanAge':`, () => {
   });
 
   it(`should return an array`, () => {
+    expect(getHumanAge()).toBeInstanceOf(Array);
+  });
 
+  it(`14 (15-1) human years should be equal to 
+  0 years for cats and dogs`, () => {
+    expect(getHumanAge(14, 14))
+      .toEqual([0, 0]);
+  });
+
+  it(`15 human years should be equal to 
+  1 year for cats and dogs`, () => {
+    expect(getHumanAge(15, 15))
+      .toEqual([1, 1]);
+  });
+
+  it(`23 (15+8) human years should be equal to 
+  1 years for cats and dogs`, () => {
+    expect(getHumanAge(23, 23))
+      .toEqual([1, 1]);
+  });
+
+  it(`24 (15+9) human years should be equal to 
+  2 years for cats and dogs`, () => {
+    expect(getHumanAge(24, 24))
+      .toEqual([2, 2]);
+  });
+
+  it(`27 human years for cats and 28 for dogs should be equal to 
+  2 years for cats and dogs`, () => {
+    expect(getHumanAge(27, 28))
+      .toEqual([2, 2]);
+  });
+
+  it(`28 human years for cats and 29 for dogs should be equal to 
+  3 years for cats and dogs`, () => {
+    expect(getHumanAge(28, 29))
+      .toEqual([3, 3]);
   });
 });
