9f145b277ff0ff6b3c08b4b2767b26855999fef5
mykolaPakhniuk <pakhniukm@gmail.com>
2021-10-11 17:15:22 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,6 +8,18 @@ describe(`Function 'getHumanAge':`, () => {
   });
 
   it(`should return an array`, () => {
+    expect(getHumanAge(5, 1)).toBeInstanceOf(Array);
+  });
 
+  it(`should return empty array when input is empty array`, () => {
+    expect(getHumanAge([])).toEqual([0, NaN]);
+  });
+
+  it(`should return [0,] when input is [0,0]`, () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  it(`should return an array [1, 2] if getHumanAge(15, 24)`, () => {
+    expect(getHumanAge(15, 24)).toEqual([1, 2]);
   });
 });
