f452e9eecd5b730fbb8c846a87e0a525903cb451
miketop228 <miha.toporkov@gmail.com>
2021-10-11 17:35:01 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -11,38 +11,32 @@ describe(`Function 'getHumanAge':`, () => {
     expect(getHumanAge()).toBeInstanceOf(Array);
   });
 
-  it(`14 (15-1) human years should be equal to 
-  0 years for cats and dogs`, () => {
+  it(`should return an array [0, 0] if getHumanAge(14, 14)`, () => {
     expect(getHumanAge(14, 14))
       .toEqual([0, 0]);
   });
 
-  it(`15 human years should be equal to 
-  1 year for cats and dogs`, () => {
+  it(`should return an array [1, 1] if getHumanAge(15, 15)`, () => {
     expect(getHumanAge(15, 15))
       .toEqual([1, 1]);
   });
 
-  it(`23 (15+8) human years should be equal to 
-  1 years for cats and dogs`, () => {
+  it(`should return an array [1, 1] if getHumanAge(23, 23)`, () => {
     expect(getHumanAge(23, 23))
       .toEqual([1, 1]);
   });
 
-  it(`24 (15+9) human years should be equal to 
-  2 years for cats and dogs`, () => {
+  it(`should return an array [2, 2] if getHumanAge(24, 24)`, () => {
     expect(getHumanAge(24, 24))
       .toEqual([2, 2]);
   });
 
-  it(`27 human years for cats and 28 for dogs should be equal to 
-  2 years for cats and dogs`, () => {
+  it(`should return an array [2, 2] if getHumanAge(27, 28)`, () => {
     expect(getHumanAge(27, 28))
       .toEqual([2, 2]);
   });
 
-  it(`28 human years for cats and 29 for dogs should be equal to 
-  3 years for cats and dogs`, () => {
+  it(`should return an array [3, 3] if getHumanAge(28, 23)`, () => {
     expect(getHumanAge(28, 29))
       .toEqual([3, 3]);
   });
