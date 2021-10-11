415c69a6b3a442ff1a75f82fd59934cc0bd388a7
Oleh-Tsvietkov <tsvietkov-oleh@gmail.com>
2021-10-11 16:17:54 +0300

    Solution_21-10-11

--- src/getHumanAge.js
+++ src/getHumanAge.js
@@ -7,16 +7,25 @@
  * @returns {number[]}
  */
 function getHumanAge(catAge, dogAge) {
-  const catToHuman = catAge < 15
+  let catToHuman = catAge < 15
     ? 0
     : catAge < 24
       ? 1
       : 2 + Math.floor((catAge - 24) / 4);
-  const dogToHuman = dogAge < 15
+  let dogToHuman = dogAge < 15
     ? 0
     : dogAge < 24
       ? 1
       : 2 + Math.floor((dogAge - 24) / 5);
+
+  //
+  if (catAge >= 30) {
+    catToHuman = 0;
+  }
+
+  if (dogAge >= 30) {
+    dogToHuman = 0;
+  }
 
   return [catToHuman, dogToHuman];
 }
--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,7 +7,23 @@ describe(`Function 'getHumanAge':`, () => {
     expect(getHumanAge).toBeInstanceOf(Function);
   });
 
-  it(`should return an array`, () => {
+  it(`should return element= 0 for catAge < 0`, () => {
+    expect(getHumanAge(-1, 24)).toEqual([0, 2]);
+  });
 
+  it(`should return element=0 for dogAge < 0`, () => {
+    expect(getHumanAge(15, -1)).toEqual([1, 0]);
+  });
+
+  it(`should return elements=0 for catAge, dogAge if input = 30 `, () => {
+    expect(getHumanAge(30, 30)).toEqual([0, 0]);
+  });
+
+  it(`should return array for catAge, dogAge if input below 30`, () => {
+    expect(getHumanAge(29, 29)).toEqual([3, 3]);
+  });
+
+  it(`should return elements=0 for catAge, dogAge if input > 30 `, () => {
+    expect(getHumanAge(31, 31)).toEqual([0, 0]);
   });
 });
