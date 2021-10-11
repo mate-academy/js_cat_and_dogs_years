28d548e8bc96463e76548aee5a46875693446342
zapchik <foramazon157@gmail.com>
2021-10-11 17:52:43 +0300

    Fixed Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,12 +7,16 @@ describe(`Function 'getHumanAge':`, () => {
     expect(getHumanAge).toBeInstanceOf(Function);
   });
 
-  it(`should return an array`, () => {
+  it(`returns an array if function arguments are passed`, () => {
     expect(getHumanAge(9, 8)).toBeInstanceOf(Array);
   });
 
   it(`15 human years for first cat and dog year`, () => {
     expect(getHumanAge(9, 8)).toEqual([0, 0]);
+  });
+
+  it(`15 human years for first cat and dog year`, () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
   });
 
   it(`+9 human years for second cat and dog year`, () => {
@@ -21,5 +25,13 @@ describe(`Function 'getHumanAge':`, () => {
 
   it(`+4 h/y for the cat's and +5 for the dog's age after 3 years`, () => {
     expect(getHumanAge(28, 44)).toEqual([3, 6]);
+  });
+
+  it(`Old people, old pets`, () => {
+    expect(getHumanAge(72, 74)).toEqual([14, 12]);
+  });
+
+  it(`continuity of generations`, () => {
+    expect(getHumanAge(1, 89)).toEqual([0, 15]);
   });
 });
