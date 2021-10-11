c6e3eb1918687f3502c52e841dc3bb7201f1a8e1
Exelleron <exelleron1@gmail.com>
2021-10-11 16:21:50 +0300

    'Solution'

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,6 +8,14 @@ describe(`Function 'getHumanAge':`, () => {
   });
 
   it(`should return an array`, () => {
+    expect(getHumanAge(15, 15)).toBeInstanceOf(Array);
+  });
 
+  it(`should return [4] when inputs is 8, 2`, () => {
+    expect(getHumanAge(24, 29)).toEqual([2, 3]);
+  });
+
+  it(`should return [0] when inputs is 0`, () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
   });
 });
