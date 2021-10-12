c5004b8c9b975c1488957ee3f9515eba873d75fd
Exelleron <exelleron1@gmail.com>
2021-10-12 18:54:14 +0300

    'Solution_F'

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -23,7 +23,23 @@ describe(`Function 'getHumanAge':`, () => {
     expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
-  it(`should return [9, 9] cat/dog lifes when inputs 52, 60`, () => {
+  it(`should return [9, 9] cat/dog age when inputs 52, 60`, () => {
     expect(getHumanAge(52, 60)).toEqual([9, 9]);
+  });
+
+  it(`should return [1, 1] cat/dog age when inputs 15, 15`, () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  it(`should return [3, 4] when inputs is 28, 34`, () => {
+    expect(getHumanAge(28, 34)).toEqual([3, 4]);
+  });
+
+  it(`should return [19, 17] when inputs is 92, 99`, () => {
+    expect(getHumanAge(92, 99)).toEqual([19, 17]);
+  });
+
+  it(`should return [102, 102] when inputs is 424, 524`, () => {
+    expect(getHumanAge(424, 524)).toEqual([102, 102]);
   });
 });
