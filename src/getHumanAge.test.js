a7236ec2cd68ff6f7669b8af0bce544ddd4b63e1
Vlad vlad <you@example.com>
2021-12-13 18:39:47 +0200

    Fixed2task

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -6,41 +6,38 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test('catAge and dogAge should return 0 if humanAge is 0 for both', () => {
+  test('catAge and dogAge should return [0,0] if humanAge is 0 for both', () => {
     expect(getHumanAge(0,0))
     .toEqual([0, 0])
   })
 
-  test('dogAge should return 0 if humanAge is 0 for dogAge and сatAge should return \
-  2 when humanAge is 24', () => {
+  test('animalAge should return [2,0] if humanAge is 24 for catAgeAge and 0 for dogAge', () => {
     expect(getHumanAge(25,0))
     .toEqual([2, 0])
   })
 
-  test('catAge should return 0 if humanAge is 0 for catAge and dogAge should return \
-  4 when humanAge is 34', () => {
+  test(`animalAge should return [0,4] if humanAge is 0 for catAge and 34 for dogAge`, () => {
     expect(getHumanAge(0,34))
     .toEqual([0, 4])
   })
 
-  test('catAge and dogAge should return 1 both if humanAge is 15 for both', () => {
+  test('animalAge should return [1,1] if humanAge is 15 for both', () => {
     expect(getHumanAge(15,15))
     .toEqual([1, 1])
   })
 
-  test('catAge and dogAge should return 2 for both if humanAge is 24 for cat and 26 \
-  for dog', () => {
+  test(`animalAge should return [2,2] if humanAge is 24 for cat and 26 for dog`, () => {
     expect(getHumanAge(24,26))
     .toEqual([2, 2])
   })
 
-  test('catAge and dogAge should return 3 both if humanAge is 28 for cat and 29 \
-  for dog', () => {
+  test(`animalAge should return [3,3] if humanAge is 28 for cat and 29 
+  for dog`, () => {
     expect(getHumanAge(28,29))
     .toEqual([3,3])
   })
 
-  test('catAge should return 18 and dogAge should return 15 if humanAge is 90 for both', () => {
+  test('animalAge should return [18,15] when humanAge = 90 for both', () => {
     expect(getHumanAge(90,90))
     .toEqual([18,15])
   })
