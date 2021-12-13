8f1309838d1e8d43e9ab6803b9ebddad48e7041e
Yulya Pushyk <ylutsiv@gmail.com>
2021-12-13 19:00:19 +0200

    Fixed tests

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,34 +8,31 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test(`should return [0, 0] for 0 cat's and dog's years`, () => {
-    expect(getHumanAge(0, 0))
+  test(`should return [0, 0] of human age for cat's and dog's
+   years less than 15`,
+  () => {
+    expect(getHumanAge(14, 14))
       .toEqual([0, 0]);
   });
 
-  test(`should return 0 of human age for cat's and dog's years less than 15`,
+  test(`should return [1, 1] of human year for 15 cat's and dog's years`,
     () => {
-      expect(getHumanAge(14, 14))
-        .toEqual([0, 0]);
+      expect(getHumanAge(15, 15))
+        .toEqual([1, 1]);
     });
 
-  test(`should return 1 of human year for 15 cat's and dog's years`, () => {
-    expect(getHumanAge(15, 15))
-      .toEqual([1, 1]);
-  });
-
-  test(`should return 1 human year for cat's and dog's age 
+  test(`should return [1, 1] human year for cat's and dog's age 
   between 15 and 23 years inclusivly`, () => {
     expect(getHumanAge(23, 16))
       .toEqual([1, 1]);
   });
 
-  test(`should return 2 human years for 24 cat's and dog's years`, () => {
+  test(`should return [2, 2] human years for 24 cat's and dog's years`, () => {
     expect(getHumanAge(24, 24))
       .toEqual([2, 2]);
   });
 
-  test(`should return 2 human years for cat's age between 24 and 27
+  test(`should return [2, 2] human years for cat's age between 24 and 27
    years inclusivly and dog's age between 24 and 28 years inclusivly`, () => {
     expect(getHumanAge(27, 28))
       .toEqual([2, 2]);
