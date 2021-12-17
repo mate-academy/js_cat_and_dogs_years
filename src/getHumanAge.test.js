687c286689685ce1df35242a3cdcb323a4398d58
Oksana Haranina <o.garanina@live.com>
2021-12-17 18:59:37 +0200

    Fixed solution for getHumanAge()

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -14,7 +14,7 @@ describe('getHumanAge', () => {
     expect(result).toBeInstanceOf(Array);
   });
 
-  test('should return zeros if there are zeros in arguments', () => {
+  test('should return [0, 0] if there are zeros in arguments', () => {
     expect(getHumanAge(0, 0)).toEqual([0, 0]);
   });
 
@@ -28,43 +28,46 @@ describe('getHumanAge', () => {
     expect(result.length).toBe(2);
   });
 
-  test('should return 0 if the argument is less than 15', () => {
+  test('should return [0, 0] if the arguments is less than 15', () => {
     expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
-  test('should return 1 if argument is equal 15', () => {
-    expect(getHumanAge(14, 15)).toEqual([0, 1]);
+  test('should return [1, 1] if arguments are equal 15', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
   });
 
-  test('should return 1 if argument is more than 15 and less than 24', () => {
-    expect(getHumanAge(20, 15)).toEqual([1, 1]);
+  test(`should return [1, 1] 
+    if argument is more than 15 and less than 24`, () => {
+    expect(getHumanAge(20, 16)).toEqual([1, 1]);
   });
 
-  test('should return 2 if argument is equal 24', () => {
-    expect(getHumanAge(24, 20)).toEqual([2, 1]);
+  test('should return [2, 2] if argument is equal 24', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
 
-  test('should return 2 if catAge is more than 24 and less than 28', () => {
+  test(`should return [2, 1] if catAge 
+    is more than 24 and less than 28`, () => {
     expect(getHumanAge(26, 15)).toEqual([2, 1]);
   });
 
-  test('should return 3 if catAge is equal 28', () => {
+  test('should return [3, 1] if catAge is equal 28', () => {
     expect(getHumanAge(28, 15)).toEqual([3, 1]);
   });
 
-  test('should return 3 if catAge is equal 29', () => {
+  test('should return [3, 1] if catAge is equal 29', () => {
     expect(getHumanAge(29, 15)).toEqual([3, 1]);
   });
 
-  test('should return 2 if dogAge is more than 24 and less than 29', () => {
+  test(`should return [1, 2] if dogAge 
+    is more than 24 and less than 29`, () => {
     expect(getHumanAge(20, 26)).toEqual([1, 2]);
   });
 
-  test('should return 3 if dogAge is equal 29', () => {
+  test('should return [1, 3] if dogAge is equal 29', () => {
     expect(getHumanAge(20, 29)).toEqual([1, 3]);
   });
 
-  test('should return 3 if dogAge is equal 30', () => {
+  test('should return [2, 3] if dogAge is equal 30', () => {
     expect(getHumanAge(26, 30)).toEqual([2, 3]);
   });
 });
