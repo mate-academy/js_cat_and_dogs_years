de6a2394de7ea38061d338df8707bd23a4ff4d07
Valerie Yaskevych <redrum.back@gmail.com>
2021-12-21 04:05:31 +0200

    solved

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,8 +1,31 @@
+'use strict';
+
 describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
   test('should be declared', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
+  });
+
+  test('should correctly calculate the age', () => {
+    const actual = getHumanAge(89, 89);
+
+    expect(actual)
+      .toEqual([18, 15]);
+  });
+
+  test('should return a number of whole years', () => {
+    const actual = getHumanAge(16.5, 16.5);
+
+    expect(actual)
+      .toEqual([1, 1]);
+  });
+
+  test('should return 0 if pet\'s age is less than 15 years', () => {
+    const actual = getHumanAge(9, 9);
+
+    expect(actual)
+      .toEqual([0, 0]);
   });
 });
