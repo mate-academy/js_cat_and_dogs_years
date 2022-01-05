4aee278559b43236e8ccb6ad9211d298af650adb
Bohdan Bandura <bohdanbandura@MacBook-Pro-Bohdan.local>
2022-01-05 14:41:30 +0200

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,8 +1,45 @@
+'use strict';
+
 describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
   test('should be declared', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
+  });
+
+  test('should be declared', () => {
+    expect(getHumanAge)
+      .toBeInstanceOf(Function);
+  });
+
+  test('Should return 0 human ages for both', () => {
+    expect(getHumanAge(14, 14))
+      .toEqual([0, 0]);
+  });
+
+  test('Should return 1 human ages for both', () => {
+    expect(getHumanAge(15, 15))
+      .toEqual([1, 1]);
+  });
+
+  test('Should return 2 human ages for both', () => {
+    expect(getHumanAge(24, 24))
+      .toEqual([2, 2]);
+  });
+
+  test('Should return 3 human ages for cat and 2 human ages for dog', () => {
+    expect(getHumanAge(28, 28))
+      .toEqual([3, 2]);
+  });
+
+  test('Should return 3 human ages for both', () => {
+    expect(getHumanAge(28, 29))
+      .toEqual([3, 3]);
+  });
+
+  test('Should return 21 human ages for cat and 17 human ages for dog', () => {
+    expect(getHumanAge(100, 100))
+      .toEqual([21, 17]);
   });
 });
