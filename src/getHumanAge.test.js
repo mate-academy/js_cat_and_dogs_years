f4bbea7d0319aad75d8f826fb03200de943c0da7
KHTRE <KHTREE@gmail.com>
2022-01-06 18:30:41 +0200

    works

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,8 +1,31 @@
+'use strict';
+
 describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
   test('should be declared', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
+  });
+
+  test('should return [0, 0] if both parameters = 0', () => {
+    const actual = getHumanAge(0, 0);
+    const expected = [0, 0];
+
+    expect(actual)
+      .toStrictEqual(expected);
+  });
+
+  test('should work with valid parameters', () => {
+    const actual = getHumanAge(28, 29);
+    const expected = [3, 3];
+
+    expect(actual)
+      .toStrictEqual(expected);
+  });
+
+  // I know that this test is basically useless, I just wanted to try it.
+  test('should return an array', () => {
+    expect(Array.isArray(getHumanAge())).toBe(true);
   });
 });
