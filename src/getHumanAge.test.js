d7217e28ac3ed0397057c6691144b146ce51a098
Victoriya Tsaruk <tsaruk.work21212@gmail.com>
2022-01-16 15:21:11 +0200

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,47 +8,47 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test('should return 0 if catAge && dogAge equal 0', () => {
+  test('function should return [0,0] if catAge = 0, dogAge = 0', () => {
     expect(getHumanAge(0, 0))
       .toEqual([0, 0]);
   });
 
-  test('should return 0 when input is 14 for both values', () => {
+  test('function should return [0,0] if catAge = 14, dogAge = 14', () => {
     expect(getHumanAge(14, 14))
       .toEqual([0, 0]);
   });
 
-  test('should return 1 when input is 15 for both values', () => {
+  test('fucntion should return [1,1] if catAge = 15, dogAge = 15', () => {
     expect(getHumanAge(15, 15))
       .toEqual([1, 1]);
   });
 
-  test('should return 1 when input is 19 for both values', () => {
+  test('fucntion should return [1,1] if catAge = 19, dogAge = 19', () => {
     expect(getHumanAge(19, 19))
       .toEqual([1, 1]);
   });
 
-  test('should add 1 human year to the result when input is 24', () => {
+  test('function should return [2,2] if catAge = 24, dogAge = 24', () => {
     expect(getHumanAge(24, 24))
       .toEqual([2, 2]);
   });
 
-  test('should add 1 human year to the result when input is 27', () => {
+  test('fucntion should return [2,2] if catAge = 24, dogAge = 24', () => {
     expect(getHumanAge(24, 24))
       .toEqual([2, 2]);
   });
 
-  test('should add 1 extra human year when input is 28 for catAge', () => {
+  test('fucntion should return [3,2] if catAge = 28, dogAge = 28', () => {
     expect(getHumanAge(28, 28))
       .toEqual([3, 2]);
   });
 
-  test('should add 1 extra human year when input is 29 for dogAge', () => {
+  test('fucntion should return [3,3] if catAge = 29, dogAge = 29', () => {
     expect(getHumanAge(29, 29))
       .toEqual([3, 3]);
   });
 
-  test('should round the value if the age is not integer', () => {
+  test('fucntion should return [11,9] if catAge = 60, dogAge = 60', () => {
     expect(getHumanAge(60, 60))
       .toEqual([11, 9]);
   });
