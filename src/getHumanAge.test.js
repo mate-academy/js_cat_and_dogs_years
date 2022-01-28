32085c8ff071432a83d303c5080c028abecc6926
liudmyla.romankova <liudmyla.romankova@pmi.bet>
2022-01-28 13:42:54 +0200

    fix mistakes

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -18,22 +18,22 @@ describe('getHumanAge', () => {
       .toEqual([0, 0]);
   });
 
-  test('should return 0 for inputs < 15', () => {
+  test('should return [0, 0] for inputs < 15', () => {
     expect(getHumanAge(14, 5))
       .toEqual([0, 0]);
   });
 
-  test('should return 1 for inputs equal 15', () => {
+  test('should return [1, 1] for inputs equal 15', () => {
     expect(getHumanAge(15, 15))
       .toEqual([1, 1]);
   });
 
-  test('should return 1 for inputs >=15 and < 24', () => {
+  test('should return [1, 1] for inputs >=15 and < 24', () => {
     expect(getHumanAge(19, 23))
       .toEqual([1, 1]);
   });
 
-  test('should return 2 for inputs equal 24', () => {
+  test('should return [2, 2] for inputs equal 24', () => {
     expect(getHumanAge(24, 24))
       .toEqual([2, 2]);
   });
