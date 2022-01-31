0cdf5e03173d5bc0d1d4fd3ceb51d176c19daa6d
Vsevolod Maslak <vsevolodmaslak@gmail.com>
2022-01-31 14:40:55 +0200

    changedNamesOfTests

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 'use strict';
 
 describe('getHumanAge', () => {
@@ -8,28 +9,28 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test('when input 0 values, must return 0', () => {
+  test('should return [0, 0] when cat and dogs years are equal to 0', () => {
     const result = getHumanAge(0, 0);
 
     expect(result)
       .toEqual([0, 0]);
   });
 
-  test('when input 15 values, must return 1', () => {
+  test('should return [1, 1] when cat and dogs years are equal to 15', () => {
     const result = getHumanAge(15, 15);
 
     expect(result)
       .toEqual([1, 1]);
   });
 
-  test('when input 24 values, must return 2', () => {
+  test('should return [2, 2] when cat and dogs years are equal to 24', () => {
     const result = getHumanAge(24, 24);
 
     expect(result)
       .toEqual([2, 2]);
   });
 
-  test('when input 100 values, must return [21, 17]', () => {
+  test('should return [21, 17] when cat and dogs years are equal to 100', () => {
     const result = getHumanAge(100, 100);
 
     expect(result)
