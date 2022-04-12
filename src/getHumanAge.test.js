f46da1910d6d05b3764d068e2d336d0ec4b58641
Larysa Khomenko <larisa.iovchu@gmail.com>
2022-04-12 17:27:14 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,61 +1,61 @@
 'use strict';
 
-describe('getHumanAge', () => {
+describe(`getHumanAge`, () => {
   const { getHumanAge } = require('./getHumanAge');
 
-  test('should be declared', () => {
+  test(`should be declared`, () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
 
-  it('should return an array', () => {
+  it(`should return an array`, () => {
     expect(getHumanAge())
       .toBeInstanceOf(Array);
   });
 
-  it('should return an array with 2 elements', () => {
+  it(`should return an array with 2 elements`, () => {
     expect(getHumanAge(0, 0))
       .toHaveLength(2);
   });
 
-  it(`'should return an array of 2 zero
-   if catAge & dogAge less than 15'`, () => {
+  it(`should return an array of 2 zero
+   if catAge & dogAge less than 15`, () => {
     expect(getHumanAge(14, 14))
       .toEqual([0, 0]);
   });
 
-  it(`'should return an array of 2 units
-   if catAge & dogAge equal to 15'`, () => {
+  it(`should return an array of 2 units
+   if catAge & dogAge equal to 15`, () => {
     expect(getHumanAge(15, 15))
       .toEqual([1, 1]);
   });
 
-  it(`'should return an array of 2 units
-   if catAge & dogAge less than 24'`, () => {
+  it(`should return an array of 2 units
+   if catAge & dogAge less than 24`, () => {
     expect(getHumanAge(23, 23))
       .toEqual([1, 1]);
   });
 
-  it(`'should return an array of 2 deuces
-   if catAge & dogAge equal to 24'`, () => {
+  it(`should return an array of 2 deuces
+   if catAge & dogAge equal to 24`, () => {
     expect(getHumanAge(24, 24))
       .toEqual([2, 2]);
   });
 
-  it(`'should return an array of 2 deuces
-   if catAge less than 28 & dogAge less than 29'`, () => {
+  it(`should return an array of 2 deuces
+   if catAge less than 28 & dogAge less than 29`, () => {
     expect(getHumanAge(27, 28))
       .toEqual([2, 2]);
   });
 
-  it(`'should return an array of 2 deuces
-   if catAge equal to 28 & dogAge equal to 29'`, () => {
+  it(`should return an array of 2 deuces
+   if catAge equal to 28 & dogAge equal to 29`, () => {
     expect(getHumanAge(28, 29))
       .toEqual([3, 3]);
   });
 
-  it(`'should return an array with elements 6 & 5
-   if catAge & dogAge equal to 43'`, () => {
+  it(`should return an array with elements 6 & 5
+   if catAge & dogAge equal to 43`, () => {
     expect(getHumanAge(43, 43))
       .toEqual([6, 5]);
   });
