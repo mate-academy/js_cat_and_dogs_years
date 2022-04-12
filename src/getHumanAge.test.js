8909aa9a2a1e60d31626ac9f1e2ac64dd5a0a8c4
Olha <dia12vanessa@gmail.com>
2022-04-12 19:06:15 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,57 +8,55 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test('should return 0 for both if input values equals 0', () => {
+  test('should return [0, 0] if input values are equal to 0', () => {
     const result = getHumanAge(0, 0);
 
     expect(result).toEqual([0, 0]);
   });
 
-  test('should return 0 for both if input values < 15', () => {
+  test('should return [0, 0] if input values < 15', () => {
     const result = getHumanAge(14, 14);
 
     expect(result).toEqual([0, 0]);
   });
 
-  test('should return 1 for both if input values equals 15 ', () => {
+  test('should return [1, 1] if input values are equal to 15 ', () => {
     const result = getHumanAge(15, 15);
 
     expect(result).toEqual([1, 1]);
   });
 
-  test('should return 1 for both if input values equals 23', () => {
+  test('should return [1, 1] if input values are equal to 23', () => {
     const result = getHumanAge(23, 23);
 
     expect(result).toEqual([1, 1]);
   });
 
-  test('should return 2 for both if input values equals 24', () => {
+  test('should return [2, 2] if input values are equal to 24', () => {
     const result = getHumanAge(24, 24);
 
     expect(result).toEqual([2, 2]);
   });
 
-  test('should return 2 for bothif input values equals 27', () => {
+  test('should return [2, 2] if input values are equal to 27', () => {
     const result = getHumanAge(27, 27);
 
     expect(result).toEqual([2, 2]);
   });
 
-  test('should return 3 for cats and 2 for dogs'
-  + 'if input values equals 28', () => {
+  test('should return [3, 2] if input values are equal to 28', () => {
     const result = getHumanAge(28, 28);
 
     expect(result).toEqual([3, 2]);
   });
 
-  test('should return 3 for both if input values equals 29', () => {
+  test('should return [3, 3] if input values are equal to 29', () => {
     const result = getHumanAge(29, 29);
 
     expect(result).toEqual([3, 3]);
   });
 
-  test('should return 21 for cats and 17 for dogs'
-  + 'if input values equals 100', () => {
+  test('should return [21, 17] if input values are equal to 100', () => {
     const result = getHumanAge(100, 100);
 
     expect(result).toEqual([21, 17]);
