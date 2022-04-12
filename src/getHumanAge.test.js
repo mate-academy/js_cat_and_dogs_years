19df1dd124d0b4312e10caca454f1963a83c49e3
Roman Stas <romastas1996@gmail.com>
2022-04-12 13:50:01 +0300

    Solution test

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,31 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return an array with 2 elem', () => {
+    expect(getHumanAge(1, 5))
+      .toHaveLength(2);
+  });
+
+  test(`should return an the array where the first element is
+ my cat's age in **human** years`, () => {
+    expect(getHumanAge(27, 0))
+      .toEqual([2, 0]);
+  });
+
+  test(`should return an the array where the second element
+  is my dog's age in **human** years`, () => {
+    expect(getHumanAge(0, 28))
+      .toEqual([0, 2]);
+  });
+
+  test(`should return 0 if the years are 0 `, () => {
+    expect(getHumanAge(0, 0))
+      .toEqual([0, 0]);
+  });
+
+  test(`should add NaN to result if No value specified`, () => {
+    expect(getHumanAge())
+      .toEqual([NaN, NaN]);
+  });
 });
