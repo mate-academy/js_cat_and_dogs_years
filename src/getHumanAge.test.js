dc3042ebbf1820f893ddfcd26cacc9a30474be22
Oleg Lukyanchuk <oleeluk@gmail.com>
2022-04-14 00:47:43 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,54 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return an Array', () => {
+    expect(getHumanAge(14, 14))
+      .toBeInstanceOf(Array);
+  });
+
+  test('should return an Array of given length', () => {
+    expect(getHumanAge(14, 14).length)
+      .toBe(2);
+  });
+
+  test('should return [0, 0] if input (0, 0)', () => {
+    expect(getHumanAge(0, 0))
+      .toEqual([0, 0]);
+  });
+
+  test('should return [0, 0] if input (14, 14)', () => {
+    expect(getHumanAge(14, 14))
+      .toEqual([0, 0]);
+  });
+
+  test('should return [1, 1] if input (15, 15)', () => {
+    expect(getHumanAge(15, 15))
+      .toEqual([1, 1]);
+  });
+
+  test('should return [1, 1] if input (23, 23)', () => {
+    expect(getHumanAge(23, 23))
+      .toEqual([1, 1]);
+  });
+
+  test('should return [2, 2] if input (24, 24)', () => {
+    expect(getHumanAge(24, 24))
+      .toEqual([2, 2]);
+  });
+
+  test('should return [2, 2] if input (27, 27)', () => {
+    expect(getHumanAge(27, 27))
+      .toEqual([2, 2]);
+  });
+
+  test('should return [3, 2] if input (28, 28)', () => {
+    expect(getHumanAge(28, 28))
+      .toEqual([3, 2]);
+  });
+
+  test('should return [21, 17] if input (100, 100)', () => {
+    expect(getHumanAge(100, 100))
+      .toEqual([21, 17]);
+  });
 });
