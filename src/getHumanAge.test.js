b568901708ed1d1617a9cf3b1879c3be11d42129
Yana <alekseenko0815@gmail.com>
2022-04-13 09:48:01 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -12,16 +12,16 @@ describe('getHumanAge', () => {
     expect(getHumanAge()).toBeInstanceOf(Array);
   });
 
-  it(`should retern an array with 2 values`, () => {
+  it(`should return an array with 2 values`, () => {
     expect(getHumanAge(9, 2)).toHaveLength(2);
   });
 
-  it(`should retern [0, 0] if animal's 
+  it(`should return [0, 0] if animal's 
   age is less than 15 not inclusive`, () => {
     expect(getHumanAge(10, 10)).toEqual([0, 0]);
   });
 
-  it(`should retern 1 human year if animal's 
+  it(`should return 1 human year if animal's 
   age is between 15 and 23 years inclusive`, () => {
     expect(getHumanAge(15, 23)).toEqual([1, 1]);
   });
