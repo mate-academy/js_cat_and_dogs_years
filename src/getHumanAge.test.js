66d00780055b56b14a7b89ec1aee6a6dbc833981
claimsecond <claim.second@gmail.com>
2022-05-24 23:23:36 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,48 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('getHumanAge(0, 0) === [0, 0]', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('getHumanAge(0, 0) === [0, 0]', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test('getHumanAge(0, 0) === [0, 0]', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  test('getHumanAge(0, 0) === [0, 0]', () => {
+    expect(getHumanAge(16, 16)).toEqual([1, 1]);
+  });
+
+  test('getHumanAge(0, 0) === [0, 0]', () => {
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
+  test('getHumanAge(0, 0) === [0, 0]', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+
+  test('getHumanAge(0, 0) === [0, 0]', () => {
+    expect(getHumanAge(25, 25)).toEqual([2, 2]);
+  });
+
+  test('getHumanAge(0, 0) === [0, 0]', () => {
+    expect(getHumanAge(27, 28)).toEqual([2, 2]);
+  });
+
+  test('getHumanAge(0, 0) === [0, 0]', () => {
+    expect(getHumanAge(28, 29)).toEqual([3, 3]);
+  });
+
+  test('getHumanAge(0, 0) === [0, 0]', () => {
+    expect(getHumanAge(29, 30)).toEqual([3, 3]);
+  });
+
+  test('getHumanAge(0, 0) === [0, 0]', () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
 });
