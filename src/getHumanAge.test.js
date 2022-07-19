08f036709be795f2a1b1a89bbb7d3d7e1d347c69
Anna Sobol <anettesobol@gmail.com>
2022-07-19 10:42:02 +0200

    Done

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,27 +8,36 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test('should return an array [0, 0] values if ages are 0', () => {
+  test('should return an object', () => {
+    expect(typeof getHumanAge(0,0))
+      .toBe('object');
+  });
+
+  test('should return values [0,0] with unspecified arguments', () => {
+    expect(getHumanAge()).toEqual([0,0])
+  });
+
+  test('should return [0, 0] values if ages are 0', () => {
     expect(getHumanAge(0, 0)).toEqual([0, 0]);
   });
 
-  test('should return an array [0, 0] values if ages are 14', () => {
-    expect(getHumanAge(10, 9)).toEqual([0, 0]);
+  test('should return [0, 0] values if ages are 14', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
-  test('should return an array with values [1, 1] if ages are 15', () => {
+  test('should return [1, 1] values if ages are 15', () => {
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
   });
 
-  test('should return an array with values [1, 1] if ages are 23', () => {
+  test('should return [1, 1] values if ages are 23', () => {
     expect(getHumanAge(23, 23)).toEqual([1, 1]);
   });
 
-  test('should return an array with values [2, 2] if ages are 24', () => {
+  test('should return [2, 2] values  if ages are 24', () => {
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
 
-  test('should return an array with values [2, 2] if ages are 27', () => {
+  test('should return [2, 2] values if ages are 27', () => {
     expect(getHumanAge(27, 27)).toEqual([2, 2]);
   });
 
