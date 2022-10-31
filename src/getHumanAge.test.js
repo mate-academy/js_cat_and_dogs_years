4ae0fb6752c2d4c9ac41381d9a455e65765172df
NikitaMinchenko <nikitaminchenko30@gmail.com>
2022-10-31 21:01:19 +0200

    Some fixes were made in the solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -13,44 +13,43 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Array)
   });
 
+  test('Values should be numeric',()=> {
+    expect(getHumanAge('a','b')).toEqual([NaN,NaN])
+  });
+
   test('Function should return a [0,0] result if both dogs and cats years are 0',()=> {
     const goals = getHumanAge(0,0)
-    expect(getHumanAge(0,0)).toEqual([0,0])
+    expect(goals).toEqual([0,0])
   });
 
   test('Function should return a [0,0] result if both dogs and cats years are below 15',()=> {
-    const goals = getHumanAge(0,0)
-    expect(getHumanAge(14,14)).toEqual([0,0])
+    const goals = getHumanAge(14,14)
+    expect(goals).toEqual([0,0])
   });
 
   test('First 15 years of both cats and dogs counts as 1 human year',()=> {
-    const goals = getHumanAge(0,0)
-    expect(getHumanAge(15,15)).toEqual([1,1])
+    const goals = getHumanAge(15,15)
+    expect(goals).toEqual([1,1])
   });
 
   test('Years from 15-23 of both cats and dogs counts as 1 human year',()=> {
-    const goals = getHumanAge(0,0)
-    expect(getHumanAge(23,23)).toEqual([1,1])
-  });
-
-  test('Years from 15-23 of both cats and dogs counts as 1 human year',()=> {
-    const goals = getHumanAge(0,0)
-    expect(getHumanAge(23,23)).toEqual([1,1])
+    const goals = getHumanAge(23,23)
+    expect(goals).toEqual([1,1])
   });
 
   test('Every 4 years after 24 counts as 1 human year for cats ',()=> {
-    const goals = getHumanAge(0,0)
-    expect(getHumanAge(28,28)).toEqual([3,2])
+    const goals = getHumanAge(28,28)
+    expect(goals).toEqual([3,2])
   });
 
   test('Every 5 years after 24 counts as 1 human year for dogs ',()=> {
-    const goals = getHumanAge(0,0)
-    expect(getHumanAge(28,29)).toEqual([3,3])
+    const goals = getHumanAge(28,29)
+    expect(goals).toEqual([3,3])
   });
 
   test('Float values ​​should be rounded down',()=> {
-    const goals = getHumanAge(0,0)
-    expect(getHumanAge(15.5,15,5)).toEqual([1,1])
+    const goals = getHumanAge(15.5,15.5)
+    expect(goals).toEqual([1,1])
   });
 
 });
