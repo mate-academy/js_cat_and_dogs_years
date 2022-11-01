e2c82fbef7b82beaae2a55531111d226fc7c68e5
Yurii Levchenko <103377396+yuriiLevchenko2022@users.noreply.github.com>
2022-11-01 08:18:29 +0200

    Update getHumanAge.test.js

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -24,13 +24,6 @@ describe('getHumanAge', () => {
     expect(getHumanAge(23, 23)).toEqual([1, 1]);
   });
 
-  test('if a cat and dog are 15 <= years < 24', () => {
-    expect(getHumanAge(15, 15)).toEqual([1, 1]);
-    expect(getHumanAge(16, 16)).toEqual([1, 1]);
-    expect(getHumanAge(17, 22)).toEqual([1, 1]);
-    expect(getHumanAge(23, 23)).toEqual([1, 1]);
-  });
-
   test('if a cat and dog are 24 <= years', () => {
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
     expect(getHumanAge(25, 25)).toEqual([2, 2]);
