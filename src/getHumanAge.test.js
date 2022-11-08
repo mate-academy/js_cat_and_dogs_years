28c1fb1a285e159e6007269f0d0d2c842b254926
Marina TORONYI <marina.toronyi>
2022-11-08 17:30:02 +0100

    Solutin after fixing

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -14,17 +14,26 @@ describe('getHumanAge', () => {
     expect(goals).toBeInstanceOf(Array);
   }); /* normaly this test is not necessary because of next test */
 
-  test('should return an expected HumanAge', () => {
-    const goals = getHumanAge(15, 15);
+  test('should return an expected HumanAge when cat&dog have 0 <= years < 15'
+    , () => {
+      const goals = getHumanAge(0, 14);
 
-    expect(goals).toEqual([1, 1]);
-  });
+      expect(goals).toEqual([0, 0]);
+    });
 
-  test('shoul return [0, 0], if cat&dog have 0 <= year < 1', () => {
-    const goals = getHumanAge(0, 0);
+  test('should return an expected HumanAge when cat&dog have 15 <= years < 24'
+    , () => {
+      const goals = getHumanAge(15, 23);
 
-    expect(goals).toEqual([0, 0]);
-  });
+      expect(goals).toEqual([1, 1]);
+    });
+
+  test('should return an expected HumanAge when cat&dog have 24 <= years'
+    , () => {
+      const goals = getHumanAge(24, 99);
+
+      expect(goals).toEqual([2, 17]);
+    });
 
   test('should return [0,0] if two inegers (catAge&dogAge) are < 0', () => {
     const goals = getHumanAge(-27, -27);
