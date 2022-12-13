f427d48a788dcf733a03972e35bee3fab76a4c64
Vlad Didkovskiy <vladgreen.vg@gmail.com>
2022-12-13 22:47:16 +0200

    getHumanAge

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,29 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('Human years should be 0 if animal year < 15', () => {
+    expect(getHumanAge(14, 14))
+      .toEqual([0, 0]);
+  });
+
+  test('First 15 animal years give 1 human year', () => {
+    expect(getHumanAge(15, 15))
+      .toEqual([1, 1]);
+  });
+
+  test('The next 9 animal years give 1 more human year', () => {
+    expect(getHumanAge(24, 24))
+      .toEqual([2, 2]);
+  });
+
+  test('Every 4 next cat years give 1 extra human year', () => {
+    expect(getHumanAge(28, 28))
+      .toEqual([3, 2]);
+  });
+
+  test('Every 5 next dog years give 1 extra human year', () => {
+    expect(getHumanAge(27, 29))
+      .toEqual([2, 3]);
+  });
 });
