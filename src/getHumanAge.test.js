0940ffe3c30c7d6d3a88679aec9b861fdf274b80
Yuliia Sokolova <yulyasokolova.lvov@gmail.com>
2022-12-24 21:57:11 +0200

    Fixes after review

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,62 +8,67 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test('should return animals age in human years if values are 0', () => {
+  test('should check 0 year', () => {
     expect(getHumanAge(0, 0))
       .toEqual([0, 0]);
   });
 
-  test('should return animals age in human years if values fall at the beginning of 1 range', () => {
+  test('should check less than 14 years (in the middle of the range)', () => {
+    expect(getHumanAge(6, 6))
+      .toEqual([0, 0]);
+  });
+
+  test('should check 14 years (at the end of the range)', () => {
     expect(getHumanAge(14, 14))
       .toEqual([0, 0]);
   });
 
-  test('should return animals age in human years if values fall at the end of 1 range', () => {
-    expect(getHumanAge(14, 14))
-      .toEqual([0, 0]);
-  });
-
-  test('should return animals age in human years if values fall at the beginning of 2 range', () => {
+  test('should check 15 years (at the beginning of the range)', () => {
     expect(getHumanAge(15, 15))
       .toEqual([1, 1]);
   });
 
-  test('should return animals age in human years if values fall at the gend of 2 range', () => {
+  test('should check between 15 and 23 years (in the middle of the range)', () => {
+    expect(getHumanAge(20, 20))
+      .toEqual([1, 1]);
+  });
+
+  test('should check 23 years (at the end of the range)', () => {
     expect(getHumanAge(23, 23))
       .toEqual([1, 1]);
   });
 
-  test('should return animals age in human years if values fall at the beginning of 3 range', () => {
+  test('should check 24 years (at the beginning of the range)', () => {
     expect(getHumanAge(24, 24))
       .toEqual([2, 2]);
   });
 
-  test('should return animals age in human years if values fall at the end of 3 range', () => {
+  test('should check 27 and 28 years (at the end of two ranges)', () => {
     expect(getHumanAge(27, 28))
       .toEqual([2, 2]);
   });
 
-  test('should return animals age in human years if values fall at the beginning of 4 range', () => {
+  test('should check 28 and 29 years (at the beginning of two ranges)', () => {
     expect(getHumanAge(28, 29))
       .toEqual([3, 3]);
   });
 
-  test('should return animals age in human years if values fall at the end of 4 range', () => {
+  test('should check 31 and 33 years (at the end of two ranges)', () => {
     expect(getHumanAge(31, 33))
       .toEqual([3, 3]);
   });
 
-  test('should return animals age in human years if values fall at the beginning of 10 range', () => {
+  test('should check 56 and 64 years (at the beginning of two ranges)', () => {
     expect(getHumanAge(56, 64))
       .toEqual([10, 10]);
   });
 
-  test('should return animals age in human years if values fall at the end of 10 range', () => {
+  test('should check 59 and 68 years (at the end of two ranges)', () => {
     expect(getHumanAge(59, 68))
       .toEqual([10, 10]);
   });
 
-  test('should return animals age in human years if values are in different ranges', () => {
+  test('should check between 28 and 31 years & between 49 and 53 years (in different ranges)', () => {
     expect(getHumanAge(30, 50))
       .toEqual([3, 7]);
   });
