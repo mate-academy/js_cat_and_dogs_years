ea822eaedadc913ca6cda3e738e8072801afdb73
Maryna Oznobishinaoznobishina.ok@gmail.comgit config --global user.name Maryna <oznobishina.ok@gmail.com>
2023-01-07 14:00:41 +0200

    first

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -15,7 +15,7 @@ describe('getHumanAge', () => {
     expect(getHumanAge(23, 23)).toEqual([1, 1]);
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
     expect(getHumanAge(27, 27)).toEqual([2, 2]);
-    // expect(getHumanAge(28, 28)).toEqual([3, 2]);
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
     expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
