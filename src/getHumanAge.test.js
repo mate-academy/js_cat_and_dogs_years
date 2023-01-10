b23e4944476404efea2a7c2ed5d3b4edd9e6922e
Andrii Dydyk <andriidydyk622@gmail.com>
2023-01-10 21:53:10 +0100

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,67 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return [0, 0] if arguments equal (0, 0)', () => {
+    const petsAge = getHumanAge(0, 0);
+
+    expect(petsAge)
+      .toEqual([0, 0]);
+  });
+
+  test('should return [0, 0] if arguments equal (14, 14)', () => {
+    const petsAge = getHumanAge(14, 14);
+
+    expect(petsAge)
+      .toEqual([0, 0]);
+  });
+
+  test('should return [1, 1] if arguments equal (15, 15)', () => {
+    const petsAge = getHumanAge(15, 15);
+
+    expect(petsAge)
+      .toEqual([1, 1]);
+  });
+
+  test('should return [1, 1] if arguments equal (23, 23)', () => {
+    const petsAge = getHumanAge(23, 23);
+
+    expect(petsAge)
+      .toEqual([1, 1]);
+  });
+
+  test('should return [2, 2] if arguments equal (24, 24)', () => {
+    const petsAge = getHumanAge(24, 24);
+
+    expect(petsAge)
+      .toEqual([2, 2]);
+  });
+
+  test('should return [2, 2] if arguments equal (27, 27)', () => {
+    const petsAge = getHumanAge(27, 27);
+
+    expect(petsAge)
+      .toEqual([2, 2]);
+  });
+
+  test('should return [3, 2] if arguments equal (28, 28)', () => {
+    const petsAge = getHumanAge(28, 28);
+
+    expect(petsAge)
+      .toEqual([3, 2]);
+  });
+
+  test('should return [3, 3] if arguments equal (28, 28)', () => {
+    const petsAge = getHumanAge(28, 29);
+
+    expect(petsAge)
+      .toEqual([3, 3]);
+  });
+
+  test('should return [3, 3] if arguments equal (100, 100)', () => {
+    const petsAge = getHumanAge(100, 100);
+
+    expect(petsAge)
+      .toEqual([21, 17]);
+  });
 });
