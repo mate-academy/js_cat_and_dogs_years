2216bc8c4d7a7cc02f0c734f4042e5b7bac9fdc6
Maryna Oznobishinaoznobishina.ok@gmail.comgit config --global user.name Maryna <oznobishina.ok@gmail.com>
2023-01-12 15:39:07 +0200

    Solution v2

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -10,12 +10,34 @@ describe('getHumanAge', () => {
 
   test('should return human age for cat and dog age', () => {
     expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('should return human age for cat and dog age', () => {
     expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test('should return human age for cat and dog age', () => {
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  test('should return human age for cat and dog age', () => {
     expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
+  test('should return human age for cat and dog age', () => {
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+
+  test('should return human age for cat and dog age', () => {
     expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
+
+  test('should return human age for cat and dog age', () => {
     expect(getHumanAge(28, 28)).toEqual([3, 2]);
+  });
+
+  test('should return human age for cat and dog age', () => {
     expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
+
 });
