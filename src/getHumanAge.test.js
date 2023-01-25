bcd59bbc88e8961edee9543488aa8707beebb4f5
Oleksandr Malyshev <sanyokmalyshev@gmail.com>
2023-01-25 12:07:22 +0700

    Add changes

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -13,30 +13,38 @@ describe('getHumanAge', () => {
       .toEqual([0, 0]);
   });
 
-  test('should return Array of 0, if catAge, dogAge lestt than 15', () => {
+  test('should return Array of 0, if catAge, dogAge are less than 15', () => {
     expect(getHumanAge(14, 14))
       .toEqual([0, 0]);
   });
 
-  test('should return expected array, if catAge, dogAge greter 14, less 24',
-    () => {
-      expect(getHumanAge(15, 15))
-        .toEqual([1, 1]);
-    });
+  test('should return expected array, if catAge, '
+  + 'dogAge are greater than 14, less 24',
+  () => {
+    expect(getHumanAge(15, 15))
+      .toEqual([1, 1]);
+  });
 
-  test('should return expected array, if catAge, dogAge greter 23, less 28',
-    () => {
-      expect(getHumanAge(24, 24))
-        .toEqual([2, 2]);
-    });
+  test('should return expected array, if catAge, '
+  + 'dogAge are greater 23, less 28',
+  () => {
+    expect(getHumanAge(24, 24))
+      .toEqual([2, 2]);
+  });
 
-  test('should return expected array, if catAge greater 27',
+  test('should return expected array, if catAge > 27',
     () => {
       expect(getHumanAge(28, 28))
         .toEqual([3, 2]);
     });
 
-  test('should return expected array', () => {
+  test('should return expected array, if dogAge > 28',
+    () => {
+      expect(getHumanAge(28, 30))
+        .toEqual([3, 3]);
+    });
+
+  test('should return expected array, if catAge > 27, dogAge > 28', () => {
     expect(getHumanAge(100, 100))
       .toEqual([21, 17]);
   });
