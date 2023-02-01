6427325a99fb64bd393e917d4ec1d8b0ac7e3caf
Khrystyna Sabat <satttinka@gmail.com>
2023-02-01 23:45:45 +0100

    solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,29 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return zero if the value is 0 or less than 15', () => {
+    expect(getHumanAge(0, 13))
+      .toEqual([0, 0]);
+  });
+
+  test('should return different results for different values', () => {
+    expect(getHumanAge(14, 15))
+      .toEqual([0, 1]);
+  });
+
+  test('should return correct results for values 23 and 24', () => {
+    expect(getHumanAge(23, 24))
+      .toEqual([1, 2]);
+  });
+
+  test('should return different results if both values are 28', () => {
+    expect(getHumanAge(28, 28))
+      .toEqual([3, 2]);
+  });
+
+  test('should return correct results, if catAge > 27, dogAge > 28', () => {
+    expect(getHumanAge(100, 100))
+      .toEqual([21, 17]);
+  });
 });
