90caaca491ab79e461b7297f5a6c74823bc7be93
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2023-02-07 15:26:35 +0200

    solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -4,7 +4,22 @@ describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
   test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+    expect(getHumanAge).toBeInstanceOf(Function);
+  });
+
+  test('should calculate age with valid data', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test('should calculate age with folat numbers', () => {
+    expect(getHumanAge(15.3, 27.4)).toEqual([1, 2]);
+  });
+
+  test('should handle case with negative numbers', () => {
+    expect(getHumanAge(-1, 0)).toEqual([0, 0]);
+  });
+
+  test('should handle infinity case', () => {
+    expect(getHumanAge(Infinity, Infinity)).toEqual([Infinity, Infinity]);
   });
 });
