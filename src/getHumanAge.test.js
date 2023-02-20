a722fbc08df888d92369e366df8f31088b27fa72
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-02-20 22:26:26 +0200

    solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -9,92 +9,97 @@ describe('getHumanAge', () => {
   });
 
   // getHumanAge(0, 0) === [0, 0]
-  test('zeros return array of zeros', () => {
+  test('should return array of zeros for zeros values', () => {
     const catAge = 0;
     const dogAge = 0;
     const array = getHumanAge(catAge, dogAge);
 
     const isZeroActualCatAge = array[0] === 0;
     const isZeroActualDogAge = array[1] === 0;
-    const expected = true;
 
     expect(isZeroActualCatAge && isZeroActualDogAge)
-      .toBe(expected);
+      .toBeTruthy();
   });
 
   // getHumanAge(14, 14) === [0, 0]
-  test('values before first human year return array of zeros', () => {
-    const catAge = 14;
-    const dogAge = 14;
-    const array = getHumanAge(catAge, dogAge);
+  test(
+    'should return array of zeros for values before first human year',
+    () => {
+      const catAge = 14;
+      const dogAge = 14;
+      const array = getHumanAge(catAge, dogAge);
 
-    const isZeroActualCatAge = array[0] === 0;
-    const isZeroActualDogAge = array[1] === 0;
-    const expected = true;
+      const isZeroActualCatAge = array[0] === 0;
+      const isZeroActualDogAge = array[1] === 0;
 
-    expect(isZeroActualCatAge && isZeroActualDogAge)
-      .toBe(expected);
-  });
+      expect(isZeroActualCatAge && isZeroActualDogAge)
+        .toBeTruthy();
+    });
 
   // getHumanAge(15, 15) === [1, 1]
-  test('values equal first human year return array of 1', () => {
-    const catAge = 15;
-    const dogAge = 15;
-    const array = getHumanAge(catAge, dogAge);
+  test(
+    'should return array of 1 for values equal first human',
+    () => {
+      const catAge = 15;
+      const dogAge = 15;
+      const array = getHumanAge(catAge, dogAge);
 
-    const isZeroActualCatAge = array[0] === 1;
-    const isZeroActualDogAge = array[1] === 1;
-    const expected = true;
+      const isZeroActualCatAge = array[0] === 1;
+      const isZeroActualDogAge = array[1] === 1;
 
-    expect(isZeroActualCatAge && isZeroActualDogAge)
-      .toBe(expected);
-  });
+      expect(isZeroActualCatAge && isZeroActualDogAge)
+        .toBeTruthy();
+    });
 
   // getHumanAge(23, 23) === [1, 1]
-  test('values before second human year return array of 1', () => {
-    const catAge = 23;
-    const dogAge = 23;
-    const array = getHumanAge(catAge, dogAge);
+  test(
+    'should return array of 1 for values before second human year',
+    () => {
+      const catAge = 23;
+      const dogAge = 23;
+      const array = getHumanAge(catAge, dogAge);
 
-    const isZeroActualCatAge = array[0] === 1;
-    const isZeroActualDogAge = array[1] === 1;
-    const expected = true;
+      const isZeroActualCatAge = array[0] === 1;
+      const isZeroActualDogAge = array[1] === 1;
 
-    expect(isZeroActualCatAge && isZeroActualDogAge)
-      .toBe(expected);
-  });
+      expect(isZeroActualCatAge && isZeroActualDogAge)
+        .toBeTruthy();
+    });
 
   // getHumanAge(24, 24) === [2, 2]
-  test('values equal second human year return array of 2', () => {
-    const catAge = 24;
-    const dogAge = 24;
-    const array = getHumanAge(catAge, dogAge);
+  test(
+    'should return array of 2 for values equal second human',
+    () => {
+      const catAge = 24;
+      const dogAge = 24;
+      const array = getHumanAge(catAge, dogAge);
 
-    const isZeroActualCatAge = array[0] === 2;
-    const isZeroActualDogAge = array[1] === 2;
-    const expected = true;
+      const isZeroActualCatAge = array[0] === 2;
+      const isZeroActualDogAge = array[1] === 2;
 
-    expect(isZeroActualCatAge && isZeroActualDogAge)
-      .toBe(expected);
-  });
+      expect(isZeroActualCatAge && isZeroActualDogAge)
+        .toBeTruthy();
+    });
 
   // getHumanAge(27, 27) === [2, 2]
-  test('values before third human year return array of 2', () => {
-    const catAge = 27;
-    const dogAge = 27;
-    const array = getHumanAge(catAge, dogAge);
+  test(
+    'should return array of 2 for values before third human year',
+    () => {
+      const catAge = 27;
+      const dogAge = 27;
+      const array = getHumanAge(catAge, dogAge);
 
-    const isZeroActualCatAge = array[0] === 2;
-    const isZeroActualDogAge = array[1] === 2;
-    const expected = true;
+      const isZeroActualCatAge = array[0] === 2;
+      const isZeroActualDogAge = array[1] === 2;
 
-    expect(isZeroActualCatAge && isZeroActualDogAge)
-      .toBe(expected);
-  });
+      expect(isZeroActualCatAge && isZeroActualDogAge)
+        .toBeTruthy();
+    });
 
   // getHumanAge(28, 28) === [3, 2]
   test(
-    'different values before third human year return array of different values',
+    `should return array of different items for different values before third
+    human year`,
     () => {
       const catAge = 28;
       const dogAge = 28;
@@ -102,15 +107,14 @@ describe('getHumanAge', () => {
 
       const isZeroActualCatAge = array[0] === 3;
       const isZeroActualDogAge = array[1] === 2;
-      const expected = true;
 
       expect(isZeroActualCatAge && isZeroActualDogAge)
-        .toBe(expected);
+        .toBeTruthy();
     });
 
   // getHumanAge(100, 100) === [21, 17]
   test(
-    'correct calculation for big values ',
+    'should return correct calculation for big values ',
     () => {
       const catAge = 100;
       const dogAge = 100;
@@ -118,10 +122,8 @@ describe('getHumanAge', () => {
 
       const isZeroActualCatAge = array[0] === 21;
       const isZeroActualDogAge = array[1] === 17;
-      const expected = true;
 
       expect(isZeroActualCatAge && isZeroActualDogAge)
-        .toBe(expected);
+        .toBeTruthy();
     });
-
 });
