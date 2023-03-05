a1074a87a9e110a4e17a5c74f99b95d4f855f5c2
Andriianov <102180541+Andriianov@users.noreply.github.com>
2023-03-05 18:00:49 +0200

    Solution2

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,10 +1,15 @@
 'use strict';
 
-describe('getHumanAge', () => {
-  const { getHumanAge } = require('./getHumanAge');
+const { getHumanAge } = require('./getHumanAge');
 
-  test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
-  });
+test('should be declared', () => {
+  expect(getHumanAge).toBeInstanceOf(Function);
+});
+
+test('should calculate age with valid data', () => {
+  expect(getHumanAge(14, 14)).toEqual([0, 0]);
+});
+
+test('should handle infinity case', () => {
+  expect(getHumanAge(Infinity, Infinity)).toEqual([Infinity, Infinity]);
 });
