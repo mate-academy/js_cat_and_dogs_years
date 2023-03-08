16e4e9e38e9f06c6f29126ba18d2bdadecaf8d0f
Oleksii Pustovit <91pustovit91@gmail.com>
2023-03-08 17:21:22 +0200

    task solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,10 +1,35 @@
 'use strict';
 
-describe('getHumanAge', () => {
-  const { getHumanAge } = require('./getHumanAge');
+const { getHumanAge } = require('./getHumanAge');
 
-  test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
-  });
+test('should be declared', () => {
+  expect(getHumanAge).toBeInstanceOf(Function);
+});
+
+test('should return an array [0, 0] if catAge and dpgAge is 14', () => {
+  expect(getHumanAge(14, 14)).toEqual([0, 0]);
+});
+
+test('should return an array [1, 1] if catAge and dpgAge is 15', () => {
+  expect(getHumanAge(15, 15)).toEqual([1, 1]);
+});
+
+test('should return an array [1, 1] if catAge and dpgAge is 23', () => {
+  expect(getHumanAge(23, 23)).toEqual([1, 1]);
+});
+
+test('should return an array [2, 2] if catAge and dpgAge is 24', () => {
+  expect(getHumanAge(24, 24)).toEqual([2, 2]);
+});
+
+test('should return an array [2, 2] if catAge and dpgAge is 27', () => {
+  expect(getHumanAge(27, 27)).toEqual([2, 2]);
+});
+
+test('should return an array [3, 2] if catAge and dpgAge is 28', () => {
+  expect(getHumanAge(28, 28)).toEqual([3, 2]);
+});
+
+test('should return an array [21, 17] if catAge and dpgAge is 100', () => {
+  expect(getHumanAge(100, 100)).toEqual([21, 17]);
 });
