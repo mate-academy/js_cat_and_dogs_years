8ddabc9248f24bc67c3db7197d3715e670c6e059
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2023-04-07 11:26:13 +0200

    sol

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -9,8 +9,8 @@ describe('getHumanAge', () => {
   });
 
   test('should be 1 human year for first 15 yeats of cat and dog', () => {
-    expect(getHumanAge([14, 13])).toEqual([0, 0]);
-    expect(getHumanAge([15, 15])).toEqual([1, 1]);
+    expect(getHumanAge(14, 13)).toEqual([0, 0]);
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
   });
 
   test('should add 1 human year between 15 and 24 cats and dogs years', () => {
@@ -22,8 +22,8 @@ describe('getHumanAge', () => {
     `should add 1 extra year for every 4 cats and every 
     5 dogs years from 24 years old`,
     () => {
-      expect(getHumanAge([27, 27])).toEqual([2, 2]);
-      expect(getHumanAge([28, 28])).toEqual([3, 2]);
-      expect(getHumanAge([100, 10])).toEqual([21, 17]);
+      expect(getHumanAge(27, 27)).toEqual([2, 2]);
+      expect(getHumanAge(28, 28)).toEqual([3, 2]);
+      expect(getHumanAge(100, 100)).toEqual([21, 17]);
     });
 });
