4aff163853d7d9f659e82257750e4faa41abb6da
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2023-04-21 14:23:50 +0200

    fixes

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,13 +8,19 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test('should be 1 human year for first 15 yeats of cat and dog', () => {
+  test('should be 1 human year for first 15 years of cat and dog', () => {
     expect(getHumanAge(14, 13)).toEqual([0, 0]);
+  });
+
+  test('should be 1 human year for first 15 years of cat and dog', () => {
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
   });
 
   test('should add 1 human year between 15 and 24 cats and dogs years', () => {
     expect(getHumanAge(17, 17)).toEqual([1, 1]);
+  });
+
+  test('should add 1 human year between 15 and 24 cats and dogs years', () => {
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
 
@@ -23,7 +29,19 @@ describe('getHumanAge', () => {
     5 dogs years from 24 years old`,
     () => {
       expect(getHumanAge(27, 27)).toEqual([2, 2]);
+    });
+
+  test(
+    `should add 1 extra year for every 4 cats and every 
+    5 dogs years from 24 years old`,
+    () => {
       expect(getHumanAge(28, 28)).toEqual([3, 2]);
+    });
+
+  test(
+    `should add 1 extra year for every 4 cats and every 
+    5 dogs years from 24 years old`,
+    () => {
       expect(getHumanAge(100, 100)).toEqual([21, 17]);
     });
 });
