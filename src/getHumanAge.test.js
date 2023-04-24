2d173750606ce5ed5fb3831adc1c61999a65ca41
Anastasiia Zheliaskova <a.zheliaskova3@gmail.com>
2023-04-24 16:39:30 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,42 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return an array', () => {
+    expect(getHumanAge(16, 16))
+      .toBeInstanceOf(Array);
+  });
+
+  test(`should return an array with two zeros
+    if catAge and dogAge are equal to 0`, () => {
+    expect(getHumanAge(0, 0))
+      .toEqual([0, 0]);
+  });
+
+  test(`should return an array with two zeros
+    if catAge and dogAge < 15`, () => {
+    expect(getHumanAge(13, 11))
+      .toEqual([0, 0]);
+  });
+
+  test(`should return an array with two ones
+    if catAge and dogAge > 15 but < 24`, () => {
+    expect(getHumanAge(16, 23))
+      .toEqual([1, 1]);
+  });
+
+  test(`should return an array with two twoes
+    if catAge and dogAge > 24 but =< 27`, () => {
+    expect(getHumanAge(25, 26))
+      .toEqual([2, 2]);
+  });
+
+  test(`should return an array with correct coverted
+    catAge and dogAge to human age if catAge and dogAge >= 28`, () => {
+    expect(getHumanAge(100, 100))
+      .toEqual([21, 17]);
+
+    expect(getHumanAge(54, 68))
+      .toEqual([9, 10]);
+  });
 });
