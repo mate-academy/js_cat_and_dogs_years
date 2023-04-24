c2da8a4c19cdd4bd31d79cdf18a838f49107e84a
Yuliia Vereshko <pyliatko@gmail.com>
2023-04-24 23:09:07 +0200

    added new tests

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -21,3 +21,13 @@ test('it should return expected ages', () => {
   expect(getHumanAge(23, 20))
     .toEqual([1, 1]);
 });
+
+test('it should return 0 if the animal age is less than first element', () => {
+  expect(getHumanAge(13, 10))
+    .toEqual([0, 0]);
+});
+
+test('it should return animal age rounded to the nearest integer.', () => {
+  expect(getHumanAge(45, 40))
+    .toEqual([7, 5]);
+});
