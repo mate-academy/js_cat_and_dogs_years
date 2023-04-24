5b2c15bfba2c16e5ec56ee985e6cface10a19c81
Yaroslav Khoma <yaroamazone@gmail.com>
2023-04-24 21:19:52 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,28 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should be 0 human year if animalAge < 15', () => {
+    expect(getHumanAge(0, 0))
+      .toEqual([0, 0]);
+
+    expect(getHumanAge(14, 14))
+      .toEqual([0, 0]);
+  });
+
+  test('should be plus 1 year if animalAge >= 15 && animalAge < 24', () => {
+    expect(getHumanAge(15, 15))
+      .toEqual([1, 1]);
+
+    expect(getHumanAge(23, 23))
+      .toEqual([1, 1]);
+  });
+
+  test('should be catAge +1 every 4 years and dogAGe +1 every 5 years', () => {
+    expect(getHumanAge(50, 50))
+      .toEqual([8, 7]);
+
+    expect(getHumanAge(100, 100))
+      .toEqual([21, 17]);
+  });
 });
