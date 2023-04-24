f050d9a3d4b61c3a7709b39ba168aa145ac191c9
Oleksandr_Hundertailo <oleksandrhundertailo@gmail.com>
2023-04-24 22:35:37 +0200

    change variable name

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -9,37 +9,37 @@ describe('getHumanAge', () => {
   });
 
   test('From 0 to 14 cat or dog years give 1 human year', () => {
-    const foo = getHumanAge(0, 14);
+    const humanAges = getHumanAge(0, 14);
 
-    expect(foo)
+    expect(humanAges)
       .toEqual([0, 0]);
   });
 
   test('From 15 to 23 cat or dog years give 1 human year', () => {
-    const foo = getHumanAge(15, 15);
+    const humanAges = getHumanAge(15, 15);
 
-    expect(foo)
+    expect(humanAges)
       .toEqual([1, 1]);
   });
 
   test('From 24 to 27 cat or dog years give 2 human year', () => {
-    const foo = getHumanAge(24, 24);
+    const humanAges = getHumanAge(24, 24);
 
-    expect(foo)
+    expect(humanAges)
       .toEqual([2, 2]);
   });
 
   test('Every 4 years after 24 dog years give 1 more human year', () => {
-    const foo = getHumanAge(28, 28);
+    const humanAges = getHumanAge(28, 28);
 
-    expect(foo)
+    expect(humanAges)
       .toEqual([3, 2]);
   });
 
   test('Every 5 years after 24 cat years give 1 more human year', () => {
-    const foo = getHumanAge(37, 35);
+    const humanAges = getHumanAge(37, 35);
 
-    expect(foo)
+    expect(humanAges)
       .toEqual([5, 4]);
   });
 });
