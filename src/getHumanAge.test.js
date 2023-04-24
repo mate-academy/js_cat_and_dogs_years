c5e3f6e51421a9d1d2695da2ca66dbad29347c08
Denys Shyshlakov <444.spam@gmail.com>
2023-04-24 21:40:35 +0300

    Cats and Dogs FIXED

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -16,27 +16,27 @@ describe('getHumanAge', () => {
     expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
-  test('Should return [0,0] for catAge = 14 and dogAge = 14', () => {
+  test('Should return [1,1] for catAge = 15 and dogAge = 15', () => {
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
   });
 
-  test('Should return [0,0] for catAge = 14 and dogAge = 14', () => {
+  test('Should return [1,1] for catAge = 23 and dogAge = 23', () => {
     expect(getHumanAge(23, 23)).toEqual([1, 1]);
   });
 
-  test('Should return [0,0] for catAge = 14 and dogAge = 14', () => {
+  test('Should return [2,2] for catAge = 24 and dogAge = 24', () => {
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
 
-  test('Should return [0,0] for catAge = 14 and dogAge = 14', () => {
+  test('Should return [2,2] for catAge = 27 and dogAge = 27', () => {
     expect(getHumanAge(27, 27)).toEqual([2, 2]);
   });
 
-  test('Should return [0,0] for catAge = 14 and dogAge = 14', () => {
+  test('Should return [3,2] for catAge = 28 and dogAge = 28', () => {
     expect(getHumanAge(28, 28)).toEqual([3, 2]);
   });
 
-  test('Should return [0,0] for catAge = 14 and dogAge = 14', () => {
+  test('Should return [21,17] for catAge = 100 and dogAge = 100', () => {
     expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
