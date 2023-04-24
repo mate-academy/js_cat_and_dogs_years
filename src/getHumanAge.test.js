43b59a3f4b61741295e4f2c988252aac84bc9036
NataliaSavenets <savenets.natalia@gmail.com>
2023-04-24 22:51:49 +0100

    solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,49 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return 0 humanYear when animals age 0', () => {
+    expect(getHumanAge(0, 0))
+      .toEqual([0, 0]);
+  });
+
+  test('should return 0 humanYear when animals age <= 14', () => {
+    expect(getHumanAge(14, 14))
+      .toEqual([0, 0]);
+  });
+
+  test('should return 1 humanYear when animals age > 14 and <= 23', () => {
+    expect(getHumanAge(15, 15))
+      .toEqual([1, 1]);
+  });
+
+  test('should return 1 humanYear when animals age > 14 and <= 23', () => {
+    expect(getHumanAge(23, 23))
+      .toEqual([1, 1]);
+  });
+
+  test('should return 2 humanYear when animals age >= 24', () => {
+    expect(getHumanAge(24, 24))
+      .toEqual([2, 2]);
+  });
+
+  test('should return 2 humanYear when animals age >= 24', () => {
+    expect(getHumanAge(24, 24))
+      .toEqual([2, 2]);
+  });
+
+  test('should return 2 humanYear when animals age >= 24 and <= 27', () => {
+    expect(getHumanAge(27, 27))
+      .toEqual([2, 2]);
+  });
+
+  test('Every 4 years after 24 catsYears give 1 extra humanYear', () => {
+    expect(getHumanAge(28, 28))
+      .toEqual([3, 2]);
+  });
+
+  test('Every 5 years after 24 dogsYears give 1 extra human year', () => {
+    expect(getHumanAge(29, 29))
+      .toEqual([3, 3]);
+  });
 });
