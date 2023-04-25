46d4a655a928237a1b714563248329dd4154f407
Anastasiia Zheliaskova <a.zheliaskova3@gmail.com>
2023-04-25 12:06:17 +0300

    Fixed task

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -26,18 +26,30 @@ describe('getHumanAge', () => {
   });
 
   test(`should return an array with two ones
+    if catAge and dogAge = 15`, () => {
+    expect(getHumanAge(15, 15))
+      .toEqual([1, 1]);
+  });
+
+  test(`should return an array with two ones
     if catAge and dogAge > 15 but < 24`, () => {
     expect(getHumanAge(16, 23))
       .toEqual([1, 1]);
   });
 
   test(`should return an array with two twoes
-    if catAge and dogAge > 24 but =< 27`, () => {
-    expect(getHumanAge(25, 26))
+    if catAge and dogAge = 24`, () => {
+    expect(getHumanAge(24, 24))
       .toEqual([2, 2]);
   });
 
-  test(`should return an array with correct coverted
+  test(`should return an array with two twoes
+    if catAge and dogAge = 27`, () => {
+    expect(getHumanAge(27, 27))
+      .toEqual([2, 2]);
+  });
+
+  test(`should return an array with converted
     catAge and dogAge to human age if catAge and dogAge >= 28`, () => {
     expect(getHumanAge(100, 100))
       .toEqual([21, 17]);
