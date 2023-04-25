8f52e5c4b678ecbbc957243a673a446f09111ffd
emmmcr <maximandreev99@gmail.com>
2023-04-25 12:06:28 +0300

    Requested fixes added

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,14 +8,29 @@ describe(`getHumanAge`, () => {
       .toBeInstanceOf(Function);
   });
 
-  test(`should return 0's if both inputs <15`, () => {
+  test(`should return 0 if both inputs <15`, () => {
     expect(getHumanAge(14, 14))
       .toEqual([0, 0]);
+  });
+
+  test(`should return 1 if both inputs are 15`, () => {
+    expect(getHumanAge(15, 15))
+      .toEqual([1, 1]);
   });
 
   test(`should return 1 if inputs are 15-23`, () => {
     expect(getHumanAge(17, 19))
       .toEqual([1, 1]);
+  });
+
+  test(`should return 1 if inputs are 23`, () => {
+    expect(getHumanAge(23, 23))
+      .toEqual([1, 1]);
+  });
+
+  test(`should return 2 if inputs are 24`, () => {
+    expect(getHumanAge(24, 24))
+      .toEqual([2, 2]);
   });
 
   test(`should return 2 if inputs are 24-27`, () => {
