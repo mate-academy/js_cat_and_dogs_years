74561d6f07fe330f9d3dec4eb5e312f618c0e7b2
emmmcr <maximandreev99@gmail.com>
2023-04-26 11:31:07 +0300

    Descriptions fixed

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,32 +8,32 @@ describe(`getHumanAge`, () => {
       .toBeInstanceOf(Function);
   });
 
-  test(`should return 0 if both inputs <15`, () => {
+  test(`should return 0 for cat and 0 for dog if both inputs <15`, () => {
     expect(getHumanAge(14, 14))
       .toEqual([0, 0]);
   });
 
-  test(`should return 1 if both inputs are 15`, () => {
+  test(`should return 1 for cat and 1 for dog if both inputs are 15`, () => {
     expect(getHumanAge(15, 15))
       .toEqual([1, 1]);
   });
 
-  test(`should return 1 if inputs are 15-23`, () => {
+  test(`should return 1 for cat and 1 for dog if inputs are 15-23`, () => {
     expect(getHumanAge(17, 19))
       .toEqual([1, 1]);
   });
 
-  test(`should return 1 if inputs are 23`, () => {
+  test(`should return 1 for cat and 1 for dog if inputs are 23`, () => {
     expect(getHumanAge(23, 23))
       .toEqual([1, 1]);
   });
 
-  test(`should return 2 if inputs are 24`, () => {
+  test(`should return 2 for cat and 2 for dog if inputs are 24`, () => {
     expect(getHumanAge(24, 24))
       .toEqual([2, 2]);
   });
 
-  test(`should return 2 if inputs are 24-27`, () => {
+  test(`should return 2 for cat and 2 for dog if inputs are 24-27`, () => {
     expect(getHumanAge(25, 25))
       .toEqual([2, 2]);
   });
@@ -43,7 +43,7 @@ describe(`getHumanAge`, () => {
       .toEqual([3, 2]);
   });
 
-  test(`should return 3 if inputs are 29-33`, () => {
+  test(`should return 3 for cat and 3 for dog if inputs are 29-33`, () => {
     expect(getHumanAge(29, 29))
       .toEqual([3, 3]);
   });
