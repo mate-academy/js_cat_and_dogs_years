daeb5d0e251ebcd05def24ceb1789c2bbdec2e8a
katzamkova <katerynadashko95@gmail.com>
2023-04-26 09:59:59 +0300

    Fixes to Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -18,42 +18,42 @@ describe('getHumanAge', () => {
       .toHaveLength(2);
   });
 
-  test(`shoud return 0 if cat and dog are 0 year old`, () => {
+  test(`should return 0 if cat and dog are 0 year old`, () => {
     expect(getHumanAge(0, 0))
       .toStrictEqual([0, 0]);
   });
 
-  test(`shoud return 0 if cat and dog are < 15 years old`, () => {
+  test(`should return 0 if cat and dog are < 15 years old`, () => {
     expect(getHumanAge(14, 14))
       .toStrictEqual([0, 0]);
   });
 
-  test(`shoud return 1 if cat and dog are 15 years old`, () => {
+  test(`should return 1 if cat and dog are 15 years old`, () => {
     expect(getHumanAge(15, 15))
       .toStrictEqual([1, 1]);
   });
 
-  test(`shoud return 1 if cat and dog are 23 years old`, () => {
+  test(`should return 1 if cat and dog are 23 years old`, () => {
     expect(getHumanAge(23, 23))
       .toStrictEqual([1, 1]);
   });
 
-  test(`shoud return 2 if cat and dog are 24 years old`, () => {
+  test(`should return 2 if cat and dog are 24 years old`, () => {
     expect(getHumanAge(24, 24))
       .toStrictEqual([2, 2]);
   });
 
-  test(`shoud return [2, 2] if cat and dog are 27 years old`, () => {
+  test(`should return [2, 2] if cat and dog are 27 years old`, () => {
     expect(getHumanAge(27, 27))
       .toStrictEqual([2, 2]);
   });
 
-  test(`shoud return [3, 2] if cat and dog are 28 years old`, () => {
+  test(`should return [3, 2] if cat and dog are 28 years old`, () => {
     expect(getHumanAge(28, 28))
       .toStrictEqual([3, 2]);
   });
 
-  test(`shoud return [21, 27] if cat and dog are 100 years old`, () => {
+  test(`should return [21, 27] if cat and dog are 100 years old`, () => {
     expect(getHumanAge(100, 100))
       .toStrictEqual([21, 17]);
   });
