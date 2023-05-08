b71a686036600e655b9d0809b85ea5f3c14826d6
Artem Sukhetskiy <artem.sukhetskiy@mirko.in.ua>
2023-05-08 15:24:51 +0300

    solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -3,8 +3,23 @@
 describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
-  test('should be declared', () => {
+  test('getHumanAge is a function', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
+  });
+
+  test('age of animals is less than 15', () => {
+    expect(getHumanAge(0, 14))
+      .toEqual([0, 0]);
+  });
+
+  test('age of animals is more or equal 15 and less than 24', () => {
+    expect(getHumanAge(15, 23))
+      .toEqual([1, 1]);
+  });
+
+  test('age of animals is more or equal 25', () => {
+    expect(getHumanAge(25, 50))
+      .toEqual([2, 7]);
   });
 });
