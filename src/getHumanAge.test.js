72d9b70f018c517835bd1603a09e0127c695a560
Artem Sukhetskiy <artem.sukhetskiy@mirko.in.ua>
2023-05-09 13:03:22 +0300

    Changed solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,17 +8,27 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test('age of animals is less than 15', () => {
+  test("should return [0, 0] when animals' ages are (0, 14)", () => {
     expect(getHumanAge(0, 14))
       .toEqual([0, 0]);
   });
 
-  test('age of animals is more or equal 15 and less than 24', () => {
+  test("should return [0, 0] when animals' ages are (4, 10)", () => {
+    expect(getHumanAge(4, 10))
+      .toEqual([0, 0]);
+  });
+
+  test("should return [1, 1] when animals' ages are (15, 23)", () => {
     expect(getHumanAge(15, 23))
       .toEqual([1, 1]);
   });
 
-  test('age of animals is more or equal 25', () => {
+  test("should return [1, 1] when animals' ages are (18, 21)", () => {
+    expect(getHumanAge(18, 21))
+      .toEqual([1, 1]);
+  });
+
+  test("should return [2, 7] when animals' ages are (25, 50)", () => {
     expect(getHumanAge(25, 50))
       .toEqual([2, 7]);
   });
