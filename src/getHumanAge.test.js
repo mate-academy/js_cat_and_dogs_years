1e43b8247fc0a94feb886191258fc2ff18f4219a
Vlad <bojko.vladislav16@gmail.com>
2023-06-04 13:45:39 +0300

    All redundant tests have been removed

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -9,16 +9,16 @@ describe('getHumanAge', () => {
   });
 
   test('should return 0 human years for cat or dog less than 15 years', () => {
-    expect(getHumanAge(10, 7)).toEqual([0, 0]);
     expect(getHumanAge(10, 15)).toEqual([0, 1]);
-  })
+  });
 
+  // eslint-disable-next-line max-len
   test('should return 1 human year for cat or dog between 15 and 24 years', () => {
     expect(getHumanAge(15, 24)).toEqual([1, 2]);
-  })
+  });
 
+  // eslint-disable-next-line max-len
   test('should return more than 1 human year for cat or dog over 24 years', () => {
-    expect(getHumanAge(28, 28)).toEqual([3, 2]);
     expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
