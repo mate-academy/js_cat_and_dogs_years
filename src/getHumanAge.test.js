08901bca84013dbe074c87fc999a52733519cebd
Natasha <kava221998@gmail.com>
2023-06-07 20:09:49 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,22 +8,26 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test('should return the Human Age of 15 years old cat and dog', () => {
+  test(`should return an array of numbers which contains 
+  the Human Age of 15 years old cat and dog accordingly`, () => {
     expect(getHumanAge(15, 15))
       .toEqual([1, 1]);
   });
 
-  test('should return the Human Age of 24 years old cat and dog', () => {
+  test(`should return an array of numbers which contains 
+  the Human Age of 24 years old cat and dog`, () => {
     expect(getHumanAge(24, 24))
       .toEqual([2, 2]);
   });
 
-  test('should return the Human Age of 28 years old cat and dog', () => {
+  test(`should return an array of numbers which contains 
+  the Human Age of 28 years old cat and dog`, () => {
     expect(getHumanAge(28, 28))
       .toEqual([3, 2]);
   });
 
-  test('should return the Human Age of 28 years old cat and 29 dog', () => {
+  test(`should return an array of numbers which contains 
+  the Human Age of 28 years old cat and 29 dog`, () => {
     expect(getHumanAge(28, 29))
       .toEqual([3, 3]);
   });
