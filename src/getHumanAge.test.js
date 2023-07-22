6c1a565efe47e1905763cddf619728c799db1b32
Oselskyi Oleksandr <oselskyi3@gmail.com>
2023-07-22 17:18:08 +0300

    fix

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -20,25 +20,25 @@ describe('getHumanAge', () => {
   });
 
   test(`should return 1 if both
-    catAge and doqAge are between 15 and 23`, () => {
+    catAge and dogAge are between 15 and 23`, () => {
     expect(getHumanAge(15, 23))
       .toEqual([1, 1]);
   });
 
   test(`should return 2 if both
-    catAge and doqAge are between 24 and 27`, () => {
+    catAge and dogAge are between 24 and 27`, () => {
     expect(getHumanAge(24, 27))
       .toEqual([2, 2]);
   });
 
   test(`should return 3 for catAge and 2 for doqAge if both
-  catAge and doqAge equal 28`, () => {
+  catAge and dogAge equal 28`, () => {
     expect(getHumanAge(28, 28))
       .toEqual([3, 2]);
   });
 
   test(`should return 21 for catAge and 17 for doqAge if both
-  catAge and doqAge equal 100`, () => {
+  catAge and dogAge equal 100`, () => {
     expect(getHumanAge(100, 100))
       .toEqual([21, 17]);
   });
