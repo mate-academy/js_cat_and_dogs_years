3488e55e3158400b2c77f39d4c72c5cb87dff86e
Valery Sazonov <vksazonov@gmail.com>
2023-08-09 15:21:06 +0300

    Added Tests

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,29 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return an array of 2 items', () => {
+    expect(getHumanAge(0, 0))
+      .toHaveLength(2);
+  });
+
+  test('should return 0 if numbers < 15', () => {
+    expect(getHumanAge(14, 14))
+      .toEqual([0, 0]);
+  });
+
+  test('should return 1 if numbers < 24', () => {
+    expect(getHumanAge(23, 23))
+      .toEqual([1, 1]);
+  });
+
+  test('should return 2 if numbers < 27', () => {
+    expect(getHumanAge(27, 27))
+      .toEqual([2, 2]);
+  });
+
+  test('should return 3 if cat years < 32 and dog years < 33', () => {
+    expect(getHumanAge(31, 32))
+      .toEqual([3, 3]);
+  });
 });
