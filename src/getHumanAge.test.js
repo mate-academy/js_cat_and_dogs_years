09739ffde021ab90a5f923c5bc5c03d44d2c822e
Yana <7949309@stud.nau.edu.ua>
2023-08-10 23:23:15 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,34 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test(`should return zeros`, () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test('should return units', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  test('should return units ', () => {
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
+  test(`should return dueces`, () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+
+  test(`should return units`, () => {
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
+
+  test(`should return the listed years to human years`,
+    () => {
+      expect(getHumanAge(28, 28)).toEqual([3, 2]);
+    });
+
+  test(`should return the listed years to human years`,
+    () => {
+      expect(getHumanAge(100, 100)).toEqual([21, 17]);
+    });
 });
