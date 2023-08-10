d8f539e5053931f369682fb68101e4b07ae88b85
Dmytro Ponomarov <ponomarovdmitry13@gmail.com>
2023-08-10 22:37:17 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,34 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return 0', () => {
+    expect(getHumanAge(0, 0))
+      .toStrictEqual([0, 0]);
+  });
+
+  test('boundary values check, minimum age returned', () => {
+    expect(getHumanAge(15, 15))
+      .toStrictEqual([1, 1]);
+  });
+
+  test('boundary values check, minimum age returned', () => {
+    expect(getHumanAge(14, 14))
+      .toStrictEqual([0, 0]);
+  });
+
+  test('boundary values check, should return 3 and 2', () => {
+    expect(getHumanAge(28, 28))
+      .toStrictEqual([3, 2]);
+  });
+
+  test('boundary values check, should return 3 and 3', () => {
+    expect(getHumanAge(29, 29))
+      .toStrictEqual([3, 3]);
+  });
+
+  test('should return result of four-digit calculations', () => {
+    expect(getHumanAge(1000, 1000))
+      .toStrictEqual([246, 197]);
+  });
 });
