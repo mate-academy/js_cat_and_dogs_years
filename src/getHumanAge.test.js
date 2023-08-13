6a3c6442de887444ae432d3ff4015f83d2839bf0
Yelyzaveta <122609036+YelyzavetaFilimonova@users.noreply.github.com>
2023-08-13 11:10:32 +0300

    Update getHumanAge.test.js

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,42 +8,42 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test('return listed years', () => {
+  test('should be return 0 if catAge or dogAge is equal 0', () => {
     expect(getHumanAge(0, 0))
       .toEqual([0, 0]);
   });
 
-  test('return listed years', () => {
+  test('should be return a right value for first 14 cat & dog age', () => {
     expect(getHumanAge(14, 14))
       .toEqual([0, 0]);
   });
 
-  test('return listed years', () => {
+  test('should be return a right value for first 15 cat & dog age', () => {
     expect(getHumanAge(15, 15))
       .toEqual([1, 1]);
   });
 
-  test('return listed years', () => {
+  test('should be return a right value for first 23 cat & dog age', () => {
     expect(getHumanAge(23, 23))
       .toEqual([1, 1]);
   });
 
-  test('return listed years', () => {
+  test('should be return a right value for first 24 cat & dog age', () => {
     expect(getHumanAge(24, 24))
       .toEqual([2, 2]);
   });
 
-  test('return listed years', () => {
+  test('should be return a right value for first 27 cat & dog age', () => {
     expect(getHumanAge(27, 27))
       .toEqual([2, 2]);
   });
 
-  test('return listed years', () => {
+  test('should be return a right value for first 28 cat & dog age', () => {
     expect(getHumanAge(28, 28))
       .toEqual([3, 2]);
   });
 
-  test('return listed years', () => {
+  test('should be return a right value for first 100 cat & dog age', () => {
     expect(getHumanAge(100, 100))
       .toEqual([21, 17]);
   });
