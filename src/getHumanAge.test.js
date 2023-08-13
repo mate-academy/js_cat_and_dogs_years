01331378084042e39e9c26b9bd252034fad984b5
Olha <ogvozdeckas@gmail.com>
2023-08-13 19:59:16 +0200

    Fixed

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -27,23 +27,29 @@ describe('getHumanAge', () => {
       .toEqual([2, 2]);
   });
 
-  test('should return 2, 2 if catAge 27 and dogAge 28', () => {
-    expect(getHumanAge(27, 28))
+  test('should return 2, 2 if catAge 27 and dogAge 27', () => {
+    expect(getHumanAge(27, 27))
       .toEqual([2, 2]);
   });
 
-  test('should return 3, 3 if catAge 28 and dogAge 29', () => {
-    expect(getHumanAge(28, 29))
+  test('should return 3, 3 if catAge 28 and dogAge 28', () => {
+    expect(getHumanAge(28, 28))
       .toEqual([3, 3]);
   });
 
-  test('should return 4, 4 if catAge 32 and dogAge 34', () => {
-    expect(getHumanAge(32, 34))
-      .toEqual([4, 4]);
+  test('should return 4, 4 if catAge 35 and dogAge 35', () => {
+    expect(getHumanAge(35, 35))
+      .toEqual([5, 5]);
   });
 
   test('should return 0, 0 if catAge 0 and dogAge 0', () => {
     expect(getHumanAge(0, 0))
       .toEqual([0, 0]);
   });
+
+  test('should return 100, 100 if catAge 21 and dogAge 17', () => {
+    expect(getHumanAge(21, 17))
+      .toEqual([100, 100]);
+  });
+
 });
