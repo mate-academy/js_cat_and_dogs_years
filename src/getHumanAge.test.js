32262e4302ac0fca14a78550edf700ac7646aa6e
H0N <honyy.qa@gmail.com>
2023-08-14 16:41:16 +0300

    FixedSolution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -9,60 +9,36 @@ describe('getHumanAge', () => {
   });
 
   test(`should return array of numbers`, () => {
-    const catAgeMock = null;
-    const dogAgeMock = null;
-    const result = getHumanAge(catAgeMock, dogAgeMock);
-
-    expect(Array.isArray(result)).toBeTruthy();
-    expect(result).toHaveLength(2);
-    expect(result).toEqual([0, 0]);
+    expect(Array.isArray(getHumanAge(null, null))).toBeTruthy();
+    expect(getHumanAge(null, null)).toHaveLength(2);
+    expect(getHumanAge(null, null)).toEqual([0, 0]);
   });
 
   test('should return 0 humanAge if input value less than 15 years', () => {
-    const catAgeMock = 14;
-    const dogAgeMock = 0;
-    const result = getHumanAge(catAgeMock, dogAgeMock);
-
-    expect(result).toHaveLength(2);
-    expect(result).toEqual([0, 0]);
+    expect(getHumanAge(14, 0)).toHaveLength(2);
+    expect(getHumanAge(14, 0)).toEqual([0, 0]);
   });
 
   test(`should return 1 humanAge
     if input value between 15 and 23 years`, () => {
-    const catAgeMock = 15;
-    const dogAgeMock = 16;
-    const result = getHumanAge(catAgeMock, dogAgeMock);
-
-    expect(result).toHaveLength(2);
-    expect(result).toEqual([1, 1]);
+    expect(getHumanAge(15, 16)).toHaveLength(2);
+    expect(getHumanAge(15, 16)).toEqual([1, 1]);
   });
 
   test(`should test return 2 humanAge
     if input value between 24 and 27 years`, () => {
-    const catAgeMock = 24;
-    const dogAgeMock = 27;
-    const result = getHumanAge(catAgeMock, dogAgeMock);
-
-    expect(result).toHaveLength(2);
-    expect(result).toEqual([2, 2]);
+    expect(getHumanAge(24, 27)).toHaveLength(2);
+    expect(getHumanAge(24, 27)).toEqual([2, 2]);
   });
 
   test(`should return one more humanAge
     for every 4 cat years and 5 dog years after 24`, () => {
-    const catAgeMock = 32;
-    const dogAgeMock = 40;
-    const result = getHumanAge(catAgeMock, dogAgeMock);
-
-    expect(result).toHaveLength(2);
-    expect(result).toEqual([4, 5]);
+    expect(getHumanAge(32, 40)).toHaveLength(2);
+    expect(getHumanAge(32, 40)).toEqual([4, 5]);
   });
 
   test(`should not duplicate human years for each 15/9 years`, () => {
-    const catAgeMock = 100;
-    const dogAgeMock = 100;
-    const result = getHumanAge(catAgeMock, dogAgeMock);
-
-    expect(result).toHaveLength(2);
-    expect(result).toEqual([21, 17]);
+    expect(getHumanAge(100, 100)).toHaveLength(2);
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
