352e35fb3ffaf7895344cb6f14290c9269025a68
KlymSudakov <sudakov.klym@gmail.com>
2023-09-27 19:41:07 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,20 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should convert ages that are less then 1', () => {
+    expect(getHumanAge(0, 14)).toEqual([0, 0]);
+  });
+
+  test('should convert ages that are equal to 1', () => {
+    expect(getHumanAge(15, 23)).toEqual([1, 1]);
+  });
+
+  test('should convert ages that are equal to 2', () => {
+    expect(getHumanAge(27, 28)).toEqual([2, 2]);
+  });
+
+  test('should convert ages that are equal to 15', () => {
+    expect(getHumanAge(77, 93)).toEqual([15, 15]);
+  });
 });
