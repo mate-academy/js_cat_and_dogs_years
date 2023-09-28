74907389f867383ee889d1a6a85980be7b02f1e0
Orest Iwanowski <orestiwanowski@gmail.com>
2023-09-28 17:58:28 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,64 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return an array', () => {
+    expect(getHumanAge(1, 1))
+      .toBeInstanceOf(Array);
+  });
+
+  test('should return an array with two elements', () => {
+    expect(getHumanAge(1, 3))
+      .toHaveLength(2);
+  });
+
+  test('should return zeros if age of cat and dog is zero', () => {
+    expect(getHumanAge(0, 0))
+      .toEqual([0, 0]);
+  });
+
+  test('should return zeros if age of cat or dog < 15', () => {
+    expect(getHumanAge(5, 11))
+      .toEqual([0, 0]);
+  });
+
+  test('should return 1 if age of cat and dog = 15', () => {
+    expect(getHumanAge(15, 15))
+      .toEqual([1, 1]);
+  });
+
+  test('should return 1 if age of cat or dog on range of 15 - 23', () => {
+    expect(getHumanAge(18, 21))
+      .toEqual([1, 1]);
+  });
+
+  test('should return 1 if age of cat and dog is 23', () => {
+    expect(getHumanAge(23, 23))
+      .toEqual([1, 1]);
+  });
+
+  test('should return 2 if age of cat and dog is 24', () => {
+    expect(getHumanAge(24, 24))
+      .toEqual([2, 2]);
+  });
+
+  test('should return 2 if age of cat and dog is 27', () => {
+    expect(getHumanAge(27, 27))
+      .toEqual([2, 2]);
+  });
+
+  test('should return 3 for cat and 2 for dog if age is 28', () => {
+    expect(getHumanAge(28, 28))
+      .toEqual([3, 2]);
+  });
+
+  test('should return 21 for cat and 17 for dog if age is 100', () => {
+    expect(getHumanAge(100, 100))
+      .toEqual([21, 17]);
+  });
+
+  test('should return 3 if the age of cat and dog is 31.5', () => {
+    expect(getHumanAge(31.5, 31.5))
+      .toEqual([3, 3]);
+  });
 });
