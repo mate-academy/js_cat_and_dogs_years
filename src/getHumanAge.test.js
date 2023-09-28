7a3b74f98c731fe20b6d6ea17ed930c017a68d0e
KlymSudakov <sudakov.klym@gmail.com>
2023-09-28 21:11:27 +0300

    add fixes, remove 0 value from arguments, join similar tests

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,26 +8,18 @@ describe('getHumanAge', () => {
   });
 
   test('should convert ages that are less then 1', () => {
-    expect(getHumanAge(0, 14)).toEqual([0, 0]);
+    expect(getHumanAge(7, 7)).toEqual([0, 0]);
   });
 
   test('should convert ages that are equal to 1', () => {
-    expect(getHumanAge(15, 23)).toEqual([1, 1]);
+    expect(getHumanAge(19, 19)).toEqual([1, 1]);
   });
 
-  test('should convert cat\'s age that are equal to 2', () => {
-    expect(getHumanAge(27, 0)).toEqual([2, 0]);
+  test('should convert ages that are equal to 2', () => {
+    expect(getHumanAge(27, 28)).toEqual([2, 2]);
   });
 
-  test('should convert dog\'s age that are equal to 2', () => {
-    expect(getHumanAge(0, 28)).toEqual([0, 2]);
-  });
-
-  test('should convert cat\'s age that are equal to 15', () => {
-    expect(getHumanAge(77, 0)).toEqual([15, 0]);
-  });
-
-  test('should convert dog\'s age that are equal to 15', () => {
-    expect(getHumanAge(0, 93)).toEqual([0, 15]);
+  test('should convert ages that are equal to 15', () => {
+    expect(getHumanAge(77, 93)).toEqual([15, 15]);
   });
 });
