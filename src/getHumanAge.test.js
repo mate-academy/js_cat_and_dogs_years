7be73dfe7ddf7efd3794663a484e16792da1dc4e
KlymSudakov <sudakov.klym@gmail.com>
2023-09-28 13:19:34 +0300

    separate tests if value > 24

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -4,8 +4,7 @@ describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
   test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+    expect(getHumanAge).toBeInstanceOf(Function);
   });
 
   test('should convert ages that are less then 1', () => {
@@ -16,11 +15,19 @@ describe('getHumanAge', () => {
     expect(getHumanAge(15, 23)).toEqual([1, 1]);
   });
 
-  test('should convert ages that are equal to 2', () => {
-    expect(getHumanAge(27, 28)).toEqual([2, 2]);
+  test('should convert cat\'s age that are equal to 2', () => {
+    expect(getHumanAge(27, 0)).toEqual([2, 0]);
   });
 
-  test('should convert ages that are equal to 15', () => {
-    expect(getHumanAge(77, 93)).toEqual([15, 15]);
+  test('should convert dog\'s age that are equal to 2', () => {
+    expect(getHumanAge(0, 28)).toEqual([0, 2]);
+  });
+
+  test('should convert cat\'s age that are equal to 15', () => {
+    expect(getHumanAge(77, 0)).toEqual([15, 0]);
+  });
+
+  test('should convert dog\'s age that are equal to 15', () => {
+    expect(getHumanAge(0, 93)).toEqual([0, 15]);
   });
 });
