2254d8fc0e489dd2f90701c2259acfe80476d73a
Olesia Semen <semenolesa01@gmail.com>
2023-10-15 23:49:04 +0300

    Testing

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -3,8 +3,23 @@
 describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
-  test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+  it('should correctly convert cat and dog ages to human years', () => {
+  
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+
+
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
