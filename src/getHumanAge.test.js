e4e1e96fd9a1fd07d9a7c238e75533df1ed60bd4
lognisgregnis <logsnskiy@ukr.net>
2023-10-22 20:47:09 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,24 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return the human age of a cat and dog', () => {
+    expect(getHumanAge(10, 15))
+      .toEqual([0, 1]);
+  });
+
+  test('should return the human age of a cat and dog', () => {
+    expect(getHumanAge(20, 30))
+      .toEqual([1, 3]);
+  });
+
+  test('should return the human age of a cat and dog', () => {
+    expect(getHumanAge(25, 10))
+      .toEqual([2, 0]);
+  });
+
+  test('should return the human age of a cat and dog', () => {
+    expect(getHumanAge(36, 34))
+      .toEqual([5, 4]);
+  });
 });
