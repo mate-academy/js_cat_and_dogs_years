f1bb26bfff6fda7ffe6ade624e7eec861943c55f
VladSlob <141657806+VladSlob@users.noreply.github.com>
2023-11-16 07:50:43 +0200

    getHumanAge.test.js

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,28 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return [0, 0] for input (0, 0)', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('should return [0, 0] for input (14, 14)', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test('should return [1, 1] for input (15, 15)', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  test('should return [2, 2] for input (24, 24)', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+
+  test('should return [3, 2] for input (28, 28)', () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+  });
+
+  test('should return [21, 17] for input (100, 100)', () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
 });
