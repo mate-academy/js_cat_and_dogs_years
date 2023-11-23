b2929c9ad66433ac4f9c21a86f6a519a1d794573
Artem Nikolaichuk <tompsoq@gmail.com>
2023-11-23 16:21:49 +0200

    fixed 1

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -3,11 +3,6 @@
 describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
-  test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
-  });
-
   test(`should return correct human age for cats and dogs`, () => {
     expect(getHumanAge(0, 0)).toEqual([0, 0]);
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
@@ -16,9 +11,5 @@ describe('getHumanAge', () => {
     expect(getHumanAge(27, 27)).toEqual([2, 2]);
     expect(getHumanAge(28, 28)).toEqual([3, 2]);
     expect(getHumanAge(100, 100)).toEqual([21, 17]);
-  });
-
-  test(`should handle negative catAge and dogAge`, () => {
-    expect(getHumanAge(-20, -29)).toEqual([0, 0]);
   });
 });
