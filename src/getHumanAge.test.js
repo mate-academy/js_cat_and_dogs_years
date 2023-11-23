8adabc7af35b94f429bb4a5b1c671b2d31b2e969
Artem Nikolaichuk <tompsoq@gmail.com>
2023-11-23 16:20:11 +0200

    fixed

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -9,17 +9,16 @@ describe('getHumanAge', () => {
   });
 
   test(`should return correct human age for cats and dogs`, () => {
-    expect(getHumanAge(0,0)).toEqual([0,0])
-    expect(getHumanAge(15, 15)).toEqual([1, 1])
-    expect(getHumanAge(23, 23)).toEqual([1, 1])
-    expect(getHumanAge(24, 24)).toEqual([2, 2])
-    expect(getHumanAge(27, 27)).toEqual([2, 2])
-    expect(getHumanAge(28, 28)).toEqual([3, 2])
-    expect(getHumanAge(100, 100)).toEqual([21, 17])
-  })
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
 
   test(`should handle negative catAge and dogAge`, () => {
-    expect(getHumanAge(-20, -29)).toEqual([0, 0])
-  })
+    expect(getHumanAge(-20, -29)).toEqual([0, 0]);
+  });
 });
-
