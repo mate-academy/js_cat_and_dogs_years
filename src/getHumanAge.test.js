ff243ac12de56d5db626d7207c12d46cd3749d68
artem <1234artem4321artem@gmail.com>
2023-11-26 20:52:30 +0200

    add tests to verify the function

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,16 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('shold return array', () => {
+    expect(typeof getHumanAge(12, 30)).toBe('object');
+  });
+
+  test('should be 2 numbers in the argument', () => {
+    expect(getHumanAge(25, 10)).toStrictEqual([2, 0]);
+  });
+
+  test('should be NaN if aguments is not a number ', () => {
+    expect(getHumanAge('two', 'one')).toStrictEqual([NaN, NaN]);
+  });
 });
