c9058ba969ef322f1696ea0cfb5106133fab1123
Sergei2132s <sergei.solovyov0294@gmail.com>
2023-11-27 10:47:50 +0200

    getHumanAge

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -3,8 +3,43 @@
 describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
-  test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+  test('should return [0, 0] for catAge and dogAge both being 0', () => {
+    const result = getHumanAge(0, 0);
+    expect(result).toEqual([0, 0]);
+  });
+
+  test('should return [0, 0] for catAge and dogAge both being less than 15', () => {
+    const result = getHumanAge(14, 14);
+    expect(result).toEqual([0, 0]);
+  });
+
+  test('should return [1, 1] for catAge and dogAge both being 15', () => {
+    const result = getHumanAge(15, 15);
+    expect(result).toEqual([1, 1]);
+  });
+
+  test('should return [1, 1] for catAge and dogAge both being between 15 and 23', () => {
+    const result = getHumanAge(23, 23);
+    expect(result).toEqual([1, 1]);
+  });
+
+  test('should return [2, 2] for catAge and dogAge both being 24', () => {
+    const result = getHumanAge(24, 24);
+    expect(result).toEqual([2, 2]);
+  });
+
+  test('should return [2, 2] for catAge and dogAge both being between 24 and 27', () => {
+    const result = getHumanAge(27, 27);
+    expect(result).toEqual([2, 2]);
+  });
+
+  test('should return [3, 2] for catAge being 28 and dogAge being 28', () => {
+    const result = getHumanAge(28, 28);
+    expect(result).toEqual([3, 2]);
+  });
+
+  test('should return [21, 17] for catAge being 100 and dogAge being 100', () => {
+    const result = getHumanAge(100, 100);
+    expect(result).toEqual([21, 17]);
   });
 });
