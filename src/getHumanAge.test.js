00f4a7427a8e9d36e7d50189dde52098f7ef8597
Nazar <nazarorlov552@gmail.com>
2023-11-27 10:42:11 +0200

    Second try

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -11,8 +11,27 @@ describe('getHumanAge', () => {
   });
 });
 
-test(`the first element should return number equal cat's age in human years
-the first element should return number equal dog's age in human years`, () => {
+test(`the first element should return
+ number equal cat's age in human years`, () => {
+  expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  expect(getHumanAge(14, 0)).toEqual([0, 0]);
+  expect(getHumanAge(15, 0)).toEqual([1, 0]);
+  expect(getHumanAge(23, 0)).toEqual([1, 0]);
+  expect(getHumanAge(28, 0)).toEqual([3, 0]);
+  expect(getHumanAge(100, 0)).toEqual([21, 0]);
+});
+
+test(`the second element should return 
+number equal dog's age in human years`, () => {
+  expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  expect(getHumanAge(0, 14)).toEqual([0, 0]);
+  expect(getHumanAge(0, 15)).toEqual([0, 1]);
+  expect(getHumanAge(0, 23)).toEqual([0, 1]);
+  expect(getHumanAge(0, 28)).toEqual([0, 2]);
+  expect(getHumanAge(0, 100)).toEqual([0, 17]);
+});
+
+test(`checking output for both imputs`, () => {
   expect(getHumanAge(0, 0)).toEqual([0, 0]);
   expect(getHumanAge(14, 14)).toEqual([0, 0]);
   expect(getHumanAge(15, 15)).toEqual([1, 1]);
