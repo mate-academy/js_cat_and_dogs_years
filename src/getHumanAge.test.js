a0fc8fb92638e7b8fee6e5821dbb12c08f96f93c
Nataliia Udachina <udachinanataliiava@gmail.com>
2023-12-02 14:14:52 +0200

    Solution v2

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -21,19 +21,29 @@ describe('getHumanAge', () => {
        they are 15 years old`, () => {
          expect(getHumanAge(15, 15)).toEqual([1, 1]);
        });
+
+       test(`should return 1 for cat and dog when
+       they are 15 years old`, () => {
+         expect(getHumanAge(23, 23)).toEqual([1, 1]);
+       });
      
        test(`should return 1 for cat and dog when
        they are 15 years old`, () => {
-         expect(getHumanAge(24, 24)).toEqual([1, 1]);
+         expect(getHumanAge(24, 24)).toEqual([2, 2]);
        });
      
        test(`should return 2 for cat and dog when
        they are 24 years old`, () => {
-         expect(getHumanAge(25, 25)).toEqual([2, 2]);
+         expect(getHumanAge(27, 27)).toEqual([2, 2]);
        });
       
        test(`should return 2 for cat and dog when
        they are 24 years old`, () => {
-         expect(getHumanAge(29, 30)).toEqual([2, 2]);
-  });
+         expect(getHumanAge(28, 28)).toEqual([3, 2]);
+      });
+
+      test(`should return 2 for cat and dog when
+       they are 24 years old`, () => {
+         expect(getHumanAge(100, 100)).toEqual([21, 17]);
+      });
 });
