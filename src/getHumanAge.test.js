1dd0f62d74fa5b837d05d172955cc83c6e0b2f23
artem <1234artem4321artem@gmail.com>
2023-12-20 16:51:51 +0200

    add more tests

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -16,6 +16,46 @@ describe('getHumanAge', () => {
     expect(getHumanAge(25, 10)).toStrictEqual([2, 0]);
   });
 
+  test('should be converted 2 agruments into human years with value 0', () => {
+    expect(getHumanAge(5, 5)).toStrictEqual([0, 0]);
+  });
+
+  test('should be converted 2 agruments into human years with value 0 - boundary value', () => {
+    expect(getHumanAge(14, 14)).toStrictEqual([0, 0]);
+  });
+
+  test('should be converted 2 agruments into human years with value 1', () => {
+    expect(getHumanAge(15, 15)).toStrictEqual([1, 1]);
+  });
+
+  test('should be converted 2 agruments into human years with value 1 - boundary value', () => {
+    expect(getHumanAge(23, 23)).toStrictEqual([1, 1]);
+  });
+
+  test('should be converted 2 agruments into human years with value 2', () => {
+    expect(getHumanAge(24, 24)).toStrictEqual([2, 2]);
+  });
+
+  test('should be converted 2 agruments into human years with value 2 - boundary value', () => {
+    expect(getHumanAge(27, 28)).toStrictEqual([2, 2]);
+  });
+
+  test('should be converted 2 agruments into human years with value 3', () => {
+    expect(getHumanAge(28, 29)).toStrictEqual([3, 3]);
+  });
+
+  test('should be converted 2 agruments into human years with value 4', () => {
+    expect(getHumanAge(32, 34)).toStrictEqual([4, 4]);
+  });
+
+  test('should be converted 2 agruments into human years with value 5', () => {
+    expect(getHumanAge(36, 39)).toStrictEqual([5, 5]);
+  });
+
+  test('should be converted 2 agruments into human years with value 6', () => {
+    expect(getHumanAge(40, 45)).toStrictEqual([6, 6]);
+  });
+
   test('should be NaN if aguments is not a number ', () => {
     expect(getHumanAge('two', 'one')).toStrictEqual([NaN, NaN]);
   });
