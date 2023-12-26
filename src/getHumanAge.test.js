d673fa492e9538ce47777198af6b8a13d42385cd
Nazar Kulchevych <nazar.kulchevych@gmail.com>
2023-12-26 13:35:43 +0200

    tests were written for getHumanAge function

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,31 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should be array', () => {
+    expect(getHumanAge())
+      .toBeInstanceOf(Array);
+  });
+
+  test('should return 0 if both values are < 15', () => {
+    expect(getHumanAge(2, 5)).toEqual([0, 0]);
+  });
+
+  test('should return 1 if both values are between 15 and 23', () => {
+    expect(getHumanAge(17, 23)).toEqual([1, 1]);
+  });
+
+  test(`should convert every 4 next cat years from 24 like 1`, () => {
+    expect(getHumanAge(28, 25)).toEqual([3, 2]);
+  });
+
+  test(`should convert every 5 next dog years from 24 like 1`, () => {
+    expect(getHumanAge(28, 29)).toEqual([3, 3]);
+  });
+
+  test(`should return 21 for catAge 
+    and 17 for dogAge if both values are 100`,
+  () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
 });
