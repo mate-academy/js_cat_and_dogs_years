838adee3eb1427af6c070207bad04ee939fc13ec
Anna Mahda <148216090+AnnaMahda@users.noreply.github.com>
2023-12-29 22:37:23 +0200

    getHumanAge.test.js

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,3 +1,8 @@
+'use strict';
+
+  describe('getHumanAge', () => {
+    const { getHumanAge } = require('./getHumanAge');
+    test('should be declared', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
