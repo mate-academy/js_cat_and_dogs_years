f99afaed799526f246c91b73ee02a6b12b25157c
Alina <aliska.nofear@gmail.com>
2024-02-06 18:00:49 +0200

    add tests for getHumanAge function

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,34 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('Should return 0 value if catsAge < 15', () => {
+    expect(getHumanAge(14, 15))
+      .toEqual([0, 1]);
+  });
+
+  test('Should return 0 value if dogsAge < 15', () => {
+    expect(getHumanAge(15, 13))
+      .toEqual([1, 0]);
+  });
+
+  test('Should return 0 values if dogsAge === 0 and catsAge === 0', () => {
+    expect(getHumanAge(0, 0))
+      .toEqual([0, 0]);
+  });
+
+  test('Should return an equal years after converted to human years', () => {
+    expect(getHumanAge(35, 20))
+      .toEqual([4, 1]);
+  });
+
+  test('Should return different human ages for cats and dogs if it is equal 28 cats and dogs age', () => {
+    expect(getHumanAge(28, 28))
+      .toEqual([3, 2]);
+  });
+
+  test('Should return different human ages for cats and dogs if it is equal 100 cats and dogs age', () => {
+    expect(getHumanAge(100, 100))
+      .toEqual([21, 17]);
+  });
 });
