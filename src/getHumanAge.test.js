57c247ddc1a757a5adeeed890c5abe10ad3e59c8
Dmytro <dped1986@gmail.com>
2024-02-07 19:03:27 +0200

    Solution_2

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,35 +8,43 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test(`should return 0, 0 because the value is less than 15`, () => {
+  test(`should return 0 for cat and dog
+   when they are less than 15 years old`, () => {
     expect(getHumanAge(0, 0)).toStrictEqual([0, 0]);
   });
 
-  test(`should return 0, 0 because the value is less than 15`, () => {
+  test(`should return 0 for cat and dog
+   when they are less than 15 years old`, () => {
     expect(getHumanAge(14, 14)).toStrictEqual([0, 0]);
   });
 
-  test(`should return 1, 1 because the value is less than 24`, () => {
+  test(`should return 1 for cat and dog
+   when they are less than 24 years old`, () => {
     expect(getHumanAge(15, 15)).toStrictEqual([1, 1]);
   });
 
-  test(`should return 1, 1 because the value is less than 24`, () => {
+  test(`should return 1 for cat and dog
+   when they are less than 24 years old`, () => {
     expect(getHumanAge(23, 23)).toStrictEqual([1, 1]);
   });
 
-  test(`should return 2, 2 because the value is less than 28 and 29`, () => {
+  test(`should return 2 for cat and dog
+   when they are less than 28 and 29 years old`, () => {
     expect(getHumanAge(24, 24)).toStrictEqual([2, 2]);
   });
 
-  test(`should return 2, 2 because the value is less than 28 and 29`, () => {
+  test(`should return 2 for cat and dog
+   when they are less than 28 and 29 years old`, () => {
     expect(getHumanAge(27, 27)).toStrictEqual([2, 2]);
   });
 
-  test(`should return 3, 2 because the value is less than 33 and 29`, () => {
+  test(`should return 2 for cat and 3 for dog
+  when they are less than 28 and 33 years old`, () => {
     expect(getHumanAge(28, 28)).toStrictEqual([3, 2]);
   });
 
-  test(`should return 21, 17 because the value is 100, 100`, () => {
+  test(`should return 21 for cat and 17 for dog
+  when they are less than 101 and 101 years old`, () => {
     expect(getHumanAge(100, 100)).toStrictEqual([21, 17]);
   });
 });
