c386e94fe20b89c1bd292c7d54bc25a0bb2ecdac
Olena Navrotska <elena.navr@gmail.com>
2024-02-07 20:39:26 +0200

    olena

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -2,26 +2,35 @@
 
 const getHumanAge = require('./getHumanAge');
 
-it('should be return 0 for cat and dog when they are less 15', () => {
-    expect(getHumanAge(14, 14)).toBe(0, 0);
-});
 
-it('should return 1 for cat and dog when 15-23', () => {
-   expect(getHumanAge(15, 15)).toBe(1, 1);
-});
+  test('should return 1 when age is 15', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
 
-it('should return 2 for cat when 24-28 ', () => {
-    expect(getHumanAge(24, 24)).toBe(2, 2);
-});
+  test('should return 0 when age is <15', () => {
+    expect(getHumanAge(12, 12)).toEqual([0, 0]);
+  });
 
-it('should return 2 for cat when 24-28 ', () => {
-    expect(getHumanAge(28, 28)).toBe(2, 2);
-});
- 
-it('should return 2 for dog when 25-28 ', () => {
-    expect(getHumanAge(25, 25)).toBe(2, 2);
-});
-  
-it('should return 2 for dog when 25-28 ', () => {
-    expect(getHumanAge(25, 25)).toBe(2, 2);
-});
+  test('should return 1 when cat age is >=15 and <24', () => {
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
+  test('should return 2 when cat age is >=24 and <28', () => {
+        expect(getHumanAge(27, 15)).toEqual([2, 1]);
+  });
+
+  test('should return 3 when cat age is 28', () => {
+    expect(getHumanAge(28, 15)).toEqual([3, 1]);
+  });
+
+  test('should return 4 when cat age is 29', () => {
+     expect(getHumanAge(29, 16)).toEqual([3, 1]);
+  });
+
+  test('should return 1 when dog age <=15', () => {
+    expect(getHumanAge(15, 23)).toEqual([1, 1]);
+  });
+
+  test('should return 2 when dog age is >15 and <23', () => {
+    expect(getHumanAge(16, 25)).toEqual([1, 2]);
+  });
