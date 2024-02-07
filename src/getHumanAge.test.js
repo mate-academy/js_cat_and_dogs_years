ce06a80a0362d1d7127872f311aebfbcb60c5aa7
Oksana Kachmar <profiscribe@gmail.com>
2024-02-07 17:18:43 +0200

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,36 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return [0, 0] when animalgAge are 0', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('should return [0, 0] when animalAge < than first', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test('should return [1, 1] when animalAge are at the first', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  test('should return [1, 1] animalAge = between the first and second', () => {
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
+  test('should return [2, 2] when animalAge are at the second', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+
+  test('should return [2, 2] animalAge = between the second and third', () => {
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
+
+  test('should return [3, 2] catAge > third, dogAge = third threshold', () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+  });
+
+  test('should return [21, 17] when catAge is 100 and dogAge is 100', () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
 });
