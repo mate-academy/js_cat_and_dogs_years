0b9feb435419611d154a41ab06d5f1a75e62d6ae
Anatolii Zinevych <anatolii.zinevych@gmail.com>
2024-02-07 19:18:47 +0200

    add some tests

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -3,8 +3,6 @@
 const { getHumanAge } = require('./getHumanAge');
 
 describe('getHumanAge', () => {
-  
-
   test('should be declared', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
@@ -17,8 +15,6 @@ test('should return human ages for cat and dog', () => {
   expect(getHumanAge(28, 29)).toEqual([3, 3]);
 
   expect(getHumanAge(32, 34)).toEqual([4, 4]);
-
-  expect(getHumanAge(36, 39)).toEqual([6, 6]);
 
   expect(getHumanAge(71, 67)).toEqual([29, 25]);
 });
