45d2688838605c96bd319861a765a5c591a1d50b
Olena Navrotska <elena.navr@gmail.com>
2024-02-07 20:26:22 +0200

    First push

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,10 +1,27 @@
 'use strict';
 
-describe('getHumanAge', () => {
-  const { getHumanAge } = require('./getHumanAge');
+const getHumanAge = require('./getHumanAge');
 
-  test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
-  });
+it('should be return 0 for cat and dog when they are less 15', () => {
+    expect(getHumanAge(14, 14)).toBe(0, 0);
+});
+
+it('should return 1 for cat and dog when 15-23', () => {
+   expect(getHumanAge(15, 15)).toBe(1, 1);
+});
+
+it('should return 2 for cat when 24-28 ', () => {
+    expect(getHumanAge(24, 24)).toBe(2, 2);
+});
+
+it('should return 2 for cat when 24-28 ', () => {
+    expect(getHumanAge(28, 28)).toBe(2, 2);
+});
+ 
+it('should return 2 for dog when 25-28 ', () => {
+    expect(getHumanAge(25, 25)).toBe(2, 2);
+});
+  
+it('should return 2 for dog when 25-28 ', () => {
+    expect(getHumanAge(25, 25)).toBe(2, 2);
 });
