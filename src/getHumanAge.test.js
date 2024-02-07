9b40900cdf7303011b63c614904063fb20804857
Tetiana Maksiuk <TetianaMaksiuk@gmail.com>
2024-02-07 15:00:59 +0200

    Implement test for getHumanAge

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,18 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should validate conversion of cat and dog ages to human years', () => {
+    const expectedForCats = [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3];
+    const expectedForDogs = [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3];
+    const inputCats = [0, 1, 14, 15, 16, 23, 24, 25, 27, 28, 29];
+    const inputDogs = [0, 1, 14, 15, 16, 23, 24, 25, 28, 29, 30];
+
+    for (let i = 0; i < inputCats.length; i++) {
+      const result = getHumanAge(inputCats[i], inputDogs[i]);
+
+      expect(result[0]).toBe(expectedForCats[i]);
+      expect(result[1]).toBe(expectedForDogs[i]);
+    }
+  });
 });
