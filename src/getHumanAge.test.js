f5b1ae80feb9a6499295ede500d2367353cd4b8d
Вероніка <veronikadzul@gmail.com>
2024-02-21 16:02:36 +0200

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,44 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test(`should return 0 for cat and dog
+   when they are less than 15 years old`, () => {
+    expect(getHumanAge(0, 0)).toStrictEqual([0, 0]);
+  });
+
+  test(`should return 0 for cat and dog
+   when they are less than 15 years old`, () => {
+    expect(getHumanAge(14, 14)).toStrictEqual([0, 0]);
+  });
+
+  test(`should return 1 for cat and dog
+   when they are less than 24 years old`, () => {
+    expect(getHumanAge(15, 15)).toStrictEqual([1, 1]);
+  });
+
+  test(`should return 1 for cat and dog
+   when they are less than 24 years old`, () => {
+    expect(getHumanAge(23, 23)).toStrictEqual([1, 1]);
+  });
+
+  test(`should return 2 for cat and dog
+   when they are less than 28 and 29 years old`, () => {
+    expect(getHumanAge(24, 24)).toStrictEqual([2, 2]);
+  });
+
+  test(`should return 2 for cat and dog
+   when they are less than 28 and 29 years old`, () => {
+    expect(getHumanAge(27, 27)).toStrictEqual([2, 2]);
+  });
+
+  test(`should return 2 for cat and 3 for dog
+   when they are less than 28 and 33 years old`, () => {
+    expect(getHumanAge(28, 28)).toStrictEqual([3, 2]);
+  });
+
+  test(`should return 21 for cat and 17 for dog
+   when they are less than 101 and 101 years old`, () => {
+    expect(getHumanAge(100, 100)).toStrictEqual([21, 17]);
+  });
 });
