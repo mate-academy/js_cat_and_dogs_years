a0a2c6bfb7bd022b6c6c1eea558c317fe636debd
bla2001 <blasmartlab@gmail.com>
2024-03-06 12:57:03 +0200

    Tests

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,76 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return an array', () => {
+    const result = getHumanAge(1, 5);
+
+    expect(result)
+      .toBeInstanceOf(Array);
+  });
+
+  test('should convert cats age and dogs age to humans age', () => {
+    const result = getHumanAge(15, 30);
+
+    expect(result).toEqual([1, 3]);
+  });
+
+  test('should return 0 if animal age is 0', () => {
+    const result = getHumanAge(0, 0);
+
+    expect(result).toEqual([0, 0]);
+  });
+
+  test('should return 0 if animal age is less to 15 years', () => {
+    const result = getHumanAge(14, 14);
+
+    expect(result).toEqual([0, 0]);
+  });
+
+  test('should return 1 if animal age equal to 15 years', () => {
+    const result = getHumanAge(15, 15);
+
+    expect(result).toEqual([1, 1]);
+  });
+
+  test(`should return 1 if animal age is greater 
+      then 15 and less then 24 years`, () => {
+    const result = getHumanAge(23, 23);
+
+    expect(result).toEqual([1, 1]);
+  });
+
+  test('should return 2 if animal age is equal to 24 years', () => {
+    const result = getHumanAge(24, 24);
+
+    expect(result).toEqual([2, 2]);
+  });
+
+  test(`should return 2 if animal age is greater 
+        then 24 and less then 28 years`, () => {
+    const result = getHumanAge(27, 27);
+
+    expect(result).toEqual([2, 2]);
+  });
+
+  test(`should return right calculated human age 
+        for cats whose age is equal to 28 years`, () => {
+    const result = getHumanAge(28, 28);
+
+    expect(result).toEqual([3, 2]);
+  });
+
+  test(`should return right calculated human age 
+        for dogs whose age is equal to 29 years`, () => {
+    const result = getHumanAge(29, 29);
+
+    expect(result).toEqual([3, 3]);
+  });
+
+  test(`should return right calculated human age 
+        for animal whose age greater then 29`, () => {
+    const result = getHumanAge(100, 100);
+
+    expect(result).toEqual([21, 17]);
+  });
 });
