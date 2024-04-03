4ddfcf070a653f3bf2819d4971f7f1102b31954a
davatrava <davatrava@gmail.com>
2024-04-03 13:43:42 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,10 +1,25 @@
 'use strict';
 
-describe('getHumanAge', () => {
-  const { getHumanAge } = require('./getHumanAge');
+const { getHumanAge } = require('./getHumanAge');
 
-  test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+describe('getHumanAge', () => {
+  test('should correctly convert cat and dog ages to human years', () => {
+    expect(getHumanAge(28, 24)).toEqual([3, 2]);
+  });
+
+  test('should handle cases when catAge or dogAge is less than 15', () => {
+    expect(getHumanAge(10, 10)).toEqual([0, 0]);
+  });
+
+  test('should be with expected results', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+
+  test('should be with expected results', () => {
+    expect(getHumanAge(30, 28)).toEqual([3, 2]);
+  });
+
+  test('should be with expected results', () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
