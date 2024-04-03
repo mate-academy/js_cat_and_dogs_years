f110589253514e8d76567f6815087f25fb62cc19
MarkelKoy <demagog1231@gmail.com>
2024-04-03 13:25:41 +0300

    Hi

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,10 +1,20 @@
 'use strict';
 
-describe('getHumanAge', () => {
-  const { getHumanAge } = require('./getHumanAge');
+const { getHumanAge } = require('./getHumanAge');
 
-  test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+describe('getHumanAge', () => {
+  it('should return correct human ages for cat and dog', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
+
+  it('should be declared', () => {
+    expect(getHumanAge).toBeInstanceOf(Function);
   });
 });
