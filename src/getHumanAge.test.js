a57e1392a327479538f1916fd62e694b9d714d2e
Sandra <malinevskaqa@gmail.com>
2024-04-03 18:10:56 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -4,7 +4,70 @@ describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
   test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+});
+
+describe('getHumanAge', () => {
+  const { getHumanAge } = require('./getHumanAge');
+
+  test('should be declared', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+});
+
+describe('getHumanAge', () => {
+  const { getHumanAge } = require('./getHumanAge');
+
+  test('should be declared', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+});
+
+describe('getHumanAge', () => {
+  const { getHumanAge } = require('./getHumanAge');
+
+  test('should be declared', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+});
+
+describe('getHumanAge', () => {
+  const { getHumanAge } = require('./getHumanAge');
+
+  test('should be declared', () => {
+    expect(getHumanAge(16, 16)).toEqual([1, 1]);
+  });
+});
+
+describe('getHumanAge', () => {
+  const { getHumanAge } = require('./getHumanAge');
+
+  test('should be declared', () => {
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+});
+
+describe('getHumanAge', () => {
+  const { getHumanAge } = require('./getHumanAge');
+
+  test('should be declared', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+});
+
+describe('getHumanAge', () => {
+  const { getHumanAge } = require('./getHumanAge');
+
+  test('should be declared', () => {
+    expect(getHumanAge(25, 25)).toEqual([2, 2]);
+  });
+});
+
+describe('getHumanAge', () => {
+  const { getHumanAge } = require('./getHumanAge');
+
+  test('should be declared', () => {
+    expect(getHumanAge(27, 28)).toEqual([2, 2]);
   });
 });
