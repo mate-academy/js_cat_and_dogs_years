94d10648b7dee90d9b52afec9b6ea9923e698bb5
MarkelKoy <demagog1231@gmail.com>
2024-04-04 18:13:32 +0300

    Hi bro

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,54 +8,43 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test('should return 0 human years for every 0 years of animals', () => {
-    expect(getHumanAge(0, 0))
-      .toEqual([0, 0]);
+  test('should return an array', () => {
+    expect(getHumanAge(0, 0)).toBeInstanceOf(Array);
   });
 
-  test('should return 1 human year for first 15 years of animals', () => {
-    expect(getHumanAge(15, 15))
-      .toEqual([1, 1]);
+  test('should return 0 for 0 input', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
   });
 
-  test('should return 2 human year for first 24 years of animals', () => {
-    expect(getHumanAge(24, 24))
-      .toEqual([2, 2]);
+  test('should return 0 for input less than 15', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
-  test(`should count third and more human years
-    as 4 for cats and 5 for dogs`, () => {
-    expect(getHumanAge(40, 39))
-      .toEqual([6, 5]);
+  test('should return 1 for input 15', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
   });
 
-  test(`should round down human years
-    if years of animals does not reach an integer value`, () => {
-    expect(getHumanAge(1, 1))
-      .toEqual([0, 0]);
+  test('should return 1 for input less than 24', () => {
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
 
-    expect(getHumanAge(14, 14))
-      .toEqual([0, 0]);
+  test('should return 2 for input 24', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
 
-    expect(getHumanAge(16, 16))
-      .toEqual([1, 1]);
+  test('should return 2 for input less than 28', () => {
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
 
-    expect(getHumanAge(23, 23))
-      .toEqual([1, 1]);
+  test('should return [3, 2] for input 28', () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+  });
 
-    expect(getHumanAge(25, 25))
-      .toEqual([2, 2]);
+  test('should return 3 for input: cat - 28, dog - 29', () => {
+    expect(getHumanAge(28, 29)).toEqual([3, 3]);
+  });
 
-    expect(getHumanAge(27, 28))
-      .toEqual([2, 2]);
-
-    expect(getHumanAge(29, 30))
-      .toEqual([3, 3]);
-
-    expect(getHumanAge(31, 33))
-      .toEqual([3, 3]);
-
-    expect(getHumanAge(102, 100))
-      .toEqual([21, 17]);
+  test('should return [21, 17] for input 100', () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
