b17e564ee6cee4f9a431358041f55fd9d1b6e84e
Tanya <tmfancy@ukr.net>
2024-04-04 17:57:13 +0300

    Solution4

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -9,34 +9,52 @@ describe('getHumanAge', () => {
   });
 });
 
-test('should be with expected results', () => {
-  expect(getHumanAge(0, 0)).toEqual([0, 0]);
+describe('getHumanAge', () => {
+  test('should return 0 for cat and dog when they are 0 years old', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
 });
 
-test('should be with expected results', () => {
-  expect(getHumanAge(14, 14)).toEqual([0, 0]);
+describe('getHumanAge', () => {
+  test('should return 0 for cat and dog when they are 14 years old', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
 });
 
-test('should be with expected results', () => {
-  expect(getHumanAge(15, 15)).toEqual([1, 1]);
+describe('getHumanAge', () => {
+  test('should return 1 for cat and dog when they are 15 years old', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
 });
 
-test('should be with expected results', () => {
-  expect(getHumanAge(23, 23)).toEqual([1, 1]);
+describe('getHumanAge', () => {
+  test('should return 1 for cat and dog when they are 23 years old', () => {
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
 });
 
-test('should be with expected results', () => {
-  expect(getHumanAge(24, 24)).toEqual([2, 2]);
+describe('getHumanAge', () => {
+  test('should return 2 for cat and dog when they are 24 years old', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
 });
 
-test('should be with expected results', () => {
-  expect(getHumanAge(27, 27)).toEqual([2, 2]);
+describe('getHumanAge', () => {
+  test('should return 2 for cat and dog when they are 27 years old', () => {
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
 });
 
-test('should be with expected results', () => {
-  expect(getHumanAge(28, 28)).toEqual([3, 2]);
+describe('getHumanAge', () => {
+  test('should return 3 for cats and 2 for dogs when they are 14 years old',
+   () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+  });
 });
 
-test('should be with expected results', () => {
-  expect(getHumanAge(100, 100)).toEqual([21, 17]);
+describe('getHumanAge', () => {
+  test('should return 21 for cats and 17 for dogs when they are 100 years old',
+   () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
 });
