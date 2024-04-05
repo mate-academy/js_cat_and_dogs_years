72251eaa610ddb6088a4cab1453902ce3fe64afe
Maksymm <makshack10@gmail.com>
2024-04-05 11:05:00 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,20 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should correctly convert cat and dog ages to human ages', () => {
+    expect(getHumanAge(0, 0)).toStrictEqual([0, 0]);
+
+    expect(getHumanAge(15, 15)).toStrictEqual([1, 1]);
+
+    expect(getHumanAge(23, 23)).toStrictEqual([1, 1]);
+
+    expect(getHumanAge(24, 24)).toStrictEqual([2, 2]);
+
+    expect(getHumanAge(27, 27)).toStrictEqual([2, 2]);
+
+    expect(getHumanAge(28, 28)).toStrictEqual([3, 2]);
+
+    expect(getHumanAge(100, 100)).toStrictEqual([21, 17]);
+  });
 });
