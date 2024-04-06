38f8ae58a74bb01bb70b692c74d82d1a7850df96
Oleksandr Sherashov <apbreloaded947@gmail.com>
2024-04-06 19:41:40 +0300

    completed task

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,10 +1,49 @@
 'use strict';
 
-describe('getHumanAge', () => {
+describe('./getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
-  test('should be declared', () => {
+  test(`should be declared`, () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
+  });
+
+  test(`should be 0 values if arguments are less than 15`, () => {
+    const result = getHumanAge(0, 0);
+
+    expect(result).toEqual([0, 0]);
+  });
+
+  test(`should be 0 values if arguments are less than 15`, () => {
+    const result = getHumanAge(12, 13);
+
+    expect(result).toEqual([0, 0]);
+  });
+
+  test(`should be 1 human year if dog and cats years are 15`, () => {
+    const result = getHumanAge(15, 15);
+
+    expect(result).toEqual([1, 1]);
+  });
+
+  test(`should be 1 human year if dog and cats years
+      are greater than 15 and less than 24 animal years`, () => {
+    const result = getHumanAge(21, 22);
+
+    expect(result).toEqual([1, 1]);
+  });
+
+  test(`should be 2 human years if dog and cats years
+      are greater or equal than 24 animal years`, () => {
+    const result = getHumanAge(24, 24);
+
+    expect(result).toEqual([2, 2]);
+  });
+
+  test(`should add 1 human years for every 4 cat years after 24 animal years
+      and 1 human year for every 5 dog years after 24 animal years`, () => {
+    const result = getHumanAge(100, 100);
+
+    expect(result).toEqual([21, 17]);
   });
 });
