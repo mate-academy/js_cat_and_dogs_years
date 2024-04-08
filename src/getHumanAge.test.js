c29d440072f5e6abc936f1a60aaa8254cfe3fcdd
kvoitsekhovska <voitsekhovskakateryna@gmail.com>
2024-04-08 20:02:27 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -3,11 +3,11 @@
 describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
-  test('should be declared', () => {
+  test('should be a function', () => {
     expect(getHumanAge).toBeInstanceOf(Function);
   });
 
-  test('should return [0, 0] when catAge and dogAge are 0', () => {
+  test('should return [0, 0] when catAge and dogAge are both 0', () => {
     expect(getHumanAge(0, 0)).toEqual([0, 0]);
   });
 
@@ -15,8 +15,20 @@ describe('getHumanAge', () => {
     expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
-  test('should return [2, 2] when catAge and dogAge are 27', () => {
-    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  test('should return [1, 1] when catAge and dogAge are exactly 15', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  test('should return [2, 2] when catAge and dogAge are both 18', () => {
+    expect(getHumanAge(18, 18)).toEqual([2, 2]);
+  });
+
+  test('should return [2, 2] when catAge and dogAge are both 23', () => {
+    expect(getHumanAge(23, 23)).toEqual([2, 2]);
+  });
+
+  test('should return [2, 2] when catAge is 24 and dogAge is 24', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
 
   test('should return [3, 2] when catAge is 28 and dogAge is 28', () => {
