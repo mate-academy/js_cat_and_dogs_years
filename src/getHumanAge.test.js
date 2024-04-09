7d2684d0c4176d61dbc679d975bff9ff1eff7b39
Olha Myrutenko <ovka888@gmail.com>
2024-04-09 21:18:23 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,14 +8,25 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test('should convert cat and dog age to human age', () => {
-    expect(getHumanAge(0, 0)).toEqual([0, 0]);
-    expect(getHumanAge(14, 14)).toEqual([0, 0]);
-    expect(getHumanAge(15, 15)).toEqual([1, 1]);
-    expect(getHumanAge(23, 23)).toEqual([1, 1]);
-    expect(getHumanAge(24, 24)).toEqual([2, 2]);
-    expect(getHumanAge(27, 27)).toEqual([2, 2]);
-    expect(getHumanAge(28, 28)).toEqual([3, 2]);
-    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  test('getHumanAge should return human age of a cat', () => {
+    expect(getHumanAge(0, 0)[0]).toBe(0);
+    expect(getHumanAge(14, 14)[0]).toBe(0);
+    expect(getHumanAge(15, 15)[0]).toBe(1);
+    expect(getHumanAge(23, 23)[0]).toBe(1);
+    expect(getHumanAge(24, 24)[0]).toBe(2);
+    expect(getHumanAge(27, 27)[0]).toBe(2);
+    expect(getHumanAge(28, 28)[0]).toBe(3);
+    expect(getHumanAge(100, 100)[0]).toBe(21);
+  });
+
+  test('getHumanAge should return human age of a dog', () => {
+    expect(getHumanAge(0, 0)[1]).toBe(0);
+    expect(getHumanAge(14, 14)[1]).toBe(0);
+    expect(getHumanAge(15, 15)[1]).toBe(1);
+    expect(getHumanAge(23, 23)[1]).toBe(1);
+    expect(getHumanAge(24, 24)[1]).toBe(2);
+    expect(getHumanAge(27, 27)[1]).toBe(2);
+    expect(getHumanAge(28, 28)[1]).toBe(2);
+    expect(getHumanAge(100, 100)[1]).toBe(17);
   });
 });
