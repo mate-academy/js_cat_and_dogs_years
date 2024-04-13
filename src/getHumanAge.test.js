adc03f66979241d43e1ec9fdc59e481f0df633a2
Kateryna Nedobytko <knedobytko@gmail.com>
2024-04-13 20:36:01 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,22 +8,22 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test('should return 0 if "catAge" & "dogAge" < 15', () => {
+  test('should return [0, 0] if "catAge" & "dogAge" < 15', () => {
     expect(getHumanAge(12, 11))
       .toEqual([0, 0]);
   });
 
-  test('shoud return 1 if 15 => "catAge" & "dogAge" =<23', () => {
+  test('shoud return [1, 1] if 15 => "catAge" & "dogAge" =<23', () => {
     expect(getHumanAge(15, 23))
       .toEqual([1, 1]);
   });
 
-  test('returns 2+Math.floor((animAge - 24)/4(for cats), 5(for dogs))', () => {
+  test('should return [27, 17] if catAge = 127 and dogAge = 100', () => {
     expect(getHumanAge(127, 100))
       .toEqual([27, 17]);
   });
 
-  test('returns 2+Math.floor((animAge - 24)/4(for cats), 5(for dogs))', () => {
+  test('should return [4, 12] if catAge = 35 and dogAge = 76', () => {
     expect(getHumanAge(35, 76))
       .toEqual([4, 12]);
   });
