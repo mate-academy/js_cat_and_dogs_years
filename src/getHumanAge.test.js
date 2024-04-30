afc21c062b5f7c0a019f24b595b7af2979f89e3e
Dmytro <dakorozer@gmail.com>
2024-04-30 12:00:48 +0300

    corrected

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,26 +8,35 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
   
-  test('animals ages less than 15', () => {
+  test('should return 0 for cat and dog when they are 0 years old', () => {
     expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('should return 0 for cat and dog when they are 14 years old', () => {
     expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
   
-  test('animals ages between 15 and 24', () => {
+  test('should return 1 for cat and dog when they are 15 years old', () => {
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+  
+  test('should return 1 for cat and dog when they are 23 years old', () => {
     expect(getHumanAge(23, 23)).toEqual([1, 1]);
   });
   
-  test('animals ages between 25 and 28', () => {
+  test('should return 2 for cat and dog when they are 24 years old', () => {
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+  
+  test('should return 2 for cat and dog when they are 27 years old', () => {
     expect(getHumanAge(27, 27)).toEqual([2, 2]);
   });
   
-  test('cat age is 28, dog age is 28', () => {
+  test('should return 3 for cat and 2 for dog when they are 28 years old', () => {
     expect(getHumanAge(28, 28)).toEqual([3, 2]);
   });
   
-  test('animals ages more than 28', () => {
+  test('should return 21 for cat and 17 for dog when they are 100 years old', () => {
     expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
