36393ce90d52c104ae845fdb4f5a584d17ef013d
Rozdorozhnii <rozdorozhnii@ukr.net>
2024-05-17 11:29:22 +0300

    Solution_v1.1

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,67 +1,71 @@
 'use strict';
 
-describe('getHumanAge', () => {
+describe(`getHumanAge`, () => {
   const { getHumanAge } = require('./getHumanAge');
 
-  test('should be declared', () => {
+  test(`should be declared`, () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
 
-  test('should be array', () => {
+  test(`should be array`, () => {
     expect.arrayContaining(getHumanAge(15, 15));
   });
 
-  test('should be array with length 2', () => {
+  test(`should be array with length 2`, () => {
     expect(getHumanAge(15, 15))
       .toHaveLength(2);
   });
 
-  test('should return 0 values with 0 arguments', () => {
+  test(`should return 0 values with 0 arguments`, () => {
     expect(getHumanAge(0, 0))
       .toEqual([0, 0]);
   });
 
-  test('should return 0 values with arguments less '
-  + 'than 15 in both cases', () => {
+  test(`should return 0 for a cat and dog `
+  + `when they are 14 years old`, () => {
     expect(getHumanAge(14, 14))
       .toEqual([0, 0]);
   });
 
-  test('should return 1 values with arguments more or '
-  + 'equal than 15 in both cases', () => {
+  test(`should return 1 for a cat and dog `
+  + `when they are 15 years old`, () => {
     expect(getHumanAge(15, 15))
       .toEqual([1, 1]);
   });
 
-  test('should return 1 values with arguments less '
-  + 'than 24 in both cases', () => {
+  test(`should return 1 for a cat and dog `
+  + `when they are 23 years old`, () => {
     expect(getHumanAge(23, 23))
       .toEqual([1, 1]);
   });
 
-  test('should return 2 values with arguments more or '
-  + 'equal than 24 in both cases', () => {
+  test(`should return 2 for a cat and dog `
+  + `when they are 24 years old`, () => {
     expect(getHumanAge(24, 24))
       .toEqual([2, 2]);
   });
 
-  test('should return 2 values with arguments 27 and 28', () => {
+  test(`should return 2 for a cat and dog when `
+  + `cat has 27 years old and dog has 28 years old`, () => {
     expect(getHumanAge(27, 28))
       .toEqual([2, 2]);
   });
 
-  test('should return 3 values with arguments 28 and 29', () => {
+  test(`should return 3 for a cat and dog when `
+  + `cat has 28 years old and dog has 29 years old`, () => {
     expect(getHumanAge(28, 29))
       .toEqual([3, 3]);
   });
 
-  test('should return 4 values with arguments 32 and 34', () => {
+  test(`should return 4 for a cat and dog when `
+  + `cat has 32 years old and dog has 34 years old`, () => {
     expect(getHumanAge(32, 34))
       .toEqual([4, 4]);
   });
 
-  test('should return 21 and 17 values with arguments 100', () => {
+  test(`should return 21 and 17 for a cat and dog `
+  + `when they are 100 years old`, () => {
     expect(getHumanAge(100, 100))
       .toEqual([21, 17]);
   });
