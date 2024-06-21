71cb88c015f91cdd0eaac55ae93ed08b5fa89173
MarianaSlotylo <slotulomaryana@gmail.com>
2024-06-21 14:36:00 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,35 +7,35 @@ describe('getHumanAge', () => {
     expect(getHumanAge).toBeInstanceOf(Function);
   });
 
-  test('should return [0, 0] for cat age 0 and dog age 0', () => {
+  test('should be return 0 if catAge or dogAge is equal 0', () => {
     expect(getHumanAge(0, 0)).toEqual([0, 0]);
   });
 
-  test('should return [0, 0] for cat age 14 and dog age 14]', () => {
+  test('should be return a right value for first 14 cat & dog age]', () => {
     expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
-  test('should return [1, 1] for cat age 15 and dog age 15]', () => {
+  test('should be return a right value for first 15 cat & dog age]', () => {
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
   });
 
-  test('should return [1, 1] for cat age 23 and dog age 23]', () => {
+  test('should be return a right value for first 23 cat & dog age]', () => {
     expect(getHumanAge(23, 23)).toEqual([1, 1]);
   });
 
-  test('should return [2, 2] for cat age 24 and dog age 24]', () => {
+  test('should be return a right value for first 24 cat & dog age]', () => {
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
 
-  test('should return [2, 2] for cat age 27 and dog age 27]', () => {
+  test('should be return a right value for first 27 cat & dog age]', () => {
     expect(getHumanAge(27, 27)).toEqual([2, 2]);
   });
 
-  test('should return [3, 2] for cat age 28 and dog age 28]', () => {
+  test('should be return a right value for first 28 cat & dog age]', () => {
     expect(getHumanAge(28, 28)).toEqual([3, 2]);
   });
 
-  test('should return [21, 17] for cat age 100 and dog age 100]', () => {
+  test('should be return a right value for first 100 cat & dog age]', () => {
     expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
