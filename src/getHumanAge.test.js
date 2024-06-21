19e73666de2df6572f75c5ed2f5cb1a200388126
MariiaKalezhniuk <mzaharova318@gmail.com>
2024-06-21 17:25:34 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,36 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return 0 if catAge or dogAge equils 0', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('should return right result if catAge or dogAge equils 14', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test('should return right result if catAge or dogAge equils 15', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  test('should return right result if catAge or dogAge equils 23', () => {
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
+  test('should return right result if catAge or dogAge equils 24', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+
+  test('should return right result if catAge or dogAge equils 27', () => {
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
+
+  test('should return right result if catAge or dogAge equils 28', () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+  });
+
+  test('should return right result if catAge or dogAge equils 100', () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
 });
