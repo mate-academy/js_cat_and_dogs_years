17ea9cd99d06198ec15abc42aacd65e2f2fe5ee6
Khrystyna Kazva <kristinakazva@gmail.com>
2024-06-21 16:51:23 +0300

    Add tests for getHumanAge function

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,24 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return [0, 0] for [0, 0]', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('should return [1, 1] for [15, 15]', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  test('should return correct human age for various cat and dog ages', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
+
+  test('should handle edge cases correctly', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
 });
