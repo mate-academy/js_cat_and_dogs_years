7686e0ba692f01bb096678a193143dd52929de6c
MishaKluch <nimamiha@gmail.com>
2024-06-21 22:50:22 +0300

    Сorrection of headers

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -9,31 +9,31 @@ describe('getHumanAge', () => {
   });
 
   test(
-    'should calculate human yearscorrectly for cat and dog ages up to 15 years',
+    'should return [0, 0] for catAge 10 and dogAge 10',
     () => {
       expect(getHumanAge(10, 10)).toEqual([0, 0]);
     });
 
   test(
-    'should calculate human yearscorrectly for cat and dog ages up to 24 years',
+    'should return [1, 1] for catAge 20 and dogAge 20',
     () => {
       expect(getHumanAge(20, 20)).toEqual([1, 1]);
     });
 
   test(
-    'should calculate human yearscorrectly for cat and dog ages over 24 years',
+    'should return [2, 2] for catAge 25 and dogAge 25',
     () => {
       expect(getHumanAge(25, 25)).toEqual([2, 2]);
     });
 
   test(
-    'should calculate human yearscorrectly for cat and dog ages over 28 years',
+    'should return [3, 2] for catAge 28 and dogAge 28',
     () => {
       expect(getHumanAge(28, 28)).toEqual([3, 2]);
     });
 
   test(
-    'should calculate human yearscorrectly for cat and dog ages over 29 years',
+    'should return [21, 17] for catAge 100 and dogAge 100',
     () => {
       expect(getHumanAge(100, 100)).toEqual([21, 17]);
     });
