c75512c5f21b43d44f981c53e7eb27641b81fb7a
Olha Panchenko <panchenko-85@ukr.net>
2024-06-23 17:00:29 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,20 +1,24 @@
+/* eslint-disable max-len */
 'use strict';
 
 describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
-  test('should convert cat and dog years to human years correctly', () => {
+  test('should convert cat and dog years to human years correctly for typical cases', () => {
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
     expect(getHumanAge(30, 30)).toEqual([3, 3]);
   });
 
-  test('should handle edge cases', () => {
+  test('should return 0 for both cat and dog when their age is 0', () => {
     expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('should return 1 for both cat and dog when their age is exactly 15', () => {
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
   });
 
-  test('should return an array with two elements', () => {
+  test('should return an array with two elements for any valid input', () => {
     const result = getHumanAge(20, 20);
 
     expect(result).toBeInstanceOf(Array);
