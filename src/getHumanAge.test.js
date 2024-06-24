7a5d5a0f6b5b95a43357654e745e37abaec3af1d
Maryna <marish1235@gmail.com>
2024-06-24 13:33:43 +0300

    Fixed

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,8 +8,8 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test('cat age 10, dog age 10', () => {
-    expect(getHumanAge(10, 10)).toEqual([0, 0]);
+  test('cat age 14, dog age 14', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
   });
 
   test('cat age 15, dog age 15', () => {
@@ -20,24 +20,24 @@ describe('getHumanAge', () => {
     expect(getHumanAge(16, 16)).toEqual([1, 1]);
   });
 
+  test('cat age 23, dog age 23', () => {
+    expect(getHumanAge(16, 16)).toEqual([1, 1]);
+  });
+
   test('cat age 24, dog age 24', () => {
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
 
-  test('cat age 25, dog age 25', () => {
-    expect(getHumanAge(25, 25)).toEqual([2, 2]);
+  test('cat age 27, dog age 28', () => {
+    expect(getHumanAge(27, 28)).toEqual([2, 2]);
   });
 
-  test('cat age 29, dog age 29', () => {
-    expect(getHumanAge(29, 29)).toEqual([3, 3]);
+  test('cat age 28, dog age 29', () => {
+    expect(getHumanAge(28, 29)).toEqual([3, 3]);
   });
 
-  test('cat age 40, dog age 40', () => {
-    expect(getHumanAge(40, 40)).toEqual([6, 5]);
-  });
-
-  test('cat age 100, dog age 100', () => {
-    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  test('cat age 29, dog age 30', () => {
+    expect(getHumanAge(29, 30)).toEqual([3, 3]);
   });
 
   test('edge case: cat age 0, dog age 0', () => {
@@ -50,9 +50,5 @@ describe('getHumanAge', () => {
 
   test('edge case: cat age 9, dog age 15', () => {
     expect(getHumanAge(9, 15)).toEqual([0, 1]);
-  });
-
-  test('edge case: cat age 20, dog age 20', () => {
-    expect(getHumanAge(20, 20)).toEqual([1, 1]);
   });
 });
