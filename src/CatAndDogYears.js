22651283315c85f9f3e3bfded912b40e5a306c44
Ivanna Pavliuk <ivannap258@gmail.com>
2024-07-28 16:13:59 +0300

    Solution

---
+++ src/CatAndDogYears.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @param {number} catAge
+ * @param {number} dogAge
+ *
+ * @returns {number[]}
+ */
+function CatAndDogYears(catAge, dogAge) {
+  const catToHuman = convertToHuman(catAge, 15, 9, 4);
+  const dogToHuman = convertToHuman(dogAge, 15, 9, 5);
+
+  return [catToHuman, dogToHuman];
+}
+
+function convertToHuman(animalAge, first, second, other) {
+  if (animalAge < first) {
+    return 0;
+  }
+
+  if (animalAge < first + second) {
+    return 1;
+  }
+
+  return 2 + Math.floor((animalAge - first - second) / other);
+}
+
+module.exports = { CatAndDogYears };
