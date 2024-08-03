8096130e0b0d0d02e82141881509655482c4037c
Viacheslav Kovryzhykh <v.kovryzhnykh@gmail.com>
2024-08-03 16:14:40 +0200

    Address review comments: keep "use strict" and use describe in tests

--- src/getHumanAge.js
+++ src/getHumanAge.js
@@ -1,13 +1,9 @@
-"use strict";
+'use strict';
 
 /**
- * Converts cat and dog years to human years.
- *
- * @param {number} catAge - The cat's age in cat years.
- * @param {number} dogAge - The dog's age in dog years.
- * @returns {number[]} - An array where the first element is
- * the cat's age in human years,
- * and the second element is the dog's age in human years.
+ * @param {number} catAge
+ * @param {number} dogAge
+ * @returns {number[]}
  */
 function getHumanAge(catAge, dogAge) {
   const catToHuman = convertToHuman(catAge, 15, 9, 4);
@@ -16,15 +12,6 @@ function getHumanAge(catAge, dogAge) {
   return [catToHuman, dogToHuman];
 }
 
-/**
- * Converts animal age to human years.
- *
- * @param {number} animalAge - The animal's age in years.
- * @param {number} first - The first threshold in animal years.
- * @param {number} second - The second threshold in animal years.
- * @param {number} other - The additional years after the second threshold.
- * @returns {number} - The age in human years.
- */
 function convertToHuman(animalAge, first, second, other) {
   if (animalAge < first) {
     return 0;
