e3ad4142654ec53cd02d2070af8c7958fab9132f
Prytula Olexandr <liga5872@gmail.com>
2024-08-05 15:49:00 +0300

    add some cases

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -12,12 +12,17 @@ describe('getHumanAge', () => {
     expect(getHumanAge(0, 0)).toEqual([0, 0]);
     expect(getHumanAge(14, 14)).toEqual([0, 0]);
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
+    expect(getHumanAge(14, 16)).toEqual([0, 1]);
     expect(getHumanAge(23, 23)).toEqual([1, 1]);
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
     expect(getHumanAge(27, 27)).toEqual([2, 2]);
+    expect(getHumanAge(28, 15)).toEqual([3, 1]);
     expect(getHumanAge(28, 28)).toEqual([3, 2]);
     expect(getHumanAge(100, 100)).toEqual([21, 17]);
     expect(getHumanAge(500, 500)).toEqual([121, 97]);
   });
-});
 
+  test('should return corect human age for cats and dogs when a negative number', () => {
+    expect(getHumanAge(-1, -1)).toEqual([0, 0]);
+  });
+});
