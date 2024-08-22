7409285f888a57c584419ccb67e3aaf19dca37f5
Eduard Herasym <edkido29@gmail.com>
2024-08-22 19:23:59 +0300

    type&lengthValidation

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -4,8 +4,21 @@ describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
   test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+    expect(getHumanAge(5, 5)).toBeInstanceOf(Array);
+  });
+
+  test('should return array with correct length', () => {
+    expect(getHumanAge(5, 5)).toHaveLength(2);
+  });
+
+  test('should return 0 for ages less than 15 for cats and dogs', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test('should return numbers', () => {
+    const result = getHumanAge(14, 14);
+
+    expect(typeof result[0]).toBe('number');
   });
 
   test('should return 0 for ages less than 15 for cats and dogs', () => {
