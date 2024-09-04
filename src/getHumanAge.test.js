966496aaaf24c8d687d161e8c81687a798f72dcc
nevermore <gritsanowo@gmail.com>
2024-09-04 16:48:26 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -11,7 +11,7 @@ describe("getHumanAge", () => {
     expect(getHumanAge(14, 0)).toEqual([0, 0]);
   });
 
-  test("should be [1, 1] for age  15", () => {
+  test("should be [1, 1] for age 15", () => {
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
   });
 
@@ -19,20 +19,32 @@ describe("getHumanAge", () => {
     expect(getHumanAge(16, 23)).toEqual([1, 1]);
   });
 
+  test("should be [1, 1] for age 23", () => {
+    expect(getHumanAge(16, 23)).toEqual([1, 1]);
+  });
+
   test("should be [2, 2] for age 24", () => {
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
 
-  test("should be [1, 1] for age more than 24", () => {
+  test("should be [2, 2] for age more than 24", () => {
     expect(getHumanAge(26, 27)).toEqual([2, 2]);
   });
 
-  test("should be 3 if cat age 28", () => {
-    expect(getHumanAge(28, 23)).toEqual([3, 1]);
+  test("should be [2, 2] for age 27", () => {
+    expect(getHumanAge(26, 27)).toEqual([2, 2]);
   });
 
-  test("should be 3 if dog age 29", () => {
-    expect(getHumanAge(18, 29)).toEqual([1, 3]);
+  test("should be [3, 2] if cat age 28 and dog age 28", () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+  });
+
+  test("should be [3, 3] if cat age 29 and dog age 29", () => {
+    expect(getHumanAge(29, 29)).toEqual([3, 3]);
+  });
+
+  test("should be [3, 3] for age more than 29", () => {
+    expect(getHumanAge(30, 31)).toEqual([3, 3]);
   });
 
   test("should be [21, 17] if cat age is 100 and dog age is 100", () => {
