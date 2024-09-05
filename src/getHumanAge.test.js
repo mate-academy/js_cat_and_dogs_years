f731b477cd4a80c95feff4f6efffc10447055c55
Vita <st.vitalina666@gmail.com>
2024-09-05 16:00:13 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -9,47 +9,47 @@ describe('getHumanAge', () => {
   });
 
   test(`should return 0 human year
+    when cat or dog age is 0`, () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test(`should return 0 human year
     when cat or dog age less than 15`, () => {
-    expect(getHumanAge(5, 8)).toEqual([0, 0]);
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
   test(`should return 1 human year
-    when cat or dog age is 15(boundary value)`, () => {
+    when cat or dog age is 15`, () => {
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
   });
 
   test(`should return 1 human year
-    when cat and dog age is 21`, () => {
-    expect(getHumanAge(21, 21)).toEqual([1, 1]);
+    when cat or dog age is 23`, () => {
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
   });
 
   test(`should return 1 more human year for
-    the next 9 dog or cat years(boundary value)`, () => {
+    the next 9 dog years`, () => {
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
 
   test(`should return 1 more human year for
-    the next 9 dog or cat years`, () => {
-    expect(getHumanAge(26, 26)).toEqual([2, 2]);
-  });
-
-  test(`should return 1 extra human year for
-    every 4 next cat years(boundary value)`, () => {
-    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+    the next 9 cat years`, () => {
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
   });
 
   test(`should return 1 extra human year for
     every 4 next cat years`, () => {
-    expect(getHumanAge(45, 28)).toEqual([7, 2]);
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
   });
 
   test(`should return 1 extra human year for
-    every 5 next dog years(boundary value)`, () => {
-    expect(getHumanAge(28, 29)).toEqual([3, 3]);
+    every 4 next dog years`, () => {
+    expect(getHumanAge(27, 29)).toEqual([2, 3]);
   });
 
   test(`should return 1 extra human year for
-    every 5 next dog years`, () => {
-    expect(getHumanAge(47, 100)).toEqual([7, 17]);
+    every 4 next cat or dog years`, () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
