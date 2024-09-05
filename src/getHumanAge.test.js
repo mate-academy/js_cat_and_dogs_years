071c4f9bae6f3af60f32af89c4ca9551cfd7dd3b
lemeshev <mate.lemeshev@gmail.com>
2024-09-06 00:33:09 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -4,12 +4,12 @@ describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
   test('should be declared', () => {
-    expect(getHumanAge()).toBe()
+    expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
 
-  it('should return [1, 1] if cat and dog ages are less than 15', () => {
-    expect(getHumanAge(14, 14)).toEqual([1, 1]);
+  it('should return [0, 0] if cat and dog ages are less than 15', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
   it('should return [1, 1] for 15 years of both cat and dog', () => {
@@ -25,7 +25,7 @@ describe('getHumanAge', () => {
   });
 
   it('should return [2, 2] for 27 years of both cat and dog', () => {
-    expect(getHumanAge(27, 27)).toEqual([3, 2]);
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
   });
 
   it('should return [3, 2] for 28 years of both cat and dog', () => {
@@ -36,8 +36,8 @@ describe('getHumanAge', () => {
     expect(getHumanAge(29, 29)).toEqual([3, 3]);
   });
 
-  it('should return [4, 4] for 33 years of both cat and dog', () => {
-    expect(getHumanAge(32, 32)).toEqual([4, 4]);
+  it('should return [4, 3] for 33 years of both cat and dog', () => {
+    expect(getHumanAge(32, 32)).toEqual([4, 3]);
   });
 
   it('should return [6, 6] for 40 years of both cat and dog', () => {
