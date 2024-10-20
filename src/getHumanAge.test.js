fa7da05dc746052ae5b0023798da8fe83bd4f995
Demian Zolotukhin <demianzolotukhin@gmail.com>
2024-10-20 11:56:41 +0300

    solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,21 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return [1, 1] if animalAge < first + second', () => {
+    expect(getHumanAge(20, 20)).toEqual([1, 1]);
+  });
+
+  test('should return [0, 0] if animalAge === 0', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('should return [2, 2] if animalAge > first + second', () => {
+    expect(getHumanAge(26, 26)).toEqual([2, 2]);
+  });
+
+  test('should return [3, 3] if more than'
+  + 'first and second and after divide by other', () => {
+    expect(getHumanAge(29, 29)).toEqual([3, 3]);
+  });
 });
