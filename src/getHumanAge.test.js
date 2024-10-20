18a0114884c6dd0e7d23097268729e85852d8554
Demian Zolotukhin <demianzolotukhin@gmail.com>
2024-10-20 12:00:36 +0300

    solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -12,16 +12,18 @@ describe('getHumanAge', () => {
     expect(getHumanAge(20, 20)).toEqual([1, 1]);
   });
 
-  test('should return [0, 0] if animalAge === 0', () => {
+  test('should return [0, 0] if both animal ages are zero', () => {
     expect(getHumanAge(0, 0)).toEqual([0, 0]);
   });
 
-  test('should return [2, 2] if animalAge > first + second', () => {
+  test('should return [2, 2] if both animals are aged greater'
+  + 'than the first and second age brackets combined', () => {
     expect(getHumanAge(26, 26)).toEqual([2, 2]);
   });
 
-  test('should return [3, 3] if more than'
-  + 'first and second and after divide by other', () => {
+  test('should return [3, 3] if both animals have ages greater than'
+  + 'the combined first and second age'+
+  'brackets and include additional years', () => {
     expect(getHumanAge(29, 29)).toEqual([3, 3]);
   });
 });
