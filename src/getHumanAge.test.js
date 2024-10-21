cebec09ae4b3f403907822cffeda656d1a5f3d4d
Vlad Kach <kachevl@gmail.com>
2024-10-21 15:31:52 +0300

    solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,28 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should work with zeros', () => {
+    const result = getHumanAge(0, 0);
+
+    expect(result).toEqual([0, 0]);
+  });
+
+  test('should work correct with young animals', () => {
+    const result = getHumanAge(10, 10);
+
+    expect(result).toEqual([0, 0]);
+  });
+
+  test('should work correct with older animals', () => {
+    const result = getHumanAge(15, 15);
+
+    expect(result).toEqual([1, 1]);
+  });
+
+  test('should work correct with old animals', () => {
+    const result = getHumanAge(64, 74);
+
+    expect(result).toEqual([12, 12]);
+  });
 });
