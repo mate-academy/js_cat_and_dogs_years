4f272f327315acf5441e4cb5e0e9e7882a1621d8
Vlad Kach <kachevl@gmail.com>
2024-10-21 15:45:26 +0300

    solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 'use strict';
 
 describe('getHumanAge', () => {
@@ -8,27 +9,27 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test('should work with zeros', () => {
-    const result = getHumanAge(0, 0);
+  test(
+    'should return array with zero values if cat and dog years are less then 15',
+    () => {
+      const result = getHumanAge(10, 10);
 
-    expect(result).toEqual([0, 0]);
-  });
+      expect(result).toEqual([0, 0]);
+    });
 
-  test('should work correct with young animals', () => {
-    const result = getHumanAge(10, 10);
+  test(
+    'should return array with 1 values if cat and dog years are less then 24',
+    () => {
+      const result = getHumanAge(15, 15);
 
-    expect(result).toEqual([0, 0]);
-  });
+      expect(result).toEqual([1, 1]);
+    });
 
-  test('should work correct with older animals', () => {
-    const result = getHumanAge(15, 15);
+  test(
+    'should return array with values [12, 12] if arguments are equal 64 and 74',
+    () => {
+      const result = getHumanAge(64, 74);
 
-    expect(result).toEqual([1, 1]);
-  });
-
-  test('should work correct with old animals', () => {
-    const result = getHumanAge(64, 74);
-
-    expect(result).toEqual([12, 12]);
-  });
+      expect(result).toEqual([12, 12]);
+    });
 });
