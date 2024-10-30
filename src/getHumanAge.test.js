75bb06a0673c8df4e67afead4253b824158d5c1d
Roma <roma150269@gmail.com>
2024-10-30 09:36:14 +0200

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -14,4 +14,18 @@ describe('getHumanAge', () => {
     expect(result)
       .toEqual([0, 0]);
   });
+
+  test('should be have expected values', () => {
+    const result = getHumanAge(100, 100);
+
+    expect(result)
+      .toEqual([21, 17]);
+  });
+
+  test('should be return [2, 2] with such arguments (27, 27))', () => {
+    const result = getHumanAge(27, 27);
+
+    expect(result)
+      .toEqual([2, 2]);
+  });
 });
