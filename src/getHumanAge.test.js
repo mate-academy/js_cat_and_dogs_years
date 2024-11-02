83f2ddfe6089ab73226e8c6b88b8d8d287f20963
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2024-11-02 10:57:00 +0200

    description for tests were changed

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,16 +8,18 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test('animals` human age should be 0 if their own age < 15', () => {
+  test(`Should return 0 for animals' human age,
+    if animals' age less than 15`, () => {
     expect(getHumanAge(0, 14)).toEqual([0, 0]);
   });
 
-  test('animals` human age should be 1 if their own age > 14 and < 24', () => {
+  test(`Should return 1 for animals' human age,
+    if animals' age greater than 14 and less than 24`, () => {
     expect(getHumanAge(15, 23)).toEqual([1, 1]);
   });
 
-  test(`cat's age should add 1 every 4 years after 23 years old
-    dog's age should add 1 every 5 years after 23 years old`, () => {
+  test(`Should add 1 cat's human year for every 4 cat's years and
+    Should add 1 dog's human year for every 5 dog's years`, () => {
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
     expect(getHumanAge(27, 28)).toEqual([2, 2]);
     expect(getHumanAge(31, 33)).toEqual([3, 3]);
