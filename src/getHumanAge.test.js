48355b38beb945a5a09ba2ad1e7058b5c061677c
Maxymiliantys <barshenikovmax2003@gmail.com>
2024-11-15 16:53:21 +0200

    s

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -14,36 +14,36 @@ describe('getHumanAge', () => {
 
   test('should return [0, 0] when both values are than 14', () => {
     expect(getHumanAge(14, 14))
-    .toBeInstanceOf([0, 0]);
+      .toBeInstanceOf([0, 0]);
   });
 
   test('should return [1, 1] when both values are 15', () => {
     expect(getHumanAge(15, 15))
-    .toBeInstanceOf([1, 1]);
+      .toBeInstanceOf([1, 1]);
   });
 
   test('should return [1, 1] when both values are less 2', () => {
     expect(getHumanAge(23, 23))
-    .toBeInstanceOf([1, 1]);
+      .toBeInstanceOf([1, 1]);
   });
 
   test('should return [2, 2]] when both values are 24', () => {
     expect(getHumanAge(24, 24))
-    .toBeInstanceOf([2, 2]);
+      .toBeInstanceOf([2, 2]);
   });
 
   test('should return [2, 2] when both values are less 28', () => {
     expect(getHumanAge(27, 27))
-    .toBeInstanceOf([2, 2]);
+      .toBeInstanceOf([2, 2]);
   });
 
   test('should return [3, 2] when both values are 28', () => {
     expect(getHumanAge(28, 28))
-    .toBeInstanceOf([3, 2]);
+      .toBeInstanceOf([3, 2]);
   });
 
   test('should return [21, 17] when both values are 100', () => {
     expect(getHumanAge(100, 100))
-    .toBeInstanceOf([21, 17]);
+      .toBeInstanceOf([21, 17]);
   });
 });
