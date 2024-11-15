326726ae9b4a77a9dad05cb99c6a0f688c76491e
ArtemYukhymenko <winner.wcar@gmail.com>
2024-11-15 14:19:19 +0200

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -4,7 +4,33 @@ describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
   test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+    expect(getHumanAge).toBeInstanceOf(Function);
+  });
+
+  test('should return [0, 0] for ages less than 15', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test('should return [1, 1] for ages between 15 and 24', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
+  test(`should return [2, 2] for ages between 24 and the
+      next thresholds`, () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
+
+  test(`should correctly calculate when crossing the threshold
+      for extra years`, () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
+
+  test('should handle cases where catAge and dogAge are different', () => {
+    expect(getHumanAge(20, 28)).toEqual([1, 3]);
+    expect(getHumanAge(100, 50)).toEqual([21, 7]);
   });
 });
