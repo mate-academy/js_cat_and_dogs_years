ccb31f6a21ec8ee08dd9f9ba567ef1ef0598ce35
Maxymiliantys <barshenikovmax2003@gmail.com>
2024-11-15 17:21:41 +0200

    solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -9,34 +9,42 @@ describe('getHumanAge', () => {
   });
 
   test('should return [0, 0] when both values are 0', () => {
-    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(0, 0))
+     .toEqual([0, 0]);
   });
 
   test('should return [0, 0] when both values are greater than 14', () => {
-    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+    expect(getHumanAge(14, 14))
+     .toEqual([0, 0]);
   });
 
   test('should return [1, 1] when both values are 15', () => {
-    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+    expect(getHumanAge(15, 15))
+     .toEqual([1, 1]);
   });
 
   test('should return [1, 1] when both values are less than 2', () => {
-    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+    expect(getHumanAge(23, 23))
+     .toEqual([1, 1]);
   });
 
   test('should return [2, 2] when both values are 24', () => {
-    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+    expect(getHumanAge(24, 24))
+     .toEqual([2, 2]);
   });
 
   test('should return [2, 2] when both values are less than 28', () => {
-    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+    expect(getHumanAge(27, 27))
+     .toEqual([2, 2]);
   });
 
   test('should return [3, 2] when both values are 28', () => {
-    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+    expect(getHumanAge(28, 28))
+     .toEqual([3, 2]);
   });
 
   test('should return [21, 17] when both values are 100', () => {
-    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+    expect(getHumanAge(100, 100))
+     .toEqual([21, 17]);
   });
 });
