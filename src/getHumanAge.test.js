6115a306ae484a9cfb02bbaa9ba5d5e80561c43b
Bodya Kutsyk <mkkbodya@gmail.com>
2025-01-06 22:35:00 +0200

    solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,10 +1,42 @@
-'use strict';
+"use strict";
 
-describe('getHumanAge', () => {
-  const { getHumanAge } = require('./getHumanAge');
+describe("getHumanAge", () => {
+  const { getHumanAge } = require("./getHumanAge");
 
-  test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+  test("should be declared", () => {
+    expect(getHumanAge).toBeInstanceOf(Function);
   });
+
+  test("should be an array", () => {
+    const result = getHumanAge(0, 0);
+    expect(result).toBeInstanceOf(Array);
+  });
+
+  test("should return zero before 15", () => {
+    const result = getHumanAge(13, 1);
+    const expectedResult = [0, 0];
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  test("should return zero for 0 years", () => {
+    const result = getHumanAge(0, 0);
+    const expectedResult = [0, 0];
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  test("should return valid for 28 human years", () => {
+    const result = getHumanAge(28, 28);
+    const expectedResult = [3, 2];
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  test("should return valid for 100 human years", () => {
+    const result = getHumanAge(100, 100);
+    const expectedResult = [21, 17];
+
+    expect(result).toEqual(expectedResult);
+  })
 });
