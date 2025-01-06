95b3657bcbc710b63577d0557a6461b2b504c665
Bodya Kutsyk <mkkbodya@gmail.com>
2025-01-06 22:39:45 +0200

    changed test description

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -9,6 +9,7 @@ describe("getHumanAge", () => {
 
   test("should be an array", () => {
     const result = getHumanAge(0, 0);
+
     expect(result).toBeInstanceOf(Array);
   });
 
@@ -19,24 +20,24 @@ describe("getHumanAge", () => {
     expect(result).toEqual(expectedResult);
   });
 
-  test("should return zero for 0 years", () => {
+  test("should return zero human years for 0 years", () => {
     const result = getHumanAge(0, 0);
     const expectedResult = [0, 0];
 
     expect(result).toEqual(expectedResult);
   });
 
-  test("should return valid for 28 human years", () => {
+  test("should return valid human yearsfor 28 cat/dog years", () => {
     const result = getHumanAge(28, 28);
     const expectedResult = [3, 2];
 
     expect(result).toEqual(expectedResult);
   });
 
-  test("should return valid for 100 human years", () => {
+  test("should return valid human years for 100 cat/dog years", () => {
     const result = getHumanAge(100, 100);
     const expectedResult = [21, 17];
 
     expect(result).toEqual(expectedResult);
-  })
+  });
 });
