3b33e3acea0a018c4e77ca5d72cb6adb0ed5aed8
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2025-02-03 14:48:32 +0200

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -4,7 +4,30 @@ describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
   test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+    expect(getHumanAge).toBeInstanceOf(Function);
+  });
+
+  test('should return an array', () => {
+    const result = getHumanAge(15, 28);
+
+    expect(result).toBeInstanceOf(Array);
+  });
+
+  test('should return 0 human years if number of animal years is 0', () => {
+    const result = getHumanAge(0, 0);
+
+    expect(result).toEqual([0, 0]);
+  });
+
+  test('should return 1 human years if number of animal years < 24', () => {
+    const result = getHumanAge(23, 23);
+
+    expect(result).toEqual([1, 1]);
+  });
+
+  test('should have expected values', () => {
+    const result = getHumanAge(100, 100);
+
+    expect(result).toEqual([21, 17]);
   });
 });
