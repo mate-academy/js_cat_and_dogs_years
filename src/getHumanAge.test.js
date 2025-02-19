59e57f1d1d407b3aeaee1afe1b6155b6696ac962
Vlad Zahorovskii <zagorovskii12@gmail.com>
2025-02-19 21:28:25 +0200

    Done

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,23 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return same results', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
+
+  test('should return first bigger than second', () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
+
+  test('should return string error', () => {
+    expect(typeof getHumanAge(100, -1)[1]).toBe('string');
+    expect(getHumanAge(-1, 28)[0]).toBe('Please enter number bigger than 0');
+  });
 });
