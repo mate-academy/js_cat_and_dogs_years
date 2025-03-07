3bc7b79f5cc6a1b689bdeae32286fd38f9c2e6c4
RomanchukOleksandr <oleksandr.romanchukk@gmail.com>
2025-03-07 17:46:25 +0200

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -4,7 +4,42 @@ describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
   test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+    expect(getHumanAge).toEqual(Function);
+  });
+
+  test('should be declared', () => {
+    expect(getHumanAge).toEqual(Function);
+  });
+
+  test('should be 0 if entered 0', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('1 equivalence class: age less than 15', () => {
+    expect(getHumanAge(6, 6)).toEqual([0, 0]);
+  });
+
+  test('2 equivalence class: age between 15 and 23', () => {
+    expect(getHumanAge(18, 18)).toEqual([1, 1]);
+  });
+
+  test('3 equivalence class: 24 years and older', () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
+
+  test('boundary value: 15 years', () => {
+    expect(getHumanAge(15, 15)).toEqual([0, 0]);
+  });
+
+  test('boundary value: 16 years', () => {
+    expect(getHumanAge(16, 16)).toEqual([1, 1]);
+  });
+
+  test('boundary value: 23 years', () => {
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
+  test('boundary value: 24 years', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
 });
