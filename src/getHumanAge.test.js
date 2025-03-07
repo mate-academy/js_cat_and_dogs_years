6f5148601f32f2ed8cf601704cee57f1d3903240
lara1029 <lara.ponizovnaya@gmail.com>
2025-03-07 18:49:32 +0200

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,36 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  it('should return [0, 0] when both ages are 0', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('should return [0, 0] when both ages are less than 15', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test('should return [1, 1] when ages are 15', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  test('should return [1, 1] when cat and dog ages are 23', () => {
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
+  test('should return [2, 2] when cat and dog ages are 24', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+
+  test('should return [2, 2] when cat and dog ages are 27', () => {
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
+
+  test('should return [3, 2] when cat age is 28 and dog age is 28', () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+  });
+
+  test('should return [21, 17] for very old ages', () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
 });
