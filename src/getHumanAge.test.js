b8b87d706e468526ef1e84898beffd6fe5c5f654
Karyna <krntsmbl@gmail.com>
2025-03-07 14:44:15 +0200

    Add tests

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,11 +8,11 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  it('shoul return [0, 0] for (0, 0)', () => {
-    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  it('should return [0, 0] for (0, 0)', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
   });
 
-  it('shoul return [1, 1] for (15, 15)', () => {
+  it('should return [1, 1] for (15, 15)', () => {
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
   });
 
@@ -20,7 +20,7 @@ describe('getHumanAge', () => {
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
 
-  it('should return [2, 2] for (23, 23)', () => {
+  it('should return [1, 1] for (23, 23)', () => {
     expect(getHumanAge(23, 23)).toEqual([1, 1]);
   });
 
@@ -28,11 +28,11 @@ describe('getHumanAge', () => {
     expect(getHumanAge(27, 27)).toEqual([2, 2]);
   });
 
-  it('should return [2, 2] for (28, 28)', () => {
+  it('should return [3, 2] for (28, 28)', () => {
     expect(getHumanAge(28, 28)).toEqual([3, 2]);
   });
 
-  it('should return [2, 2] for (100, 100)', () => {
+  it('should return [21, 17] for (100, 100)', () => {
     expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
