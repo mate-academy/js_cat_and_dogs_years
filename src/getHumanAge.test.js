27a49d4ad8ae70c4712d685be5049d7d58fe9f34
CDDVDRAM <gidrobear@gmail.com>
2025-03-09 16:48:12 +0200

    New

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,16 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('HumanAge for cat', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('HumanAge for cat', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test('HumanAge for cat', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
 });
