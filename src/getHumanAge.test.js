8ea22bfeef536f0a0cd191e7dfe8b10388b89812
ShramkoOleksandr <alexshramko243@gmail.com>
2025-03-11 20:58:19 +0200

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,10 +1,21 @@
 'use strict';
 
 describe('getHumanAge', () => {
-  const { getHumanAge } = require('./getHumanAge');
+  const { getHumanAge } = require('./getHumanAge'); // імпортуємо
 
   test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+    expect(getHumanAge).toBeInstanceOf(Function);
+  });
+
+  test('should return [0, 0] for (0, 0)', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('should return [1, 1] for (15, 15)', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  test('should return [21, 17] for (100, 100)', () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
