bcac5ba46487d8efae3f2690cbd4caf7041f63f9
Valeriia Kozmenko <leraz9953@gmail.com>
2025-03-24 14:58:15 +0200

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,10 +1,48 @@
 'use strict';
 
-describe('getHumanAge', () => {
-  const { getHumanAge } = require('./getHumanAge');
+const { getHumanAge } = require('./getHumanAge');
 
+describe('getHumanAge', () => {
   test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+    expect(getHumanAge).toBeInstanceOf(Function);
+  });
+
+  test('should return [0, 0] for (0, 0)', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('should return [0, 0] for (14, 14)', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test('should return [1, 1] for (15, 15)', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  test('should return [1, 1] for (23, 23)', () => {
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
+  test('should return [2, 2] for (24, 24)', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+
+  test('should return [2, 2] for (27, 27)', () => {
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
+
+  test('should return [3, 2] for (28, 28)', () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+  });
+
+  test('should return [21, 17] for (100, 100)', () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
+
+  test('should return correct human age for edge cases', () => {
+    expect(getHumanAge(16, 16)).toEqual([1, 1]);
+    expect(getHumanAge(25, 25)).toEqual([2, 2]);
+    expect(getHumanAge(29, 29)).toEqual([3, 3]);
+    expect(getHumanAge(50, 50)).toEqual([8, 7]);
   });
 });
