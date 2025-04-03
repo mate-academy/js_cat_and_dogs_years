48cf1e326893a043227765a7f4f65421e977ba95
Max_Hrebinets <brainisbro@gmail.com>
2025-04-03 13:22:20 +0300

    done

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,7 +8,7 @@ describe('getHumanAge', () => {
       .toBeInstanceOf(Function);
   });
 
-  test('shoud return correct values', () => {
+  test('should return correct values', () => {
     expect(getHumanAge(9, 16)).toEqual([0, 1]);
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
     expect(getHumanAge(24, 20)).toEqual([2, 1]);
