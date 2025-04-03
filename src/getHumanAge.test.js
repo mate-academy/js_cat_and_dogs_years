d18250fc63866face3157b5beb402c185fa1e05b
Max_Hrebinets <brainisbro@gmail.com>
2025-04-03 13:20:06 +0300

    done

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,12 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('shoud return correct values', () => {
+    expect(getHumanAge(9, 16)).toEqual([0, 1]);
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+    expect(getHumanAge(24, 20)).toEqual([2, 1]);
+    expect(getHumanAge(25, 30)).toEqual([2, 3]);
+    expect(getHumanAge(30, 40)).toEqual([3, 5]);
+  });
 });
