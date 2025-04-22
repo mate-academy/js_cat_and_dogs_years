d914865377cf4a30e532dfe075706ba9b151f28b
Janani <jananiraja1405@gmail.com>
2025-04-23 11:56:28 +1200

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,24 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('Cat and Dog years are zero', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('Cat and Dog years are less than 15', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test('Cat and Dog years for human age 1', () => {
+    expect(getHumanAge(16, 16)).toEqual([1, 1]);
+  });
+
+  test('Cat and Dog years to cover age 2', () => {
+    expect(getHumanAge(25, 25)).toEqual([2, 2]);
+  });
+
+  test('Cat and Dogs with different years', () => {
+    expect(getHumanAge(50, 100)).toEqual([8, 17]);
+  });
 });
