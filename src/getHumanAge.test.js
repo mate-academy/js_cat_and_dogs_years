c9d1c86a07755435e9ff1559d21827d5d7920612
Csillag61 <csillagok@gmail.com>
2025-05-25 20:22:20 -0400

    humanAge_test

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,23 +1,23 @@
-"use strict";
+'use strict';
 
-const { getHumanAge } = require("./getHumanAge");
+const { getHumanAge } = require('./getHumanAge');
 
-describe("getHumanAge", () => {
-  test("should be declared", () => {
+describe('getHumanAge', () => {
+  test('should be declared', () => {
     expect(getHumanAge).toBeInstanceOf(Function);
   });
 
-  test("should return [0, 0] for ages below first threshold", () => {
+  test('should return [0, 0] for ages below first threshold', () => {
     expect(getHumanAge(0, 0)).toEqual([0, 0]);
     expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
-  test("should return [1, 1] for ages within the first threshold", () => {
+  test('should return [1, 1] for ages within the first threshold', () => {
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
     expect(getHumanAge(23, 23)).toEqual([1, 1]);
   });
 
-  test("should return [2, 2] for ages within the second threshold", () => {
+  test('should return [2, 2] for ages within the second threshold', () => {
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
     expect(getHumanAge(27, 27)).toEqual([2, 2]);
   });
