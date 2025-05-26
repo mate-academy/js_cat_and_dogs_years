0330f0515773e5037877a21f672b068238458a9e
Csillag61 <csillagok@gmail.com>
2025-05-25 21:06:29 -0400

    fixed bugs

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,24 +7,31 @@ describe('getHumanAge', () => {
     expect(getHumanAge).toBeInstanceOf(Function);
   });
 
-  test('should return [0, 0] for ages below first threshold', () => {
+  test('should return [0, 0] for ages below the first threshold', () => {
     expect(getHumanAge(0, 0)).toEqual([0, 0]);
     expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
-  test('should return [1, 1] for ages within the first threshold', () => {
+  test('should return [1, 1] for ages exactly at the first threshold', () => {
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
-    expect(getHumanAge(23, 23)).toEqual([1, 1]);
-  });
-
-  test('should return [2, 2] for ages within the second threshold', () => {
-    expect(getHumanAge(24, 24)).toEqual([2, 2]);
-    expect(getHumanAge(27, 27)).toEqual([2, 2]);
   });
 
   // eslint-disable-next-line max-len
-  test('should correctly calculate human age beyond the second threshold', () => {
+  test('should return correct values for ages just above the second threshold', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
     expect(getHumanAge(28, 28)).toEqual([3, 2]);
+  });
+
+  test('should correctly convert large animal ages', () => {
     expect(getHumanAge(100, 100)).toEqual([21, 17]);
+    expect(getHumanAge(1000, 1000)).toEqual([246, 197]);
+  });
+
+  // eslint-disable-next-line max-len
+  test('should return correct values when cat and dog ages differ significantly', () => {
+    expect(getHumanAge(60, 20)).toEqual([12, 1]);
+    expect(getHumanAge(35, 40)).toEqual([6, 5]);
+    expect(getHumanAge(50, 55)).toEqual([10, 8]);
   });
 });
