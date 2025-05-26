d7e1d4dad0f4e180579a79c51dbe20c50bd7ac8c
Csillag61 <csillagok@gmail.com>
2025-05-25 20:17:00 -0400

    humanAge_test

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,10 +1,30 @@
-'use strict';
+"use strict";
 
-describe('getHumanAge', () => {
-  const { getHumanAge } = require('./getHumanAge');
+const { getHumanAge } = require("./getHumanAge");
 
-  test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+describe("getHumanAge", () => {
+  test("should be declared", () => {
+    expect(getHumanAge).toBeInstanceOf(Function);
+  });
+
+  test("should return [0, 0] for ages below first threshold", () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test("should return [1, 1] for ages within the first threshold", () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
+  test("should return [2, 2] for ages within the second threshold", () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
+
+  // eslint-disable-next-line max-len
+  test('should correctly calculate human age beyond the second threshold', () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
