1b92860b513a5f587094dfcfa45cc1555a0c4435
anIRI26 <shtefanesa.iri@gmail.com>
2025-05-27 18:25:13 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,54 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return [0, 0] for (0, 0)', () => {
+    expect(getHumanAge(0, 0))
+      .toEqual([0, 0]);
+  });
+
+  test('should return [1, 1] for (15, 15)', () => {
+    expect(getHumanAge(15, 15))
+      .toEqual([1, 1]);
+  });
+
+  test('should return [1, 1] for (16, 16)', () => {
+    expect(getHumanAge(16, 16))
+      .toEqual([1, 1]);
+  });
+
+  test('should return [1, 1] for (17, 17)', () => {
+    expect(getHumanAge(17, 17))
+      .toEqual([1, 1]);
+  });
+
+  test('should return [2, 2] for (24, 24)', () => {
+    expect(getHumanAge(24, 24))
+      .toEqual([2, 2]);
+  });
+
+  test('should return [2, 2] for (25, 25)', () => {
+    expect(getHumanAge(25, 25))
+      .toEqual([2, 2]);
+  });
+
+  test('should return [2, 2] for (27, 27)', () => {
+    expect(getHumanAge(27, 27))
+      .toEqual([2, 2]);
+  });
+
+  test('should return [3, 2] for (28, 27)', () => {
+    expect(getHumanAge(28, 27))
+      .toEqual([3, 2]);
+  });
+
+  test('should return [3, 3] for (28, 29)', () => {
+    expect(getHumanAge(28, 29))
+      .toEqual([3, 3]);
+  });
+
+  test('should return [2, 3] for (27, 29)', () => {
+    expect(getHumanAge(27, 29))
+      .toEqual([2, 3]);
+  });
 });
