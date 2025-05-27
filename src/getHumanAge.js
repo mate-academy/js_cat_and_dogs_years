e69a5c76a2331fcfe6b561bd1bb4e9ff527d6d12
Nata <a.nata.work@gmail.com>
2025-05-27 17:29:47 +0300

    Solution

--- src/getHumanAge.js
+++ src/getHumanAge.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/**
+/*
  * @param {number} catAge
  * @param {number} dogAge
  *
--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -3,8 +3,43 @@
 describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
-  test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+  test('should return 0 human year for 0 animal year', () => {
+    expect(getHumanAge(0, 0))
+      .toEqual([0, 0]);
+  });
+
+  test('should return 0 human year for 14 animal year', () => {
+    expect(getHumanAge(14, 14))
+      .toEqual([0, 0]);
+  });
+
+  test('should return 1 human year for 15 animal year', () => {
+    expect(getHumanAge(15, 15))
+      .toEqual([1, 1]);
+  });
+
+  test('should return 1 human year for 23 animal year', () => {
+    expect(getHumanAge(23, 23))
+      .toEqual([1, 1]);
+  });
+
+  test('should return 2 human year for 24 animal year', () => {
+    expect(getHumanAge(24, 24))
+      .toEqual([2, 2]);
+  });
+
+  test('should return 2 human year for 27 animal year', () => {
+    expect(getHumanAge(15, 15))
+      .toEqual([1, 1]);
+  });
+
+  test('should return 3 and 2 human year for 28 animal year', () => {
+    expect(getHumanAge(28, 28))
+      .toEqual([3, 2]);
+  });
+
+  test('should return diferent human age values for 100 animal year', () => {
+    expect(getHumanAge(100, 100))
+      .toEqual([21, 17]);
   });
 });
