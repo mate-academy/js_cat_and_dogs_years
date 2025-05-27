a0eec5ef53ec50160e2fbb476870f2085b2672dc
Alyona Stolyarenko <estolarenko5@gmail/com>
2025-05-27 17:11:18 +0300

    Add and test getHumanAge function

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,10 +1,35 @@
 'use strict';
 
-describe('getHumanAge', () => {
-  const { getHumanAge } = require('./getHumanAge');
+const { getHumanAge } = require('./getHumanAge');
 
-  test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
-  });
+test('getHumanAge(0, 0) === [0, 0]', () => {
+  expect(getHumanAge(0, 0)).toStrictEqual([0, 0]);
+});
+
+test('getHumanAge(14, 14) === [0, 0]', () => {
+  expect(getHumanAge(14, 14)).toStrictEqual([0, 0]);
+});
+
+test('getHumanAge(15, 15) === [1, 1]', () => {
+  expect(getHumanAge(15, 15)).toStrictEqual([1, 1]);
+});
+
+test('getHumanAge(23, 23) === [1, 1]', () => {
+  expect(getHumanAge(23, 23)).toStrictEqual([1, 1]);
+});
+
+test('getHumanAge(24, 24) === [2, 2]', () => {
+  expect(getHumanAge(24, 24)).toStrictEqual([2, 2]);
+});
+
+test('getHumanAge(27, 27) === [2, 2]', () => {
+  expect(getHumanAge(27, 27)).toStrictEqual([2, 2]);
+});
+
+test('getHumanAge(28, 28) === [3, 2]', () => {
+  expect(getHumanAge(28, 28)).toStrictEqual([3, 2]);
+});
+
+test('getHumanAge(100, 100) === [21, 17]', () => {
+  expect(getHumanAge(100, 100)).toStrictEqual([21, 17]);
 });
