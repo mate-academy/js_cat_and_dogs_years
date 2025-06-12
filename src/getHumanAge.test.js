eb3a6fbf9c99e0b98937e642bb265312cc77b0cf
Vladymyrska Valentyna <vladymyrskayvalia@gmail.com>
2025-06-12 16:11:04 +0300

    Add unit tests for getHumanAge function

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -75,4 +75,33 @@ describe('getHumanAge', () => {
   test('should not works with negative numbers', () => {
     expect(getHumanAge(-1, -1)).toEqual([0, 0]);
   });
+
+  test('should return [0, 0] if arguments are not provided', () => {
+  expect(getHumanAge()).toEqual([0, 0]);
+});
+
+test('should return [0, 0] if arguments are null', () => {
+  expect(getHumanAge(null, null)).toEqual([0, 0]);
+});
+
+test('should return [0, 0] if arguments are strings', () => {
+  expect(getHumanAge('15', '15')).toEqual([0, 0]);
+});
+
+test('should return correct value when only catAge is passed', () => {
+  expect(getHumanAge(30)).toEqual([3, 0]); // catAge = 30, dogAge = undefined
+});
+
+test('should return correct value when only dogAge is passed (as second param)', () => {
+  expect(getHumanAge(undefined, 30)).toEqual([0, 3]); // undefined, dogAge = 30
+});
+
+test('should ignore decimal part and return correct values', () => {
+  expect(getHumanAge(28.9, 29.1)).toEqual([3, 3]); // округлення вниз
+});
+
+test('should handle extremely large numbers correctly', () => {
+  expect(getHumanAge(1000, 1000)).toEqual([245, 197]);
+});
+
 })
