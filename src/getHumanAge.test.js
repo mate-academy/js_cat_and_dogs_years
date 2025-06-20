5e94f9a75ddf46ed2c213896e0db9e276f825f9e
Maksym Korchynskyi <maksym.24.09.2003@gmail.com>
2025-06-20 17:10:45 +0300

    add solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -4,7 +4,18 @@ describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
   test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+    expect(getHumanAge).toBeInstanceOf(Function);
+  });
+
+  test('should return 1 year for first 15 years', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  test('should add 1 year for next 9 years', () => {
+    expect(getHumanAge(15 + 9, 15 + 9)).toEqual([2, 2]);
+  });
+
+  test('should add 1 year for each next 4/5 years', () => {
+    expect(getHumanAge(15 + 9 + 4, 15 + 9 + 5)).toEqual([3, 3]);
   });
 });
