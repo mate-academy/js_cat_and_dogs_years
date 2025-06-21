0c0113366ae8de55382af327bcf158a85b744196
Anton Kuchmasov <antek.ukraine@gmail.com>
2025-06-21 20:15:34 +0300

    added solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -4,7 +4,22 @@ describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
   test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+    expect(getHumanAge).toBeInstanceOf(Function);
+  });
+
+  test('should return 0 if incorrect data is given', () => {
+    expect(getHumanAge(-5, 0)).toEqual([0, 0]);
+  });
+
+  test('should return NaN if input !== numbers', () => {
+    expect(getHumanAge('a', 'b')).toEqual([NaN, NaN]);
+  }); ;
+
+  test('should return [5, 4] if human ages = 36', () => {
+    expect(getHumanAge(36, 36)).toEqual([5, 4]);
+  });
+
+  test('should return [21, 17] if human ages = 100', () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
