1aefb4db94c4184f2096381d7b7fa7b8b2feace4
Alex Pakhomov <pakhomov.alex.v@gmail.com>
2025-07-05 15:15:40 +0200

    add task solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,28 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return zero, if value is 0', () => {
+    const result = getHumanAge(0, 0);
+
+    expect(result).toEqual([0, 0]);
+  });
+
+  test('should return zero, if value is less than 15', () => {
+    const result = getHumanAge(14, 14);
+
+    expect(result).toEqual([0, 0]);
+  });
+
+  test('should return integer', () => {
+    const result = getHumanAge(21, 21);
+
+    expect(result).toEqual([1, 1]);
+  });
+
+  test('should calculate both ages correctly', () => {
+    const result = getHumanAge(28, 29);
+
+    expect(result).toEqual([3, 3]);
+  });
 });
