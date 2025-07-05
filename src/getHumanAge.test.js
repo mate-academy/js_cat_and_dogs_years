0c8870d4fd726ac364895d590b2e09913d8ce8c7
Max <max@Noutbuk-Apple.local>
2025-07-05 17:28:37 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,12 +1,13 @@
 'use strict';
+
 describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
+
   test('should be declared', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
 
- 
   test('should return 0 for cat and dog when they are 0 years old', () => {
     expect(getHumanAge(0, 0)).toEqual([0, 0]);
   });
@@ -15,7 +16,6 @@ describe('getHumanAge', () => {
     expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
-  
   test('should return 1 for cat and dog when they are 15 years old', () => {
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
   });
@@ -24,7 +24,6 @@ describe('getHumanAge', () => {
     expect(getHumanAge(23, 23)).toEqual([1, 1]);
   });
 
-  
   test('should return 2 for cat and dog when they are 24 years old', () => {
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
@@ -33,13 +32,11 @@ describe('getHumanAge', () => {
     expect(getHumanAge(27, 27)).toEqual([2, 2]);
   });
 
- 
   test('should return 3 for cat and 2 for dog when they are 28 years old', () => {
     expect(getHumanAge(28, 28)).toEqual([3, 2]);
   });
 
- 
   test('should return 21 for cat and 17 for dog when they are 100 years old', () => {
     expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
-  });
+});
