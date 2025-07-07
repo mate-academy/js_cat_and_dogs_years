ec83c30a0bfba75957e15bd38129850e4064751a
Bernavschi Natalia <natb@list.ru>
2025-07-07 21:57:48 +0300

    add task solution for js_cat_and_dogs_years

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -4,7 +4,44 @@ describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
   test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+    expect(getHumanAge).toBeInstanceOf(Function);
+  });
+
+  test('should return [0, 0] for (0, 0)', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('should return [0, 0] for (14, 14)', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test('should return [1, 1] for (15, 15)', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  test('should return [1, 1] for (23, 23)', () => {
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
+  test('should return [2, 2] for (24, 24)', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+
+  test('should return [2, 2] for (27, 27)', () => {
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
+
+  test('should return [3, 2] for (28, 28)', () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+  });
+
+  test('should return [21, 17] for (100, 100)', () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
+
+  test('should handle edge cases correctly', () => {
+    expect(getHumanAge(15 + 9 - 1, 15 + 9 - 1)).toEqual([1, 1]);
+    expect(getHumanAge(15 + 9, 15 + 9)).toEqual([2, 2]);
+    expect(getHumanAge(15 + 9 + 4, 15 + 9 + 5)).toEqual([3, 3]);
   });
 });
