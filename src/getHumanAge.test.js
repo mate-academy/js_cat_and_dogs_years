959f09260e9b16d2cf9f229a5086e13faac8cd81
mrsvolodya <volodyamurskyi@gmail.com>
2025-07-15 23:39:19 +0300

    resolve mistakes

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,41 +1,41 @@
-"use strict";
+'use strict';
 
-describe("getHumanAge", () => {
-  const { getHumanAge } = require("./getHumanAge");
+describe('getHumanAge', () => {
+  const { getHumanAge } = require('./getHumanAge');
 
-  test("should be declared", () => {
+  test('should be declared', () => {
     expect(getHumanAge).toBeInstanceOf(Function);
   });
 
-  test("should return [0, 0] when catAge and dogAge are [0, 0]", () => {
+  test('should return [0, 0] when catAge and dogAge are [0, 0]', () => {
     expect(getHumanAge(0, 0)).toEqual([0, 0]);
   });
 
-  test("should return [0, 0] when catAge and dogAge are [1, 1]", () => {
+  test('should return [0, 0] when catAge and dogAge are [14, 14]', () => {
     expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
-  test("should return [1, 1] when catAge and dogAge are [15, 15]", () => {
+  test('should return [1, 1] when catAge and dogAge are [15, 15]', () => {
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
   });
 
-  test("should return [1, 1] when catAge and dogAge are [23, 23]", () => {
+  test('should return [1, 1] when catAge and dogAge are [23, 23]', () => {
     expect(getHumanAge(23, 23)).toEqual([1, 1]);
   });
 
-  test("should return [2, 2] when catAge and dogAge are [24, 24]", () => {
+  test('should return [2, 2] when catAge and dogAge are [24, 24]', () => {
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
 
-  test("should return [2, 2] when catAge and dogAge are [27, 27]", () => {
+  test('should return [2, 2] when catAge and dogAge are [27, 27]', () => {
     expect(getHumanAge(27, 27)).toEqual([2, 2]);
   });
 
-  test("should return [3, 2] when catAge and dogAge are [28, 28]", () => {
+  test('should return [3, 2] when catAge and dogAge are [28, 28]', () => {
     expect(getHumanAge(28, 28)).toEqual([3, 2]);
   });
 
-  test("should return [21, 17] when catAge and dogAge are [100, 100]", () => {
+  test('should return [21, 17] when catAge and dogAge are [100, 100]', () => {
     expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
