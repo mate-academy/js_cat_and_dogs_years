29968c1fe4e842f93dbe12aba72060518e5e45b7
Pavlo Pukalo <misterkatlet@gmail.com>
2025-07-23 19:38:49 +0300

    solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,30 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return correct values', () => {
+    expect(getHumanAge(0, 0))
+      .toEqual([0, 0]);
+
+    expect(getHumanAge(14, 14))
+      .toEqual([0, 0]);
+
+    expect(getHumanAge(15, 15))
+      .toEqual([1, 1]);
+
+    expect(getHumanAge(23, 23))
+      .toEqual([1, 1]);
+
+    expect(getHumanAge(24, 24))
+      .toEqual([2, 2]);
+
+    expect(getHumanAge(27, 27))
+      .toEqual([2, 2]);
+
+    expect(getHumanAge(28, 28))
+      .toEqual([3, 2]);
+
+    expect(getHumanAge(100, 100))
+      .toEqual([21, 17]);
+  });
 });
