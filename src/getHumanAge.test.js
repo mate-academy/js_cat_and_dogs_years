2c57c5a8838b55fb4b86a9dd0947d298ddaf6b42
Igor-Yaremchuk <yomzfk@gmail.com>
2025-07-30 15:55:36 +0300

    sol

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -13,27 +13,27 @@ describe('getHumanAge', () => {
   });
 
   test('should return [0, 0] for [14, 14]', () => {
-    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
   test('should return [1, 1] for [15, 15]', () => {
-    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
   });
 
   test('should return [2, 2] for [24, 24]', () => {
-    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
 
   test('should return [2, 2] for [27, 27]', () => {
-    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
   });
 
   test('should return [3, 2] for [28, 28]', () => {
-    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
   });
 
-  test('should return [100, 100] for [21, 17]', () => {
-    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  test('should return [21, 17] for [100, 100]', () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 
   test('should handle edge case where only cat has age', () => {
