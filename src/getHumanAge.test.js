2523f8599e240b7486e16f01354f56f9e82248f8
Eater228 <543145yura@gmail.com>
2025-08-20 19:42:50 +0300

    add solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,24 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('Should return 0 for 0 cat and dog years', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('Should return 1 for 15 cat and dog years', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  test('Should return 2 for 24 cat and dog years', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+
+  test('Should return 3 for 28 years cat and 2 for 28 dog years', () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+  });
+
+  test('Should return 21 for 100 cat years and 17 for 100 dog years', () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
 });
