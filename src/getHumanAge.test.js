260565f2815e94dd774f00b9d086a680ab3e8763
Dmytro Koriahin <dkoryahin@gmail.com>
2025-08-20 17:16:44 +0300

    add task solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,12 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return [0, 0] when both cat and dog are younger than 15', () => {
+    expect(getHumanAge(12, 13)).toEqual([0, 0]);
+  });
+
+  test('should return [1, 1] when animal years are between 15 and 24', () => {
+    expect(getHumanAge(18, 23)).toEqual([1, 1]);
+  });
 });
