e4678229e7da105e6d8b9bf364bd5055e6c9f861
Oleksii-Ivan Stetsyk <farshaleks@gmail.com>
2025-09-29 18:22:19 +0300

    add tests for hetHumanAge

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -3,8 +3,51 @@
 describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
-  test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+  test('should return [0, 0] for input (0, 0)', () => {
+    const result = getHumanAge(0, 0);
+
+    expect(result).toEqual([0, 0]);
+  });
+
+  test('should return [0, 0] for input (14, 14)', () => {
+    const result = getHumanAge(14, 14);
+
+    expect(result).toEqual([0, 0]);
+  });
+
+  test('should return [1, 1] for input (15, 15)', () => {
+    const result = getHumanAge(15, 15);
+
+    expect(result).toEqual([1, 1]);
+  });
+
+  test('should return [1, 1] for input (23, 23)', () => {
+    const result = getHumanAge(23, 23);
+
+    expect(result).toEqual([1, 1]);
+  });
+
+  test('should return [2, 2] for input (24, 24)', () => {
+    const result = getHumanAge(24, 24);
+
+    expect(result).toEqual([2, 2]);
+  });
+
+  test('should return [2, 2] for input (27, 27)', () => {
+    const result = getHumanAge(27, 27);
+
+    expect(result).toEqual([2, 2]);
+  });
+
+  test('should return [3, 2] for input (28, 28)', () => {
+    const result = getHumanAge(28, 28);
+
+    expect(result).toEqual([3, 2]);
+  });
+
+  test('should return [21, 17] for input (100, 100)', () => {
+    const result = getHumanAge(100, 100);
+
+    expect(result).toEqual([21, 17]);
   });
 });
