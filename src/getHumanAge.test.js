dff0cfc41f4ba3cbfc4f030471184a1477f681c6
oleh <olehphw@gmail.com>
2025-10-05 13:44:07 +0200

    add tests

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,29 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should be an array of 2 items', () => {
+    expect(getHumanAge(20, 30)).toHaveLength(2);
+    expect(Array.isArray(getHumanAge(20, 30))).toBe(true);
+  });
+
+  test('should return correct result for 0, 0', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('should return correct result for 15, 15', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  test('should return correct result for 24, 24', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+
+  test('should return correct result for 28, 28', () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+  });
+
+  test('should return correct result for 100, 100', () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
 });
