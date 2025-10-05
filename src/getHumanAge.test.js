36472890ab7807caa75b30ce3c619b6c45b4067f
oleh <olehphw@gmail.com>
2025-10-05 13:56:23 +0200

    fixed tests 1

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -17,12 +17,24 @@ describe('getHumanAge', () => {
     expect(getHumanAge(0, 0)).toEqual([0, 0]);
   });
 
+  test('should return correct result for 14, 14', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
   test('should return correct result for 15, 15', () => {
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
   });
 
+  test('should return correct result for 23, 23 (just before 24)', () => {
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
   test('should return correct result for 24, 24', () => {
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+
+  test('should return correct result for 27, 27 (post-24 range)', () => {
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
   });
 
   test('should return correct result for 28, 28', () => {
@@ -31,5 +43,15 @@ describe('getHumanAge', () => {
 
   test('should return correct result for 100, 100', () => {
     expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
+
+  test('should handle different cat and dog ages independently', () => {
+    expect(getHumanAge(28, 27)).toEqual([3, 2]);
+    expect(getHumanAge(25, 29)).toEqual([3, 3]);
+  });
+
+  test('should discard fractional inputs', () => {
+    expect(getHumanAge(24.9, 24.9)).toEqual([2, 2]);
+    expect(getHumanAge(14.5, 15.9)).toEqual([0, 1]);
   });
 });
