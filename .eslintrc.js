200f202bf21fcdf3b4c827d9a6c0b44990a999c8
Andrii Fesych <andriyhimself@gmail.com>
2025-10-28 21:32:36 +0100

    removed prettier dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
+  extends: '@mate-academy/eslint-config',
   env: {
     jest: true
   },
   rules: {
-    'no-proto': 0,
-    'prettier/prettier': 'off'
+    'no-proto': 0
   },
   plugins: ['jest']
 };
