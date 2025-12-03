ccc485ba22725b79b6b52371a7bfd7ac71f8a275
Vasyl Zhyliakov <snak87@gmail.com>
2025-12-03 09:01:38 +0200

    solution v 1.2

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,27 +1,28 @@
-'use strict';
+"use strict";
 
-describe('getHumanAge', () => {
-  const { getHumanAge } = require('./getHumanAge');
+describe("getHumanAge", () => {
+  const { getHumanAge } = require("./getHumanAge");
 
-  test('should be declared', () => {
+  test("should be declared", () => {
     expect(getHumanAge).toBeInstanceOf(Function);
   });
 
-  test('should return 0, when age less than first', () => {
+  test("should return 0, when age less than first", () => {
     expect(getHumanAge(0, 0)).toEqual([0, 0]);
     expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
-  test('should return 1, when age less than first + second', () => {
+  test("should return 1, when age less than first + second", () => {
     expect(getHumanAge(15, 15)).toEqual([1, 1]);
     expect(getHumanAge(23, 23)).toEqual([1, 1]);
   });
 
-  test('should return 2, when age less than first + second + other', () => {
+  test("should return 2, when age less than first + second + other", () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
     expect(getHumanAge(27, 27)).toEqual([2, 2]);
   });
 
-  test('should return 2 and more, when age more than first + second', () => {
+  test("should return 2 and more, when age more than first + second", () => {
     expect(getHumanAge(28, 28)).toEqual([3, 2]);
     expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
