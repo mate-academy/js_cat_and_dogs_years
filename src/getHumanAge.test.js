22f3d93c0a0df13d743063e0c90a2d0f8ea254ef
Vasyl Zhyliakov <snak87@gmail.com>
2025-12-03 08:58:37 +0200

    solution v 1.1

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -8,18 +8,21 @@ describe('getHumanAge', () => {
   });
 
   test('should return 0, when age less than first', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
     expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
   test('should return 1, when age less than first + second', () => {
-    expect(getHumanAge(20, 22)).toEqual([1, 1]);
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
   });
 
-  test('should return 2, when age equal first + second', () => {
-    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  test('should return 2, when age less than first + second + other', () => {
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
   });
 
   test('should return 2 and more, when age more than first + second', () => {
     expect(getHumanAge(28, 28)).toEqual([3, 2]);
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
