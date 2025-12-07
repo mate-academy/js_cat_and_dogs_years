74833aa1e3e53b8a3cdbb3503eaefe0271438f5f
Hannah Olbrich <hannaholbrich@gmail.com>
2025-12-07 18:16:49 +0000

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,16 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return correct human age for given dog age', () => {
+    const result = getHumanAge(5, 5);
+
+    expect(result).toEqual([0, 0]);
+  });
+
+  test('should return 0 for non-positive dog age', () => {
+    const result = getHumanAge(3, -1);
+
+    expect(result).toEqual([0, 0]);
+  });
 });
