853d38d693c39fa5c3f1d3e459067c744776e744
Hannah Olbrich <hannaholbrich@gmail.com>
2025-12-07 21:47:06 +0000

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -9,18 +9,13 @@ describe('getHumanAge', () => {
   });
 
   test('should return correct human age for given dog age', () => {
-    const dogAge = 10;
-    const humanAge = 0;
-    const result = getHumanAge(humanAge, dogAge);
-
-    expect(result).toEqual([0, 0]);
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+    expect(getHumanAge(30, 30)).toEqual([3, 3]);
   });
 
   test('should return 0 for non-positive dog age', () => {
-    const dogAge = 0;
-    const humanAge = 5;
-    const result = getHumanAge(humanAge, dogAge);
-
-    expect(result).toEqual([0, 0]);
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(-5, -5)).toEqual([0, 0]);
   });
 });
