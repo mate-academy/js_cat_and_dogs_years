9abfba2acf828ad6cd8b591e5db43f4892395146
Dmytro <dimusgamer2.0@gmail.com>
2026-01-06 03:06:50 +0200

    Implement tests

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,4 +7,29 @@ describe('getHumanAge', () => {
     expect(getHumanAge)
       .toBeInstanceOf(Function);
   });
+
+  test('should return [0, 0] for input (0, 0) and (14, 14)', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test('should return [1, 1] for input (15, 15) and (23, 23)', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
+  test('should return [2, 2] for input (24, 24) and (27, 27)', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
+
+  test('should return [3, 2] for input (28, 28)', () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+  });
+
+  test('should return correct human ages for inputs', () => {
+    expect(getHumanAge(40, 50)).toEqual([6, 7]);
+    expect(getHumanAge(60, 80)).toEqual([11, 13]);
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
 });
