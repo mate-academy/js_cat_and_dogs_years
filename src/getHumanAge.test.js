7a55e68396c0091e679cea654a06e55c44b9bd4a
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2026-02-04 12:59:47 +0200

    added solution to the task

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,10 +1,27 @@
-'use strict';
+/* eslint-disable max-len */
+/* eslint-disable quotes */
+"use strict";
 
-describe('getHumanAge', () => {
-  const { getHumanAge } = require('./getHumanAge');
+describe("getHumanAge", () => {
+  const { getHumanAge } = require("./getHumanAge");
 
-  test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+  test("should be declared", () => {
+    expect(getHumanAge).toBeInstanceOf(Function);
+  });
+
+  test("should have correct array length ", () => {
+    expect(getHumanAge(100, 100)).toHaveLength(2);
+  });
+
+  test("should be array with two 0 result if two parameters < 15 ", () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test("should be be array with first zero result if first parameter = 0", () => {
+    expect(getHumanAge(0, 16)).toEqual([0, 1]);
+  });
+
+  test("should be be array with second zero result if second parameter = 0", () => {
+    expect(getHumanAge(28, 0)).toEqual([3, 0]);
   });
 });
