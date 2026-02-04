9b88e04d0355deaf9d79ac27731b1760f517d05b
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2026-02-04 13:08:21 +0200

    added correct tests according to AI comments

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -9,23 +9,39 @@ describe("getHumanAge", () => {
     expect(getHumanAge).toBeInstanceOf(Function);
   });
 
-  test("should have correct array length ", () => {
+  test("Result should have correct array length ", () => {
     expect(getHumanAge(100, 100)).toHaveLength(2);
   });
 
-  test("should be array with two 0 result if two parameters < 15 ", () => {
+  test("should be array with results [0, 0] if two parameters equal 0 ", () => {
     expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
-  test("should be be array with first zero result if first parameter = 0", () => {
-    expect(getHumanAge(0, 16)).toEqual([0, 1]);
+  test("should be array with results [0, 0] if two parameters < 15 ", () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
-  test("should be be array with second zero result if second parameter = 0", () => {
-    expect(getHumanAge(28, 0)).toEqual([3, 0]);
+  test("should be array with results [1, 1] if both parameters equal 15", () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
   });
 
-  test("should be a correct result if both parameters = 100", () => {
+  test("should be array with results [1, 1] if both parameters equal 23", () => {
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
+  test("should be array with results [2, 2] if both parameters equal 24", () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+
+  test("should be array with results [2, 2] if both parameters equal 27", () => {
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
+
+  test("should be array with results [3, 2] if both parameters equal 28", () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+  });
+
+  test("should be array with results [21, 17] if both parameters equal 100", () => {
     expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
