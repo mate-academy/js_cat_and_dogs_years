2af1faa0bca5822ec219430f02423d4f148a4902
Anton Kuchmasov <antek.ukraine@gmail.com>
2026-02-09 14:59:46 +0200

    added more testcases, fixed linter

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -13,13 +13,21 @@ describe('getHumanAge', () => {
 
   test('should return NaN if input !== numbers', () => {
     expect(getHumanAge('a', 'b')).toEqual([NaN, NaN]);
-  }); ;
+  });
 
   test('should return [5, 4] if human ages = 36', () => {
     expect(getHumanAge(36, 36)).toEqual([5, 4]);
   });
 
   test('should return [21, 17] if human ages = 100', () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
+
+  it('should return [0,0] for input (14, 14)', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  it('should  return [21,17] for input(100, 100)', () => {
     expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
