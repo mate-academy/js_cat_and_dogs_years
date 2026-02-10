fb6a486b6473739fbd694dbc9f600787b3b1cc13
Svitlana Chepa <svetachepa2320@gmail.com>
2026-02-10 11:35:57 +0000

    fix task solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -12,30 +12,30 @@ describe('getHumanAge', () => {
   });
 
   test('14,14 -> [0,0]', () => {
-    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
   test('15,15 -> [1,1]', () => {
-    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
   });
 
   test('23,23 -> [1,1]', () => {
-    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
   });
 
   test('24,24 -> [2,2]', () => {
-    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
 
   test('27,27 -> [2,2]', () => {
-    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
   });
 
   test('28,28 -> [3,2]', () => {
-    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
   });
 
   test('100,100 -> [21,17]', () => {
-    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
