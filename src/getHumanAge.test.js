911edb912f1199b0f4194aa68ac0aa6f5aad80e3
Olha Lishenko <lishenko.olha@gmail.com>
2026-02-22 13:33:50 +0200

    task solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,10 +1,28 @@
-'use strict';
+"use strict";
 
-describe('getHumanAge', () => {
-  const { getHumanAge } = require('./getHumanAge');
+describe("getHumanAge", () => {
+  const { getHumanAge } = require("./getHumanAge");
 
-  test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+  test("should be declared", () => {
+    expect(getHumanAge).toBeInstanceOf(Function);
+  });
+
+  test("should return an array", () => {
+    expect(getHumanAge(55, 41)).toBeInstanceOf(Array);
+  });
+
+  test("should be 0 if animal's age less then first", () => {
+    const result = getHumanAge(10, 8);
+    expect(result).toEqual([0, 0]);
+  });
+
+  test("should be 1 if animal's age less then sum of first and second", () => {
+    const result = getHumanAge(20, 20);
+    expect(result).toEqual([1, 1]);
+  });
+
+  test("should be rounder number", () => {
+    const result = getHumanAge(30, 38);
+    expect(result).toEqual([3, 4]);
   });
 });
