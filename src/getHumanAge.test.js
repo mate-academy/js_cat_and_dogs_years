1c645ddff9e61b0863848a407ecba136a3ec48fb
Dmytro Nikulin <dmytronikulin@Dmytros-MacBook-Air.local>
2026-02-25 12:31:10 +0200

    Add solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -4,7 +4,43 @@ describe('getHumanAge', () => {
   const { getHumanAge } = require('./getHumanAge');
 
   test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+    expect(getHumanAge).toBeInstanceOf(Function);
+  });
+
+  test(`should get correct human ages
+    for both cat and dog ages being 0`, () => {
+    const result = getHumanAge(0, 0);
+
+    expect(result).toEqual([0, 0]);
+  });
+
+  test(`should get correct human ages
+    for both cat and dog ages being 15`, () => {
+    const result = getHumanAge(15, 15);
+
+    expect(result).toEqual([1, 1]);
+  });
+
+  test(`should get correct human ages
+    for both cat and dog ages being 24`, () => {
+    const result = getHumanAge(24, 24);
+
+    expect(result).toEqual([2, 2]);
+  });
+
+  test(`should get correct human ages
+    for every next 4 years for cats
+    and every next 5 years for dogs`, () => {
+    const result = getHumanAge(28, 28);
+
+    expect(result).toEqual([3, 2]);
+  });
+
+  test(`should get correct human ages
+    for every next 4 years for cats
+    and every next 5 years for dogs`, () => {
+    const result = getHumanAge(100, 100);
+
+    expect(result).toEqual([21, 17]);
   });
 });
