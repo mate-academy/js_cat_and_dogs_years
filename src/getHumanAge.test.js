2d2e4cce8e6f4a92a66237102cab67f71f1596ab
Vital <vitaligna@gmail.com>
2026-03-06 18:15:06 +0200

    add solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,10 +1,39 @@
-'use strict';
+"use strict";
+// eslint-disable-next-line max-len //
 
-describe('getHumanAge', () => {
-  const { getHumanAge } = require('./getHumanAge');
+describe("getHumanAge", () => {
+  const { getHumanAge } = require("./getHumanAge");
 
-  test('should be declared', () => {
-    expect(getHumanAge)
-      .toBeInstanceOf(Function);
+  test("should be declared", () => {
+    expect(getHumanAge).toBeInstanceOf(Function);
+  });
+
+  test("returns [0, 0] for ages 0", () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test("returns [0, 0] below first threshold", () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test("returns [1, 1] at first threshold", () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  test("returns [1, 1] between 15 and 23", () => {
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
+  test("returns [2, 2] at second threshold", () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
+
+  test("calculates uneven cat and dog ages", () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+  });
+
+  test("calculates large ages correctly", () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
