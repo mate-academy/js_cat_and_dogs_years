3a40117a6a307103990c6b33b4b230552d9f6305
Nazarii Kaminskyi <nazarruda333@gmail.com>
2026-03-17 08:44:34 +0200

    add task solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -6,4 +6,40 @@ describe('getHumanAge', () => {
   test('should be declared', () => {
     expect(getHumanAge).toBeInstanceOf(Function);
   });
+
+  test('should return [0, 0] for zero ages', () => {
+    const result = getHumanAge(0, 0);
+
+    expect(result).toEqual([0, 0]);
+  });
+
+  test('should return [0, 0] if ages are less than 15', () => {
+    const result = getHumanAge(14, 14);
+
+    expect(result).toEqual([0, 0]);
+  });
+
+  test('should return [1, 1] for ages equal to 15', () => {
+    const result = getHumanAge(15, 15);
+
+    expect(result).toEqual([1, 1]);
+  });
+
+  test('should return [1, 1] for ages < 24', () => {
+    const result = getHumanAge(23, 23);
+
+    expect(result).toEqual([1, 1]);
+  });
+
+  test('should return [2, 2] for ages = 24', () => {
+    const result = getHumanAge(24, 24);
+
+    expect(result).toEqual([2, 2]);
+  });
+
+  test('should calculate different ages for cat and dog after 24', () => {
+    const result = getHumanAge(28, 28);
+
+    expect(result).toEqual([3, 2]);
+  });
 });
