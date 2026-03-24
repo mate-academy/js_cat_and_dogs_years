25f5639da75f97bee82e394fe198938dbed4d1e8
Heorhii Savostikov <georgesavostikov@gmail.com>
2026-03-24 22:29:47 +0200

    added extra new cases for tests

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -15,8 +15,16 @@ describe('getHumanAge', () => {
     expect(getHumanAge(14, 14)).toEqual([0, 0]);
   });
 
+  test('should return [1, 1] for ages 15 15', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
   test('should return [1, 1] for ages 23 23', () => {
     expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
+  test('should return [2, 2] for ages 24 24', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
 
   test('should return [2, 2] for ages 27, 27', () => {
