78d3f5b05706b86c06ffa24abd0ec89b3b388820
BadOfficer <tarasbondarenko2005@gmail.com>
2026-04-15 21:28:54 +0300

    Added required tests

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -7,24 +7,33 @@ describe('getHumanAge', () => {
     expect(getHumanAge).toBeInstanceOf(Function);
   });
 
-  test(`should calculate large and different ages 
-    for dogs and cats correctly`, () => {
-    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  test('should return zero human age for zero animal age', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
   });
 
   test('should calculate age at first threshold', () => {
-    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
   });
 
-  test('should calculate too small ages', () => {
-    expect(getHumanAge(14, 14)).toEqual([0, 0]);
-  });
-
-  test('should calculate medium ages', () => {
+  test(`should return 2 human years after 
+    completing 24 animal years`, () => {
     expect(getHumanAge(24, 24)).toEqual([2, 2]);
   });
 
-  test('should return zero human age for zero animal age', () => {
-    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  test(`should discard fractional human years when 
+    calculating additional years`, () => {
+    expect(getHumanAge(26, 26)).toEqual([2, 2]);
+  });
+
+  test('should correctly calculate medium animal ages', () => {
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
+
+  test('should apply different aging rules for cats and dogs', () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+  });
+
+  test('should correctly calculate large animal ages', () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
