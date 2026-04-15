fcff6480a30f60c6551868726c0eaab9600c47b8
BadOfficer <tarasbondarenko2005@gmail.com>
2026-04-15 21:14:42 +0300

    Solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -6,4 +6,25 @@ describe('getHumanAge', () => {
   test('should be declared', () => {
     expect(getHumanAge).toBeInstanceOf(Function);
   });
+
+  test(`should calculate large and different 
+        ages for dogs and cats correctly`, () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
+  });
+
+  test('should calculate age at first threshold', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  test('should calculate too small ages', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test('should calculate medium ages', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+
+  test('should return zero human age for zero animal age', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
 });
