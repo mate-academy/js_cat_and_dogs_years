13dad5242ccb72ff224b9147bba21b74ffbcc089
Linus <Linus@MacBook-Pro.local>
2026-04-29 08:37:06 +0300

    implemet tests for getHumanAge()

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,9 +1,41 @@
 'use strict';
 
-describe('getHumanAge', () => {
-  const { getHumanAge } = require('./getHumanAge');
+const { getHumanAge } = require('./getHumanAge');
 
+describe('getHumanAge', () => {
   test('should be declared', () => {
     expect(getHumanAge).toBeInstanceOf(Function);
+  });
+
+  test('should return [0, 0] for age 0', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('should return [0, 0] when age is less than 15', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test('should return [1, 1] when age equals 15', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+  });
+
+  test('should return [1, 1] when age less than 25', () => {
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
+  test('should return [2, 2] when age equals 24', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+  });
+
+  test('should return [2, 2] when age does not reach next threshold', () => {
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
+
+  test('should return [3, 2] when car reaches next threshold', () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+  });
+
+  test('should correctly convert large value', () => {
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
