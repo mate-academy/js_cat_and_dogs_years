30192b4beae8ee6ffc9e51b30ebd67e0bcd8f56a
Yurii Kumpanenko <yuriikumpanenko@ukr.net>
2026-05-01 16:53:46 +0300

    add task solution

--- src/getHumanAge.test.js
+++ src/getHumanAge.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 'use strict';
 
 describe('getHumanAge', () => {
@@ -5,5 +6,28 @@ describe('getHumanAge', () => {
 
   test('should be declared', () => {
     expect(getHumanAge).toBeInstanceOf(Function);
+  });
+
+  test('should be zero if dog or cat years are also zero', () => {
+    expect(getHumanAge(0, 0)).toEqual([0, 0]);
+  });
+
+  test('should return zeros when dog or cat years are less than 15', () => {
+    expect(getHumanAge(14, 14)).toEqual([0, 0]);
+  });
+
+  test('should return one when dog or cat years are greater than 15 and less than 24', () => {
+    expect(getHumanAge(15, 15)).toEqual([1, 1]);
+    expect(getHumanAge(23, 23)).toEqual([1, 1]);
+  });
+
+  test('should return two when dog or cat years are greater than 24 and less than 27', () => {
+    expect(getHumanAge(24, 24)).toEqual([2, 2]);
+    expect(getHumanAge(27, 27)).toEqual([2, 2]);
+  });
+
+  test('should return different values when dog or cat years are greater than 27', () => {
+    expect(getHumanAge(28, 28)).toEqual([3, 2]);
+    expect(getHumanAge(100, 100)).toEqual([21, 17]);
   });
 });
